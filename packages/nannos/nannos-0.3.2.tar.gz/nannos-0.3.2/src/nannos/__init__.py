a5ca97e09dd8643f29ff88e109c89e4896581ebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:12 +0000

    Add package nannos/0.3.2/nannos-0.3.2.tar.gz

---
+++ packages/nannos/nannos-0.3.2.tar.gz/nannos-0.3.2/src/nannos/__init__.py
@@ -0,0 +1,60 @@
+from .__about__ import __author__, __description__, __version__
+
+try:
+    _ADJOINT
+    from autograd import numpy
+except:
+    import numpy
+
+from .constants import *
+from .excitation import *
+from .lattice import *
+from .layers import *
+from .log import *
+from .simulation import *
+
+
+def set_backend(backend):
+    """Set the numerical backend.
+
+    Parameters
+    ----------
+    backend : str
+        Either ``numpy`` or ``autograd``.
+
+
+    """
+
+    import importlib
+    import sys
+
+    global _ADJOINT
+    if backend == "autograd":
+        _ADJOINT = True
+        log.info("Setting autograd backend")
+    elif backend == "numpy":
+        try:
+            del _ADJOINT
+        except:
+            pass
+        log.info("Setting numpy backend")
+    else:
+        raise ValueError(
+            f"Unknown backend '{backend}'. PLesase choose between 'numpy' and 'autograd'."
+        )
+
+    import nannos
+
+    importlib.reload(nannos)
+
+    its = [s for s in sys.modules.items() if s[0].startswith("nannos")]
+    for k, v in its:
+        importlib.reload(v)
+
+
+def get_backend():
+    try:
+        _ADJOINT
+        return "autograd" if _ADJOINT else "numpy"
+    except:
+        return "numpy"
