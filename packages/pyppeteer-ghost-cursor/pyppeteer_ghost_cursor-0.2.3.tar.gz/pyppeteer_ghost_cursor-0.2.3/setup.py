da4b20a7802189d80f3b834b317032dbc226437d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:01 +0000

    Add package pyppeteer-ghost-cursor/0.2.3/pyppeteer_ghost_cursor-0.2.3.tar.gz

---
+++ packages/pyppeteer-ghost-cursor/pyppeteer_ghost_cursor-0.2.3.tar.gz/pyppeteer_ghost_cursor-0.2.3/setup.py
@@ -0,0 +1,35 @@
+import pathlib
+import setuptools
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+INACTIVE_MESSAGE = """<h1>THIS PROJECT IS NO LONGER ACTIVE. PLEASE USE <a href="https://pypi.org/project/python-ghost-cursor/">python_ghost_cursor</a> INSTEAD.</h1>\n\n"""
+README = INACTIVE_MESSAGE + (HERE / "README.md").read_text()
+setuptools.setup(
+    name="pyppeteer_ghost_cursor",
+    version="0.2.3",
+    description="Pyppeteer implementation of Xetera/ghost-cursor",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mcolella14/pyppeteer_ghost_cursor",
+    author="mcolella14",
+    author_email="mcolella14@gmail.com",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    package_data={"pyppeteer_ghost_cursor": ["js/*.js"]},
+    install_requires=["bezier"],
+    classifiers=[
+        "Development Status :: 7 - Inactive",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Internet :: WWW/HTTP :: Browsers",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
