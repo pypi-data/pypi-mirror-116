a4c749d3c5b73decdb2f2d788d2954a21c1a1905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:18 +0000

    Add package framework-detector/0.2.0/framework-detector-0.2.0.tar.gz

---
+++ packages/framework-detector/framework-detector-0.2.0.tar.gz/framework-detector-0.2.0/framework_detector/__init__.py
@@ -0,0 +1,1 @@
+from framework_detector.main import detect, get_dockerfile
---
+++ packages/framework-detector/framework-detector-0.2.0.tar.gz/framework-detector-0.2.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['framework_detector',
+ 'framework_detector.dockerfiles',
+ 'framework_detector.frameworks']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'framework-detector',
+    'version': '0.2.0',
+    'description': 'Detects which framework is in use in a project',
+    'long_description': '# Framework detector\n\nDetects which framework is in use for a project and suggests a dockerfile.\n\nStrongly influenced by https://github.com/netlify/framework-info\n\n## Installation\n\n```sh\npip install framework-detector\n```\n\n## Usage\n\n```python\nfrom framework_detector import detect, get_dockerfile\nfrom pathlib import Path\n\nframework = detect(Path.cwd())\n\ndockerfile = get_dockerfile(framework["dockerfile"])\n```\n\n## Supported frameworks\n\n- Flask\n- Spring Boot\n- Django (No Dockerfile)',
+    'author': 'Miles Budden',
+    'author_email': 'git@miles.so',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/pbexe/framework-detector',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
