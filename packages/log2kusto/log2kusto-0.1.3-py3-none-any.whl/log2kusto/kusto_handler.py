8054b0c7af624718605771303a3d02e491ccf9bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:35 +0000

    Add package log2kusto/0.1.3/log2kusto-0.1.3-py3-none-any.whl

---
+++ packages/log2kusto/log2kusto-0.1.3-py3-none-any.whl/log2kusto/kusto_handler.py
@@ -0,0 +1,46 @@
+import logging
+import pandas as pd
+import kusto_tools.k_io.kusto_io as kio
+
+
+class KustoHandler(logging.Handler):
+    def __init__(self, cluster:str, database:str, tablename:str, attributes_list:list) -> None:
+        """Constructor
+        Args:
+            cluster: kusto cluster name
+            database: kusto database
+            table (string): table name
+            attributes_list: The list of attributes that maps to the schema of the table.
+        """
+        super().__init__()
+        
+        self.cluster = cluster
+        self.database = database
+        self.tablename = tablename
+        self.db_conn = kio.KustoIngest(kusto_ingest_cluster=self.cluster, kusto_database=self.database)
+        self.attributes = attributes_list
+
+        self.log_rows_list = []
+        return
+
+    def emit(self, record):
+        self.format(record)
+        record_values = [record.__dict__[k] for k in self.attributes]
+        self.log_rows_list.append(record_values)
+        #print(self.log_rows_list)
+        
+    def flush_writes(self):
+        log_df = pd.DataFrame(self.log_rows_list, columns=self.attributes)
+        self.db_conn.write_pandas_to_table(log_df, self.tablename)
+        print("\n {0} log records written to: cluster('{1}').database('{2}').{3}"\
+            .format(len(self.log_rows_list), self.cluster, self.database, self.tablename))
+
+    def flush(self):
+        pass
+        #self.flush_writes()
+
+    def close(self):
+        super().close()
+
+
+
