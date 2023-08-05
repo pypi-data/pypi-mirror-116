db901bb6c3d75c78885bc89b007ecc550707d333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:31 +0000

    Add package schemdule-extensions-simplegui/0.0.8/schemdule_extensions_simplegui-0.0.8-py3-none-any.whl

---
+++ packages/schemdule-extensions-simplegui/schemdule_extensions_simplegui-0.0.8-py3-none-any.whl/schemdule/extensions/simplegui/__reg__.py
@@ -0,0 +1,13 @@
+
+from types import MethodType
+
+from schemdule.prompters.builder import PrompterBuilder
+
+from . import MessageBoxPrompter, __version__
+
+
+def useMessageBox(self, auto_close: bool = False, final: bool = False) -> PrompterBuilder:
+    return self.use(MessageBoxPrompter(auto_close, final))
+
+
+PrompterBuilder.useMessageBox = useMessageBox
