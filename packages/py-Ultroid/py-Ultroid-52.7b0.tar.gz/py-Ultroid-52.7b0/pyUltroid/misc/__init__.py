24048aa2b01c40970e95aacef9b030d59195d9df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:21 +0000

    Add package py-Ultroid/52.7b0/py-Ultroid-52.7b0.tar.gz

---
+++ packages/py-Ultroid/py-Ultroid-52.7b0.tar.gz/py-Ultroid-52.7b0/pyUltroid/misc/__init__.py
@@ -0,0 +1,17 @@
+from .. import LOGS, asst, udB, ultroid_bot  # pylint ignore
+
+CMD_HELP = {}
+
+
+def sudoers():
+    return udB["SUDOS"].split()
+
+
+def should_allow_sudo():
+    if udB["SUDO"] == "True":
+        return True
+    return False
+
+
+def owner_and_sudos():
+    return [str(ultroid_bot.uid), *sudoers()]
