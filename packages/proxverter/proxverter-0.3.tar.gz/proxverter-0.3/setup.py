be747b9e5b97b37f4f4ec165cf00abbeb0014073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:20 +0000

    Add package proxverter/0.3/proxverter-0.3.tar.gz

---
+++ packages/proxverter/proxverter-0.3.tar.gz/proxverter-0.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README-pypi.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="proxverter",
+    version="0.3",
+    author="hash3liZer",
+    author_email="admin@shellvoide.com",
+    description="Cross platform system wide proxy server & TLS Interception library for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hash3liZer/Proxverter",
+    project_urls={
+        "Bug Tracker": "https://github.com/hash3liZer/Proxverter/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
+        "Operating System :: OS Independent",
+    ],
+    packages=['proxverter', 'proxverter.plugins'],
+    python_requires=">=3.7",
+    install_requires=[
+        'pyopenssl',
+        'proxy.py'
+    ]
+)
