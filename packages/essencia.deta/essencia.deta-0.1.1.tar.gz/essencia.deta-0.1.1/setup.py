037e0130bd631081047c0d972bebadd6cc058e1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:20 +0000

    Add package essencia.deta/0.1.1/essencia.deta-0.1.1.tar.gz

---
+++ packages/essencia.deta/essencia.deta-0.1.1.tar.gz/essencia.deta-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['essencia', 'essencia.deta']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['deta>=1.0.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'essencia.deta',
+    'version': '0.1.1',
+    'description': 'Deta base for essencia enterprise.',
+    'long_description': None,
+    'author': 'arantesdv',
+    'author_email': 'arantesdv@me.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
