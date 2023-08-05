5a607a814bacf71527ccbad25f4f30e65e8711d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:21 +0000

    Add package notifications-android-tv/0.1.3/notifications_android_tv-0.1.3.tar.gz

---
+++ packages/notifications-android-tv/notifications_android_tv-0.1.3.tar.gz/notifications_android_tv-0.1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="notifications_android_tv",
+    version="0.1.3",
+    description="Notifications for Android/Fire TVs",
+    url="https://github.com/engrbm87/notifications_android_tv",
+    download_url="https://github.com/engrbm87/notifications_android_tv/archive/refs/tags/0.1.3.tar.gz",
+    author="Rami Mousleh",
+    author_email="engrbm87@gmail.com",
+    install_requires=["requests"],
+    license="MIT",
+    packages=["notifications_android_tv"],
+    keywords = ["android tv", "fire tv", "notifications"],
+    zip_safe=False,
+)
