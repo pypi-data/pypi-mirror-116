ebc84cb1a0022ea44ac1dddd0cf3d91987c81f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:00 +0000

    Add package rosetta-service-monitor/0.1.1/rosetta_service_monitor-0.1.1-py3-none-any.whl

---
+++ packages/rosetta-service-monitor/rosetta_service_monitor-0.1.1-py3-none-any.whl/rosetta_service_monitor/client/conf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python  
+# -*- coding:utf-8 _*-
+""" 
+@author: xl
+@file: conf.py 
+@time: 2021/08/09
+@contact: 
+@site:  
+@software: PyCharm 
+"""
+import os
+
+import attrdict
+from dotenv import load_dotenv
+from loguru import logger
+
+load_dotenv(verbose=True)
+
+MQ_SERVER = os.environ.get('MQ_SERVER', '')
+TOPIC = os.environ.get('TOPIC', '')
+FEI_SHU = os.environ.get('FEI_SHU', '')
+
+KAFKA_CONFIG = attrdict.AttrDict({
+    'TOPIC': TOPIC,
+    'KAFKA_SERVER': MQ_SERVER.strip().split('|')
+})
+
+CONF = attrdict.AttrDict({
+    "KAFKA_CONFIG": KAFKA_CONFIG,
+    "FEI_SHU": FEI_SHU
+})
+
+logger.info(CONF)
