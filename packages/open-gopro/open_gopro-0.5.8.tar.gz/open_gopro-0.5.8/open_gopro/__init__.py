e010666bb5d54e3673261641108cd936cc89db37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:45 +0000

    Add package open-gopro/0.5.8/open_gopro-0.5.8.tar.gz

---
+++ packages/open-gopro/open_gopro-0.5.8.tar.gz/open_gopro-0.5.8/open_gopro/__init__.py
@@ -0,0 +1,21 @@
+# __init__.py/Open GoPro, Version 1.0 (C) Copyright 2021 GoPro, Inc. (http://gopro.com/OpenGoPro).
+# This copyright was auto-generated on Tue May 18 22:08:50 UTC 2021
+
+"""All GoPro exports that the the user will want should be exported here."""
+
+from open_gopro import params, constants
+from open_gopro.gopro import GoPro
+from open_gopro.constants import UUID, SettingId, StatusId, CmdId, QueryCmdId
+from open_gopro.responses import GoProResp
+
+__all__ = [
+    "params",
+    "GoPro",
+    "UUID",
+    "SettingId",
+    "StatusId",
+    "CmdId",
+    "GoProResp",
+    "constants",
+    "QueryCmdId",
+]
