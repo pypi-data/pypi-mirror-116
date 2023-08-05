3d328178384f7ab2cd1b7fe137aa032ad75e0611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:37 +0000

    Add package dv-data-generator/0.0.1/dv-data-generator-0.0.1.tar.gz

---
+++ packages/dv-data-generator/dv-data-generator-0.0.1.tar.gz/dv-data-generator-0.0.1/dv_data_generator/utils.py
@@ -0,0 +1,42 @@
+import time
+
+
+def write_csv(file_location: str, text: str):
+    with open(file_location, "wb+") as f_out:
+        for line in text.split("\n"):
+            if line.split(",")[0] == "":
+                break
+            f_out.write((line + "\n").encode("utf-8"))
+
+
+def get_report_url(service, query_id, sleep_limit=60):
+    result = None
+    exponantial_sleep = 1
+    while not result:
+        time.sleep(exponantial_sleep)
+        try:
+            query = service.queries().getquery(queryId=query_id).execute()
+            result = query if not query["metadata"]["running"] else None
+        except Exception as e:
+            if exponantial_sleep >= sleep_limit:
+                raise e
+        exponantial_sleep += 1
+    return result["metadata"]["googleCloudStoragePathForLatestReport"]
+
+
+def try_run(method, value, fail_method=None, sleep_limit=10):
+    exponantial_sleep = 1
+    failed = True
+    data = {}
+    while failed:
+        time.sleep(exponantial_sleep)
+        try:
+            data = method(value)
+            failed = False
+        except Exception as err:
+            if fail_method:
+                return fail_method(value)
+            if exponantial_sleep >= sleep_limit:
+                raise err
+        exponantial_sleep += 1
+    return data
