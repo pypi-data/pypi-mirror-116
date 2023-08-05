5a31e9e03d1b2ef88104826ada9da71cc8a66380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:23 +0000

    Add package meaning-eraser/0.0.1/meaning_eraser-0.0.1-py3-none-any.whl

---
+++ packages/meaning-eraser/meaning_eraser-0.0.1-py3-none-any.whl/meaning.py
@@ -0,0 +1,6 @@
+from time import sleep
+
+_ = input('Are you sure you want to delete the meaning of your life? (Yes / Of course): ')
+print('As you wish...')
+sleep(5)
+print('Now your life is absolutely useless.')
