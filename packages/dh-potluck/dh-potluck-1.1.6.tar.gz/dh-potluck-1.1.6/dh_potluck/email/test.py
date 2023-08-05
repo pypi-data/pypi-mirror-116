72a781b24840559f7c8ce113bca495419d768b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:43 +0000

    Add package dh-potluck/1.1.6/dh-potluck-1.1.6.tar.gz

---
+++ packages/dh-potluck/dh-potluck-1.1.6.tar.gz/dh-potluck-1.1.6/dh_potluck/email/test.py
@@ -0,0 +1,38 @@
+# from dh_potluck.email.email_service import EmailService
+#
+# # EmailService.send_email_html(
+# #     [
+# #         {
+# #             'email': 'jake.delmas@dashhudson.com',
+# #             'name': 'jake delmas',
+# #             'type': 'to'
+# #         },
+# #         {
+# #             'email': 'jakemdelmas@gmail.com',
+# #             'name': 'jake delmas',
+# #             'type': 'cc'
+# #         }
+# #     ],
+# #     subject='Test Subject',
+# #     html='<b>this is a test</b>',
+# # )
+# # EmailService.send_email_template(
+# #     [
+# #         {
+# #             'email': 'jake.delmas@dashhudson.com',
+# #             'name': 'jake delmas',
+# #             'type': 'to'
+# #         },
+# #         {
+# #             'email': 'jakemdelmas@gmail.com',
+# #             'name': 'jake delmas',
+# #             'type': 'cc'
+# #         }
+# #     ],
+# #     'restore-password',
+# #     [{'name': 'link', 'content': 'test'}, {'name': 'name', 'content': 'JAKEEE'}],
+# #     subject='template test',
+# # )
+#
+# test = 'dddd' 'ccccc'
+# print(test)
