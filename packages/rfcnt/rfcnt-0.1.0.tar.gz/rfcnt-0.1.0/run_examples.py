669ac61f3df94c9bb6a1ee17c004e1db0fcfc05a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:35 +0000

    Add package rfcnt/0.1.0/rfcnt-0.1.0.tar.gz

---
+++ packages/rfcnt/rfcnt-0.1.0.tar.gz/rfcnt-0.1.0/run_examples.py
@@ -0,0 +1,14 @@
+import sys
+from rfcnt.tests import examples
+
+print( "Running example 1..." )
+examples.example_1()
+
+
+"""
+Jupyter Notebook:
+!pip install ./rfcnt-0.1.0.tar.gz
+!pip install --upgrade matplotlib
+from rfcnt.tests import examples
+examples.example_1()
+"""
