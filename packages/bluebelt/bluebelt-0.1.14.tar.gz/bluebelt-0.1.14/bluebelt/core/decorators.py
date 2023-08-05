7bc0c86697ce02a2a95cd1a5634f1cda48b973ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:52 +0000

    Add package bluebelt/0.1.14/bluebelt-0.1.14.tar.gz

---
+++ packages/bluebelt/bluebelt-0.1.14.tar.gz/bluebelt-0.1.14/bluebelt/core/decorators.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import math
+
+def class_methods(cls):
+
+    @property
+    def attributes(self):
+        for attr, value in self.__dict__.items():
+            if isinstance(value, (str, int, float, list, tuple)):
+                print(f'{attr}: {value}')
+            else:
+                print(f'{attr}: {type(value)}')
+
+    @property
+    def help(self):
+        print(self.__doc__)
+
+    setattr(cls, 'attributes', attributes)
+    setattr(cls, 'help', help)
+    
+    return cls
+
+
+
+# resampling
+resolution_functions = {
+    'sum': sum,
+    'min': min,
+    'max': max,
+    }
+
+def resolution_function(func):
+    resolution_functions[func.__name__] = func
+    return func
