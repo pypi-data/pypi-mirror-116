9fe458a1065615bbaaaffc497d0d2dce31a696ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:02 +0000

    Add package continual-inference/0.1.2/continual-inference-0.1.2.tar.gz

---
+++ packages/continual-inference/continual-inference-0.1.2.tar.gz/continual-inference-0.1.2/continual/interface.py
@@ -0,0 +1,56 @@
+from abc import ABC
+
+from torch import Tensor
+
+
+class _CoModule(ABC):
+    """Base class for continual modules.
+    Deriving from this class enforces that neccessary class methods are implemented
+    """
+
+    def __init_subclass__(cls) -> None:
+        for fn, description in [
+            ("forward", "frame-wise forward computation"),
+            (
+                "forward_regular",
+                "clip-wise forward computation with state initialisation",
+            ),
+            (
+                "forward_regular_unrolled",
+                "clip-wise forward without state initialisation, but which is identical to the non-continual component",
+            ),
+        ]:
+            assert callable(
+                getattr(cls, fn, None)
+            ), f"A CoModule should implement a `{fn}` function which performs {description}."
+
+        assert (
+            type(getattr(cls, "delay", None)) == property
+        ), "A CoModule should implement a `delay` property."
+
+    def forward(self, input: Tensor) -> Tensor:
+        """Clip-wise forward computation with state initialisation"""
+        ...  # pragma: no cover
+
+    def forward_regular(self, input: Tensor) -> Tensor:
+        """Clip-wise forward computation with state initialisation"""
+        ...  # pragma: no cover
+
+    def forward_regular_unrolled(self, input: Tensor) -> Tensor:
+        """Clip-wise forward without state initialisation, but which is identical to the non-continual component"""
+        ...  # pragma: no cover
+
+    @property
+    def delay(self) -> int:
+        """Temporal delay of the module
+
+        Returns:
+            int: Temporal delay of the module
+        """
+        ...  # pragma: no cover
+
+    def clean_state(self):
+        """Clean module state
+        This serves as a dummy function for modules which do not require state-cleanup
+        """
+        ...  # pragma: no cover
