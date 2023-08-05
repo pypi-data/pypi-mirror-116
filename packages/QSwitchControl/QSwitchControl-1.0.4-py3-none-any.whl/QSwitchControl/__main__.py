8b719a04298c470e00620827a8b7251d9882e616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:21 +0000

    Add package QSwitchControl/1.0.4/QSwitchControl-1.0.4-py3-none-any.whl

---
+++ packages/QSwitchControl/QSwitchControl-1.0.4-py3-none-any.whl/QSwitchControl/__main__.py
@@ -0,0 +1,53 @@
+"""
+MIT License
+
+Copyright (c) 2021 Parsa.py
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout
+
+from QSwitchControl import SwitchControl
+
+
+class Form(QWidget):
+	def __init__(self):
+		super().__init__()
+		self.initUI()
+
+	def initUI(self):
+		self.resize(400, 400)
+		self.setWindowTitle("SwitchControl test")
+		self.setStyleSheet("""
+		background-color: #222222;
+		""")
+		switch_control = SwitchControl()
+		hbox = QHBoxLayout()
+		hbox.addWidget(switch_control, Qt.AlignCenter, Qt.AlignCenter)
+		self.setLayout(hbox)
+		self.show()
+
+
+app = QApplication(sys.argv)
+form = Form()
+if __name__ == '__main__':
+	sys.exit(app.exec_())
