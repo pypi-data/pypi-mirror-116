ec1a876ee6c0d105dd22502290c51db1a3452271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:00 +0000

    Add package http-containerize/0.4.3/http-containerize-0.4.3.tar.gz

---
+++ packages/http-containerize/http-containerize-0.4.3.tar.gz/http-containerize-0.4.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='http-containerize',
+    version='0.4.3',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/evankanderson/pyfun',
+    license="Apache",
+    packages=setuptools.find_packages(include=("framework",)),
+    install_requires=[
+        'cloudevents >=1.2, <2',
+        'Flask>=1.0.2,<2',
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: Unix',
+    ],
+)
