9bbdb2aa8e768eab2b3cf137def41dbfec4962b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:22 +0000

    Add package omd-emea/0.0.4/omd_emea-0.0.4.tar.gz

---
+++ packages/omd-emea/omd_emea-0.0.4.tar.gz/omd_emea-0.0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Generic code for processing and execution for OMD EMEA'
+
+# Setting up
+setup(
+    name="omd_emea",
+    version=VERSION,
+    author="Aniruddha Sengupta",
+    author_email="aniruddha.sengupta@omd.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['requests', 'pandas', 'facebook-business', 'google', 'sqlalchemy', 'selenium'],
+    keywords=['python', 'omd', 'general', 'generic'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
