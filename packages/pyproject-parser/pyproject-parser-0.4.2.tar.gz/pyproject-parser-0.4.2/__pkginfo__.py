1dc12f4c6ea634f40a1e368dc6c0daaa8ef194cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:00 +0000

    Add package pyproject-parser/0.4.2/pyproject-parser-0.4.2.tar.gz

---
+++ packages/pyproject-parser/pyproject-parser-0.4.2.tar.gz/pyproject-parser-0.4.2/__pkginfo__.py
@@ -0,0 +1,20 @@
+#  This file is managed by 'repo_helper'. Don't edit it directly.
+#  Copyright © 2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#
+#  This file is distributed under the same license terms as the program it came with.
+#  There will probably be a file called LICEN[S/C]E in the same directory as this file.
+#
+#  In any case, this program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# This script based on https://github.com/rocky/python-uncompyle6/blob/master/__pkginfo__.py
+#
+
+__all__ = ["extras_require"]
+
+extras_require = {
+		"readme": ["docutils==0.16", "readme-renderer[md]>=27.0"],
+		"cli": ["click>=7.1.2", "consolekit>=1.2.0"],
+		"all": ["click>=7.1.2", "consolekit>=1.2.0", "docutils==0.16", "readme-renderer[md]>=27.0"]
+		}
