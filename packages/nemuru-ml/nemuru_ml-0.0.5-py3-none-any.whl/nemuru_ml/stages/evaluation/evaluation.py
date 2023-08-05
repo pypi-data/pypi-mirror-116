bde999eecd59ffa548e2a6774ebe97a8d4b882c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:35 +0000

    Add package nemuru-ml/0.0.5/nemuru_ml-0.0.5-py3-none-any.whl

---
+++ packages/nemuru-ml/nemuru_ml-0.0.5-py3-none-any.whl/nemuru_ml/stages/evaluation/evaluation.py
@@ -0,0 +1,26 @@
+import abc
+from common.model_registry import ModelRegistry
+
+
+class Evaluation(abc.ABC):
+
+    def __init__(self, model_registry: ModelRegistry):
+        self._model_registry = model_registry
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'is_better_than_staging') and
+                callable(subclass.is_better_than_staging))
+
+    @classmethod
+    def initialize(cls, model_registry: ModelRegistry):
+        return cls(model_registry)
+
+    @abc.abstractmethod
+    def is_better_than_staging(self, model_uri: str, model_name: str) -> bool:
+        """Load in the data set"""
+        raise NotImplementedError
+
+    def update_model(self, model_uri: str, model_name: str):
+        if self.is_better_than_staging(model_uri, model_name):
+            self._model_registry.update_staging(model_uri, model_name)
