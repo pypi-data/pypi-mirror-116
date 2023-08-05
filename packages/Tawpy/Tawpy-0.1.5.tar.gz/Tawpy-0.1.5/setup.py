7c03fd875bf33ef8eaf521c7b8e831834404cb0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:07 +0000

    Add package Tawpy/0.1.5/Tawpy-0.1.5.tar.gz

---
+++ packages/Tawpy/Tawpy-0.1.5.tar.gz/Tawpy-0.1.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+file = open("README.md")
+long_description = file.read() 
+file.close()
+
+
+setup(
+    name = 'Tawpy', 
+    version = '0.1.5', 
+    author = 'devKeef',
+    author_email = 'keef.devv@gmail.com',
+    description = 'For requesting gifs from the tenor website',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/devKeef/Tawpy',
+    classifiers =[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    install_requires = [
+        "requests" , 
+        "json"
+    ],
+    packages = find_packages(),
+    python_requires = '>=2.7', 
+    include_package_data = True
+)
