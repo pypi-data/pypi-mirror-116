e935bd67f48e4654da2ccf56e0ae90b327b2d232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:20 +0000

    Add package nafigator/0.1.30/nafigator-0.1.30-py2.py3-none-any.whl

---
+++ packages/nafigator/nafigator-0.1.30-py2.py3-none-any.whl/nafigator/utils.py
@@ -0,0 +1,108 @@
+# coding: utf-8
+
+"""Utils module."""
+
+import io
+import re
+from lxml import etree
+
+
+def load_dtd(dtd_url):
+
+    dtd = None
+    r = open(dtd_url)
+    if r:
+        dtd_file_object = io.StringIO(r.read())
+        dtd = etree.DTD(dtd_file_object)
+    if dtd is None:
+        logging.error("failed to load dtd from" + str(dtd_url))
+    else:
+        logging.info("Succesfully to load dtd from" + str(dtd_url))
+    return dtd
+
+
+def time_in_correct_format(datetime_obj):
+    """
+    Function that returns the current time (UTC)
+    """
+    return datetime_obj.strftime("%Y-%m-%dT%H:%M:%SUTC")
+
+
+def normalize_token_orth(orth):
+    if "\n" in orth:
+        return "NEWLINE"
+    else:
+        return remove_control_characters(orth)
+
+
+def prepare_comment_text(text: str):
+    """ """
+    text = text.replace("--", "DOUBLEDASH")
+    if text.endswith("-"):
+        text = text[:-1] + "SINGLEDASH"
+    return text
+
+
+# def remove_illegal_chars(text):
+#     return re.sub(illegal_pattern, "", text)
+
+# Only allow legal strings in XML:
+# http://stackoverflow.com/a/25920392/2899924
+# illegal_pattern = re.compile(
+#     "[^\u0020-\uD7FF\u0009\u000A\u000D\uE000-\uFFFD\u10000-\u10FFFF]+"
+# )
+# improved version:
+
+# A regex matching the "invalid XML character range"
+ILLEGAL_XML_CHARS_RE = re.compile(
+    r"[\x00-\x08\x0b\x0c\x0e-\x1F\uD800-\uDFFF\uFFFE\uFFFF]"
+)
+
+
+def remove_illegal_chars(text):
+    return re.sub(ILLEGAL_XML_CHARS_RE, "", text)
+
+
+def remove_control_characters(html):
+    # type: (t.Text) -> t.Text
+    """
+    Strip invalid XML characters that `lxml` cannot parse.
+    """
+    # See: https://github.com/html5lib/html5lib-python/issues/96
+    #
+    # The XML 1.0 spec defines the valid character range as:
+    # Char ::= #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF]
+    #
+    # We can instead match the invalid characters by inverting that range into:
+    # InvalidChar ::= #xb | #xc | #xFFFE | #xFFFF | [#x0-#x8] | [#xe-#x1F] | [#xD800-#xDFFF]
+    #
+    # Sources:
+    # https://www.w3.org/TR/REC-xml/#charsets,
+    # https://lsimons.wordpress.com/2011/03/17/stripping-illegal-characters-out-of-xml-in-python/
+    def strip_illegal_xml_characters(s, default, base=10):
+        # Compare the "invalid XML character range" numerically
+        n = int(s, base)
+        if (
+            n in (0xB, 0xC, 0xFFFE, 0xFFFF)
+            or 0x0 <= n <= 0x8
+            or 0xE <= n <= 0x1F
+            or 0xD800 <= n <= 0xDFFF
+        ):
+            return ""
+        return default
+
+    # We encode all non-ascii characters to XML char-refs, so for example "💖" becomes: "&#x1F496;"
+    # Otherwise we'd remove emojis by mistake on narrow-unicode builds of Python
+    html = html.encode("ascii", "xmlcharrefreplace").decode("utf-8")
+    html = re.sub(
+        r"&#(\d+);?",
+        lambda c: strip_illegal_xml_characters(c.group(1), c.group(0)),
+        html,
+    )
+    html = re.sub(
+        r"&#[xX]([0-9a-fA-F]+);?",
+        lambda c: strip_illegal_xml_characters(c.group(1), c.group(0), base=16),
+        html,
+    )
+    html = ILLEGAL_XML_CHARS_RE.sub("", html)
+    return html
