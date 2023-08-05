85cef2fcefcc7ff4a7da81b5d313bbc68c06ef41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:44 +0000

    Add package rwrap/0.1.1/rwrap-0.1.1.tar.gz

---
+++ packages/rwrap/rwrap-0.1.1.tar.gz/rwrap-0.1.1/rwrap/__init__.py
@@ -0,0 +1,23 @@
+"""Make using R packages from Python easier."""
+
+import sys
+from importlib import metadata
+
+from loguru import logger
+import rpy2.rinterface as ri
+
+from .wrapper import RLibraryWrapper
+
+
+__version__ = metadata.version("rwrap")
+
+logger.remove()
+logger.add(sys.stderr, level="WARNING")
+
+
+def __getattr__(name: str) -> RLibraryWrapper:
+    try:
+        return RLibraryWrapper(name)
+    except ri.RRuntimeError:
+        # TODO: use ModuleNotFoundError, ImportError
+        raise AttributeError(f'Error while importing "{name}"')
