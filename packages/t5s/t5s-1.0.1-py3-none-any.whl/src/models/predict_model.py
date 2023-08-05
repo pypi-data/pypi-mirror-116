0e2ff13dde664bad3d5eb6c286f21c70d7bd1b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:30 +0000

    Add package t5s/1.0.1/t5s-1.0.1-py3-none-any.whl

---
+++ packages/t5s/t5s-1.0.1-py3-none-any.whl/src/models/predict_model.py
@@ -0,0 +1,23 @@
+import yaml
+
+from src.models.model import Summarization
+import pandas as pd
+
+
+def predict_model(text):
+    """
+    Predict the summary of the given text.
+    """
+    with open("model_params.yml") as f:
+        params = yaml.safe_load(f)
+
+    model = Summarization()
+    model.load_model(model_type=params["model_type"], model_dir=params["model_dir"])
+    pre_summary = model.predict(text)
+    return pre_summary
+
+
+if __name__ == "__main__":
+    text = pd.load_csv("data/processed/test.csv")["input_text"][0]
+    pre_summary = predict_model(text)
+    print(pre_summary)
