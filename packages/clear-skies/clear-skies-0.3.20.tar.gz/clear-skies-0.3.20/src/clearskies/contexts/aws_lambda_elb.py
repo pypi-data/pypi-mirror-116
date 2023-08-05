bb300ec604cf3bd70da53a35c981330d4da24d35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:52 +0000

    Add package clear-skies/0.3.20/clear-skies-0.3.20.tar.gz

---
+++ packages/clear-skies/clear-skies-0.3.20.tar.gz/clear-skies-0.3.20/src/clearskies/contexts/aws_lambda_elb.py
@@ -0,0 +1,31 @@
+from ..input_outputs import AWSLambdaELB as AWSLambdaELBInputOutput
+import clearskies
+from .build_context import build_context
+from .context import Context
+
+
+class AWSLambdaELB(Context):
+    def __init__(self, di):
+        super().__init__(di)
+
+    def __call__(self, event, context):
+        if self._handler is None:
+            raise ValueError("Cannot execute AWSLambdaELB context without first configuring it")
+
+        return self._handler(AWSLambdaELBInputOutput(event, context))
+
+def aws_lambda_elb(
+    application,
+    di_class=clearskies.di.StandardDependencies,
+    bindings=None,
+    binding_classes=None,
+    binding_modules=None
+):
+    return build_context(
+        AWSLambdaELB,
+        application,
+        di_class,
+        bindings=bindings,
+        binding_classes=binding_classes,
+        binding_modules=binding_modules
+    )
---
+++ packages/clear-skies/clear-skies-0.3.20.tar.gz/clear-skies-0.3.20/src/clearskies/contexts/test.py
@@ -0,0 +1,70 @@
+from ..authentication import public
+from ..mocks import InputOutput
+from ..di import StandardDependencies
+from ..backends import MemoryBackend
+from datetime import datetime, timezone
+from .build_context import build_context
+from .context import Context
+
+
+class Test:
+    _application = None
+    input_output = None
+    memory_backend = None
+    now = None
+
+    def __init__(self, di):
+        self.di = di
+
+    def configure(self, application):
+        self.now = datetime.now().replace(tzinfo=timezone.utc, microsecond=0)
+        self._application = application
+        self.memory_backend = self.di.build(MemoryBackend, cache=False)
+        self.memory_backend.silent_on_missing_tables(silent=True)
+
+        self.di.bind('now', self.now)
+        self.di.bind('cursor_backend', self.memory_backend)
+
+    def __call__(self, method=None, body=None, headers=None, url=None, input_output=None):
+        if self._application is None:
+            raise ValueError("Cannot call the test context without an application")
+
+        if input_output is None:
+            input_output = InputOutput()
+        if body is not None:
+            input_output.set_body(body)
+        if headers is not None:
+            input_output.set_request_headers(headers)
+        if method is not None:
+            input_output.set_request_method(method)
+        if url is not None:
+            input_output.set_request_url(url)
+
+        self._handler = self.di.build(self._application.handler_class, cache=False)
+        self._handler.configure({
+            **{'authentication': public()},
+            **self._application.handler_config,
+        })
+        return self._handler(input_output)
+
+    def bind(self, key, value):
+        self.di.bind(key, value)
+
+    def build(self, key):
+        return self.di.build(key)
+
+def test(
+    application,
+    di_class=StandardDependencies,
+    bindings=None,
+    binding_classes=None,
+    binding_modules=None
+):
+    return build_context(
+        Test,
+        application,
+        di_class,
+        bindings=bindings,
+        binding_classes=binding_classes,
+        binding_modules=binding_modules
+    )
