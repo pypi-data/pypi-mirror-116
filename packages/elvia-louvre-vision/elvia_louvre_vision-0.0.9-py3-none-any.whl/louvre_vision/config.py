e1cdf1e9762b74da0b16ac2d68cb451b432476b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:21 +0000

    Add package elvia-louvre-vision/0.0.9/elvia_louvre_vision-0.0.9-py3-none-any.whl

---
+++ packages/elvia-louvre-vision/elvia_louvre_vision-0.0.9-py3-none-any.whl/louvre_vision/config.py
@@ -0,0 +1,9 @@
+class Config():
+    """Configuration values."""
+
+    custom_vision_max_training_file_size = int(6e6)
+    default_delimiter: str = ';'
+    default_image_variant: str = "standard"
+    hash_length: int = 10
+    image_longer_side: int = 2000
+    predictions_rounding_decimal_places: int = 3
