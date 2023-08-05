79dcfaef87c321dee6ce2c1ee55c457906666e69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:46 +0000

    Add package open-jarvis-sdk/0.0.1/open_jarvis_sdk-0.0.1-py3-none-any.whl

---
+++ packages/open-jarvis-sdk/open_jarvis_sdk-0.0.1-py3-none-any.whl/jarvis_sdk/__init__.py
@@ -0,0 +1,9 @@
+"""
+## List of modules:
+
+* [Skill](jarvis_sdk/Skill.html)
+    * [CapturedIntentData](jarvis_sdk/Skill.html#CapturedIntentData)
+
+"""
+
+from .Skill import Skill, CapturedIntentData
