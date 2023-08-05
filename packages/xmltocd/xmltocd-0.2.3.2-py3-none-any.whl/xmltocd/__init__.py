24d9fac95a7a57bf7f8c1b0c15435af14365bc0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:29 +0000

    Add package xmltocd/0.2.3.2/xmltocd-0.2.3.2-py3-none-any.whl

---
+++ packages/xmltocd/xmltocd-0.2.3.2-py3-none-any.whl/xmltocd/__init__.py
@@ -0,0 +1,12 @@
+from ._parse import *
+from ._ntype import *
+from ._exception import *
+from ._rely import *
+from ._collections import *
+from .manager import *
+from .struct import *
+
+name = 'xmltocd'
+__version__ = '0.2.3.2'
+__author__ = 'jiyang'
+__license__ = 'MIT'
