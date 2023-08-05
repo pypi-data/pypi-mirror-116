dc866efb5b4bceb6a370539c0f5e16c9eb3722dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:39 +0000

    Add package salebox-django/0.0.222/salebox-django-0.0.222.tar.gz

---
+++ packages/salebox-django/salebox-django-0.0.222.tar.gz/salebox-django-0.0.222/saleboxdjango/management/commands/lib/models/contentpageitem.py
@@ -0,0 +1,21 @@
+from saleboxdjango.models import ContentPage, ContentPageItem
+
+
+def sync_contentpageitem(data):
+    # get fieldname
+    fieldnames = [f.name for f in ContentPageItem._meta.get_fields()]
+
+    # retrieve / create object
+    o = ContentPageItem.objects.filter(id=data['id']).first()
+    if o is None:
+        o = ContentPageItem()
+
+    # populate foreign keys
+    if data['page']:
+        data['page'] = ContentPage.objects.get(id=data['page'])
+
+    # save data
+    for key in data:
+        if key in fieldnames:
+            setattr(o, key, data[key])
+    o.save()
