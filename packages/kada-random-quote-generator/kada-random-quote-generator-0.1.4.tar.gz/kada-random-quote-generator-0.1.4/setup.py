1f226b6ab3523aed8a296c3c39ca936b184325f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:11 +0000

    Add package kada-random-quote-generator/0.1.4/kada-random-quote-generator-0.1.4.tar.gz

---
+++ packages/kada-random-quote-generator/kada-random-quote-generator-0.1.4.tar.gz/kada-random-quote-generator-0.1.4/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['random_quote_generator']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'kada-random-quote-generator',
+    'version': '0.1.4',
+    'description': 'Randomly generate programming wisdom quote.',
+    'long_description': None,
+    'author': 'Kada Liao',
+    'author_email': 'kadaliao@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
