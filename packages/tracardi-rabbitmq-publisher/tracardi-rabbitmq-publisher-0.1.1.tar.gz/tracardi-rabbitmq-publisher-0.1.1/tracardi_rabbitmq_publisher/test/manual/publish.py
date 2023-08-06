95bc69b25baa6599e34f569addac919de46e2a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:59 +0000

    Add package tracardi-rabbitmq-publisher/0.1.1/tracardi-rabbitmq-publisher-0.1.1.tar.gz

---
+++ packages/tracardi-rabbitmq-publisher/tracardi-rabbitmq-publisher-0.1.1.tar.gz/tracardi-rabbitmq-publisher-0.1.1/tracardi_rabbitmq_publisher/test/manual/publish.py
@@ -0,0 +1,23 @@
+import asyncio
+
+from kombu import Connection, Exchange, Queue, Producer
+
+from tracardi_rabbitmq_publisher.model.queue_config import QueueConfig
+from tracardi_rabbitmq_publisher.plugin import RabbitPublisherAction
+
+async def main():
+    plugin = await RabbitPublisherAction.build(
+        **{
+            "source": {
+                "id": "58df3b5c-3109-4750-bb5b-81f5386950b1"
+            },
+            "queue": {
+                "name": "tracardi3",
+                "routingKey": "trk",
+            }
+        }
+    )
+
+    await plugin.run({"a": 1})
+
+asyncio.run(main())
