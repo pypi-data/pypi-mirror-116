b2cb8d90559f6128ca2b53b0175d85a71e839624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:26 +0000

    Add package botd/51/botd-51-py3-none-any.whl

---
+++ packages/botd/botd-51-py3-none-any.whl/bot/all.py
@@ -0,0 +1,11 @@
+# This file is placed in the Public Domain.
+
+"import all but spc"
+
+import bot.adm as adm
+import bot.cms as cms
+import bot.fnd as fnd
+import bot.irc as irc
+import bot.log as log
+import bot.tdo as tdo
+import bot.udp as udp
