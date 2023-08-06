72ebebd4973d98a00d6ec7aa668875a24af501d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:58 +0000

    Add package LNMarkets/1.4.0/LNMarkets-1.4.0.tar.gz

---
+++ packages/LNMarkets/LNMarkets-1.4.0.tar.gz/LNMarkets-1.4.0/LNMarkets/APIUrls.py
@@ -0,0 +1,28 @@
+
+lnapi = 'https://api.lnmarkets.com/v1'
+
+# User
+userUrl = '/user'
+updateUrl = '/user/update-password'
+userHistoryUrl = '/user/history'
+tokenUrl = '/user/jwt'
+depositUrl = '/user/deposit'
+withdrawUrl = '/user/withdraw'
+withdrawLNUrl = '/user/withdraw/lnurl'
+loginUrl = '/login/token'
+logoutUrl = '/user/logout'
+
+#Positions
+positionUrl = '/futures'
+addMarginUrl = '/futures/add-margin'
+cashinUrl = '/futures/cash-in'
+cancelUrl = '/futures/cancel'
+closeAllUrl = '/futures/all/close'
+
+#State
+stateUrl = '/state'
+nodeUrl = '/state/node'
+
+#History
+indexUrl = '/history/index'
+bidOfferUrl = '/history/bid-offer'
