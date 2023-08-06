0643bb21fc2345298fff199fafdbcce2cc9ffe4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:50 +0000

    Add package SCALECAST/0.2.6/SCALECAST-0.2.6.tar.gz

---
+++ packages/SCALECAST/SCALECAST-0.2.6.tar.gz/SCALECAST-0.2.6/examples/README.md
@@ -0,0 +1,12 @@
+# Examples
+
+- Example 1: [housing.py](housing.py)
+- Example 2: [avocados.ipynb](avocados.ipynb)
+- Example 3: [housing_different_levels.py](housing_different_levels.py)
+- Example 4 (external repository): [housing_prices.ipynb](https://github.com/mikekeith52/housing_prices/blob/main/housing_prices.ipynb)
+
+## See the blog posts:
+1. [Forecasting one series](https://towardsdatascience.com/introducing-scalecast-a-forecasting-library-pt-1-33b556d9b019)
+2. [Forecasting many series at scale](https://towardsdatascience.com/may-the-forecasts-be-with-you-introducing-scalecast-pt-2-692f3f7f0be5)
+3. [Forecasting one series on different levels](https://towardsdatascience.com/forecast-on-3-levels-introducing-scalecast-pt-3-eb725e0be6c9)
+4. [Two-differenced data forecasting](https://medium.com/codex/forecasting-home-prices-in-your-zip-code-ab9be2c33d0e)
---
+++ packages/SCALECAST/SCALECAST-0.2.6.tar.gz/SCALECAST-0.2.6/setup.py
@@ -0,0 +1,31 @@
+
+from setuptools import setup, find_packages
+
+setup(
+  name = 'SCALECAST',
+  version = '0.2.6',
+  license='MIT',        # https://help.github.com/articles/licensing-a-repository
+  short_description = 'A flexible, minimal-code forecasting object.',
+  long_description='See the full documentation on [GitHub](https://github.com/mikekeith52/scalecast).',
+  long_description_content_type='text/markdown',
+  author = 'Michael Keith',
+  author_email = 'mikekeith52@gmail.com',
+  packages=find_packages('src'),
+  package_dir={'': 'src'},
+  url = 'https://github.com/mikekeith52/scalecast',
+  keywords = ['FORECAST', 'SCALE', 'FLEXIBLE'],
+  install_requires = [
+    'scikit-learn',
+    'statsmodels',
+    'scipy',
+    'eli5',
+    'numpy',
+    'pandas',
+    'scipy',
+    'matplotlib',
+    'seaborn',
+    'xgboost',
+    'lightgbm',
+    'openpyxl'
+  ],
+)
