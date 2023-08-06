b2bffbe5dbbc048494d63c5409249e956c3b71b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:37 +0000

    Add package xgboost-distribution/0.2.0/xgboost_distribution-0.2.0-py2.py3-none-any.whl

---
+++ packages/xgboost-distribution/xgboost_distribution-0.2.0-py2.py3-none-any.whl/xgboost_distribution/distributions/base.py
@@ -0,0 +1,39 @@
+"""Distribution base class
+"""
+from abc import ABC, abstractmethod
+from collections import namedtuple
+
+
+class BaseDistribution(ABC):
+    """Base class distribution for XGBDistribution.
+
+    Note that distributions are stateless, hence a distribution is just a collection of
+    functions that operate on the data (`y`) and the outputs of the xgboost (`params`).
+    """
+
+    def __init__(self):
+        self.Predictions = namedtuple("Predictions", (p for p in self.params))
+
+    def check_target(self, y):
+        pass
+
+    @property
+    @abstractmethod
+    def params(self):
+        pass
+
+    @abstractmethod
+    def starting_params(self, y):
+        pass
+
+    @abstractmethod
+    def gradient_and_hessian(self, y, params):
+        pass
+
+    @abstractmethod
+    def loss(self, y, params):
+        pass
+
+    @abstractmethod
+    def predict(self, params):
+        pass
