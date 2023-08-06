33afc461725181a26989363a5aaa61c40dd6ed89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:33 +0000

    Add package shishua/0.2.0/shishua-0.2.0.tar.gz

---
+++ packages/shishua/shishua-0.2.0.tar.gz/shishua-0.2.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+shishua = Extension('shishuabinding',
+    extra_compile_args = ['-march=native', '-g'],
+    sources = ['src/binding/binding.pyx'],
+    depends = [
+        'src/binding/buffered-shishua.h',
+        'src/binding/shishua.h',
+        'src/binding/shishua-sse2.h',
+        'src/binding/shishua-avx2.h',
+        'src/binding/shishua-neon.h',
+    ])
+
+setup(ext_modules = cythonize([shishua]))
