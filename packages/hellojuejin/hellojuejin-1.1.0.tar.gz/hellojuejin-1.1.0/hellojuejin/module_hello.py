d39daeee6dfe64130ab77a5a6577e9ebd12daade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:48 +0000

    Add package hellojuejin/1.1.0/hellojuejin-1.1.0.tar.gz

---
+++ packages/hellojuejin/hellojuejin-1.1.0.tar.gz/hellojuejin-1.1.0/hellojuejin/module_hello.py
@@ -0,0 +1,7 @@
+name = "模块hellojuejin"
+
+print(name)
+
+def hello(name):
+
+    print("hello {0},welcome to juejin".format(name) )
---
+++ packages/hellojuejin/hellojuejin-1.1.0.tar.gz/hellojuejin-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'hellojuejin',
+        version = '1.1.0',
+        py_modules = ['hellojuejin.module_hello'],
+        author = 'Anne_cute',
+        author_email = '****@qq.com',
+        url = 'http://www.hp.com',
+        description = 'hello Juejin',
+    )
