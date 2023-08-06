7ab5b6114db5d8386a6afc67abbaf50ff7e8d075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:22 +0000

    Add package vantage-client/0.1.0/vantage-client-0.1.0.tar.gz

---
+++ packages/vantage-client/vantage-client-0.1.0.tar.gz/vantage-client-0.1.0/vantage/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from vantage.api.costs_api import CostsApi
+from vantage.api.ping_api import PingApi
+from vantage.api.prices_api import PricesApi
