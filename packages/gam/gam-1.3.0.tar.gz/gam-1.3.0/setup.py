f070a2c94fcc564de699f4758010d1a438279c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:54 +0000

    Add package gam/1.3.0/gam-1.3.0.tar.gz

---
+++ packages/gam/gam-1.3.0.tar.gz/gam-1.3.0/setup.py
@@ -0,0 +1,59 @@
+# SPDX-Copyright: Copyright (c) Capital One Services, LLC
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2021 Capital One Services, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from io import open
+
+from setuptools import setup
+
+CURR_DIR = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(CURR_DIR, "README.md"), encoding="utf-8") as file_open:
+    LONG_DESCRIPTION = file_open.read()
+
+DESCRIPTION = "Global Explanations for Deep Neural Networks"
+
+extras_require = {
+    "complete": [
+        "dask[complete] >= 2021.2.0",
+        "dask-distance >= 0.2.0",
+        "dask-ml >= 1.8.0",
+        "plotly-express >= 0.4.1",
+        "nbformat >= 4.2.0",
+    ]
+}
+
+setup(
+    name="gam",
+    version="1.3.0",
+    packages=["gam", "tests"],
+    maintainer="Brian Barr",
+    maintainer_email="brian.barr@capitalone.com",
+    url="https://github.com/capitalone/global-attribution-mapping",
+    classifiers=["Programming Language :: Python :: 3"],
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    license="Apache License 2.0",
+    install_requires=[
+        "pandas >= 1.1.3",
+        "scikit-learn >= 0.23.2",
+        "numpy >= 1.19.2",
+        "kaleido == 0.2.1",
+    ],
+    extras_require=extras_require,
+    python_requires=">=3.6",
+)
---
+++ packages/gam/gam-1.3.0.tar.gz/gam-1.3.0/tests/test_banditPAM.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import time
+
+import numpy as np
+import pandas as pd
+
+from gam.clustering import KMedoids
+from gam.spearman_distance import spearman_squared_distance
+import dask.dataframe as dd
+
+np.random.seed(42)
+
+
+def test_banditPAM():
+    # load the data
+    df = pd.read_csv("tests/banditPAM_data.csv")
+    attributions = df.values
+
+    """"Run kmedoids on sample attributions"""
+    kmed2 = KMedoids(
+        4,
+        dist_func="euclidean",
+        batchsize=200,
+        # dist_func=spearman_squared_distance,
+        max_iter=20,
+        tol=0.001,
+        init_medoids="bandit",
+        swap_medoids="bandit",
+        verbose=False,
+    )
+    start_time = time.time()
+    kmed2.fit(attributions, verbose=False)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Finished test in {elapsed_time:.2f}")
+    print(kmed2.centers)
+
+    # if testing with 'euclidean' distance
+    assert np.isin(kmed2.centers, [256, 209, 470, 304]).all()
+
+
+def test_banditPAM_dask():
+    # load the data
+    ddf = dd.read_csv("tests/banditPAM_data.csv", dtype={'ARTICLE_ID': 'object'}).repartition(npartitions=4)
+    attributions = ddf.to_dask_array(lengths=True)
+
+    """"Run kmedoids on sample attributions"""
+    kmed2 = KMedoids(
+        n_clusters=4,
+        dist_func="euclidean",
+        batchsize=200,
+        # dist_func=spearman_squared_distance,
+        max_iter=20,
+        tol=0.001,
+        init_medoids="bandit",
+        swap_medoids="bandit",
+        verbose=False,
+    )
+    start_time = time.time()
+    kmed2.fit(attributions, verbose=False)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Finished test in {elapsed_time:.2f}")
+    print(kmed2.centers)
+
+    # if testing with 'euclidean' distance
+    assert np.isin(kmed2.centers, [256, 209, 470, 304]).all()
