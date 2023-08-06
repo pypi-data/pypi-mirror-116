d994b42c6df48293bce8d5d756e0a5885a2983bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:30 +0000

    Add package freiner/3.0.2/freiner-3.0.2.tar.gz

---
+++ packages/freiner/freiner-3.0.2.tar.gz/freiner-3.0.2/tests/util.py
@@ -0,0 +1,30 @@
+import functools
+import math
+import time
+from pathlib import Path
+from typing import Any, Callable, ContextManager
+
+from freezegun import freeze_time as _freeze_time
+from freezegun.api import FrozenDateTimeFactory
+
+
+ROOTDIR = Path(__file__).parent.parent
+DOCKERDIR = ROOTDIR / ".docker"
+
+
+def fixed_start(func: Callable[..., Any]):
+    @functools.wraps(func)
+    def _inner(*args: Any, **kwargs: Any):
+        start = time.time()
+        while time.time() < math.ceil(start):
+            time.sleep(0.01)
+        return func(*args, **kwargs)
+
+    return _inner
+
+
+def freeze_time() -> ContextManager[FrozenDateTimeFactory]:
+    f = _freeze_time()
+    # This is a necessary fix for our testing, and the third-party type hints aren't sufficient
+    f.ignore = tuple(set(f.ignore) - {"threading"})  # type: ignore
+    return f
