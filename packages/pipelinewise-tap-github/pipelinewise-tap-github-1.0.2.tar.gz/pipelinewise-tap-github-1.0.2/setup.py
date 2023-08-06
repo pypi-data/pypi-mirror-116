25b8cc87a3360abbd2c7118084a0019d4efda851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:36 +0000

    Add package pipelinewise-tap-github/1.0.2/pipelinewise-tap-github-1.0.2.tar.gz

---
+++ packages/pipelinewise-tap-github/pipelinewise-tap-github-1.0.2.tar.gz/pipelinewise-tap-github-1.0.2/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='pipelinewise-tap-github',
+      version='1.0.2',
+      description='Singer.io tap for extracting data from the GitHub API',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Wise',
+      url='https://github.com/transferwise/pipelinewise-tap-github',
+      classifiers=[
+          'License :: OSI Approved :: GNU Affero General Public License v3',
+          'Programming Language :: Python :: 3 :: Only'
+      ],
+      py_modules=['tap_github'],
+      install_requires=[
+          'pipelinewise-singer-python==1.*',
+          'requests==2.20.0'
+      ],
+      extras_require={
+          'test': [
+              'pylint',
+              'pytest',
+              'ipdb',
+              'nose',
+          ]
+      },
+      entry_points='''
+          [console_scripts]
+          tap-github=tap_github:main
+      ''',
+      packages=['tap_github'],
+      package_data = {
+          'tap_github': ['tap_github/schemas/*.json']
+      },
+      include_package_data=True
+)
