b812dfe68e89f4b0b02f5dc02e36986c262d2120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:15 +0000

    Add package pipypackageexample/0.1/pipypackageexample-0.1.tar.gz

---
+++ packages/pipypackageexample/pipypackageexample-0.1.tar.gz/pipypackageexample-0.1/setup.py
@@ -0,0 +1,13 @@
+from os import name
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pipypackageexample',
+    version = '0.1',
+    license = 'MIT',
+    description = 'paquete de prueba para una clase udemy',
+    author = 'Alejandra Arias',
+    packages = find_packages(),
+    url = "https://github.com/aleiarias/pipypackageexample"
+
+)
