de6c98bbbf047724d8da15aa813aade68afcb166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:13 +0000

    Add package MyCryptoKeys/0.1.0/MyCryptoKeys-0.1.0-py3.9.egg

---
+++ packages/MyCryptoKeys/MyCryptoKeys-0.1.0-py3.9.egg/MyCryptoKeys/__main__.py
@@ -0,0 +1,10 @@
+"""This module serves as the entry point of MyCryptoKeys."""
+
+
+def main():
+    """The actual entry point."""
+    raise NotImplementedError()
+
+
+if __name__ == '__main__':
+    main()
