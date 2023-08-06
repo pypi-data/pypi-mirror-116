09f0024b09aa324b7e1ed50b108dc8905f082f81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:38 +0000

    Add package tapioca-arbache/0.2.0/tapioca_arbache-0.2.0-py3-none-any.whl

---
+++ packages/tapioca-arbache/tapioca_arbache-0.2.0-py3-none-any.whl/tapioca_arbache/resource_mapping.py
@@ -0,0 +1,12 @@
+CRM_PERFIL_ENDPOINT = {
+    "perfis": {
+        "resource": "backend/perfis/",
+        "methods": ["GET"],
+    }
+}
+CRM_OAUTH = {
+    "oauth_token": {
+        "resource": "backend/oauth-token/",
+        "methods": ["POST"],
+    }
+}
