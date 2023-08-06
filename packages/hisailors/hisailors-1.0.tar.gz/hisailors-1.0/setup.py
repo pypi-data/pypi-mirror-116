7d79bc99de97e360b4695dd74b4111807eaf802c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:44 +0000

    Add package hisailors/1.0/hisailors-1.0.tar.gz

---
+++ packages/hisailors/hisailors-1.0.tar.gz/hisailors-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hisailors',
+    url="https://github.com/skiloop/hisailors",
+    version='1.0',
+    license='MIT',
+    zip_safe=False,
+    author='skiloop',
+    author_email='skiloop@gmail.com',
+    description='A group of tools for http',
+    python_requires='>=3.6',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+)
