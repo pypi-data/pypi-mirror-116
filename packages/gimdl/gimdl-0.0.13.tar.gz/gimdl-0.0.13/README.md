97f377d0f600ce0bb48fa4825d58e87150cb44d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:10 +0000

    Add package gimdl/0.0.13/gimdl-0.0.13.tar.gz

---
+++ packages/gimdl/gimdl-0.0.13.tar.gz/gimdl-0.0.13/README.md
@@ -0,0 +1,29 @@
+# Google Image Search & Download using the API
+
+This will fetch images using the google API and save to an images folder. You just specify the search term. eg. "cat" or "dog"
+
+## Installation
+
+Run the following to install:
+
+```python
+pip install gimdl
+```
+
+## Usage
+
+```python
+import gimdl
+from gimdl import fetch_images
+
+# specify what images you want to fetch
+fetch_images("cat")
+```
+| Requirement | URL |
+| ----------- | ----------- |
+| pip install Google-Images-Search | https://pypi.org/project/Google-Images-Search/ |
+| Google API Key | https://console.developers.google.com |
+| pip install python-dotenv | https://pypi.org/project/python-dotenv/ |
+| pip install windows-curses (Windows only)  | https://pypi.org/project/windows-curses/ |
+
+
---
+++ packages/gimdl/gimdl-0.0.13.tar.gz/gimdl-0.0.13/setup.py
@@ -0,0 +1,32 @@
+# setup.py
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='gimdl',
+    version='0.0.13',
+    description='Google Image Search Downloader - using API ',
+    py_modules=["gimdl"],
+    install_requires=['python-dotenv'],
+    package_dir={'': 'src'},
+    
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+    ],
+    project_urls={  # Optional
+        'GitHub': 'https://github.com/RGGH/gimdl',
+    },
+
+    keywords='googleimages, images, search', 
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
