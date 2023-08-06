98498d5c54b774398c2f3cf7e0a3ada09d42302d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:21 +0000

    Add package ailever/0.2.721/ailever-0.2.721-py3-none-any.whl

---
+++ packages/ailever/ailever-0.2.721-py3-none-any.whl/ailever/analysis/ANALYSISInterfaces.py
@@ -0,0 +1,16 @@
+from .data_processing import DataControlBlock
+from .data_preprocessing import DataPreprocessor
+from .data_transformation import DataTransformer
+from .exploratory_data_analysis import ExploratoryDataAnalysis
+from .exploratory_data_analysis import Counting
+
+def DataProcessor(frames):
+    return DataControlBlock(frames)
+
+def EDA(frame, save=False, path='ExploratoryDataAnalysis', verbose=True):
+    return ExploratoryDataAnalysis(frame=frame, save=save, path=path, verbose=verbose)
+
+def counting(frame, view='table', save=False, path=None):
+    counter = Counting(frame=frame)
+    frame = counter.CountUniqueValues(view=view, save=save, path=path)
+    return frame
