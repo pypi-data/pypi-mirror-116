7cdd0292faf74a03a5c29aad15e13b658e5d10d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:31 +0000

    Add package sqwalk/0.5.2/sqwalk-0.5.2.tar.gz

---
+++ packages/sqwalk/sqwalk-0.5.2.tar.gz/sqwalk-0.5.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name='sqwalk',
+      version='0.5.2',
+      description='A simple quantum walk simulator in python',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      keywords='quantum walker stochastic decomposition',
+      url='https://github.com/Buffoni/SQWalk',
+      author='Lorenzo Buffoni',
+      author_email='lorenzo.buffoni@unifi.it',
+      license='Apache License 2.0',
+      packages=['sqwalk'],
+      install_requires=[
+          'qutip',
+          'numpy',
+          'matplotlib',
+          'qiskit'
+      ],
+      include_package_data=True,
+      zip_safe=False)
