7e36fa5063c12137319c078012adbfa7151e11fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:09 +0000

    Add package pyzeversolar/0.0.2/pyzeversolar-0.0.2.tar.gz

---
+++ packages/pyzeversolar/pyzeversolar-0.0.2.tar.gz/pyzeversolar-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyzeversolar",
+    version="0.0.2",
+    author="brahmah",
+    author_email="brahmah90@gmail.com",
+    description="Library to communicate with ZeverSolar inverters",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/brahmah/pyzeversolar",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires='>=3.6',
+)
