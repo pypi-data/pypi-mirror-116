00baa2346f2573c0f7f281f65049965953928469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:20 +0000

    Add package blankly/1.4.1b0/blankly-1.4.1b0-py3-none-any.whl

---
+++ packages/blankly/blankly-1.4.1b0-py3-none-any.whl/blankly/deployment/reporter_headers.py
@@ -0,0 +1,63 @@
+"""
+    Class for defining headers for deployment infrastructure
+    Copyright (C) 2021  Emerson Dove
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from typing import Any
+
+from blankly.strategy.strategy_base import Strategy
+
+
+class Reporter:
+    def __init__(self):
+        self.__live_vars = {}
+        pass
+
+    def export_live_var(self, var: Any, name: str, description: str = None):
+        """
+        Create a variable that can be updated by external processes
+        All strings must be in ascii characters
+
+        Args:
+            var: Any variable that can represented in a string (ex: float, str, int)
+            name: The name of the live_var
+            description (optional): A longer description for use in GUIs or other areas where context is important
+        """
+        self.__live_vars[id(var)] = var
+
+    def update_live_var(self, var):
+        """
+        Get the variable as with any changes that may have occurred
+
+        Args:
+            var: The variable that was exported initially
+        """
+        return self.__live_vars[id(var)]
+
+    def export_strategy(self, strategy: Strategy):
+        """
+        Export a strategy for monitoring. This is used internally on the construction of the strategy object
+
+        Args:
+            strategy (Strategy): The strategy object to monitor
+        """
+        pass
+
+    def log_strategy_event(self, strategy_object, event_name, **kwargs):
+        """
+        Export a strategy event that has occurred
+        """
+        pass
