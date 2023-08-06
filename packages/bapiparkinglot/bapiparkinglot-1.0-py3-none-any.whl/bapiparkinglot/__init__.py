b3799d3551b211b247de272954bdbf40499b535d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:12 +0000

    Add package bapiparkinglot/1.0/bapiparkinglot-1.0-py3-none-any.whl

---
+++ packages/bapiparkinglot/bapiparkinglot-1.0-py3-none-any.whl/bapiparkinglot/__init__.py
@@ -0,0 +1,10 @@
+from bapiparkinglot.core import *
+
+__author__ = 'Haryo Bagas Assyafah'
+__copyright__ = 'Copyright 2021 Bear Au Jus - ジュースとくま'
+__credits__ = ['Haryo Bagas Assyafah']
+__license__ = "MIT"
+__version__ = "1.0"
+__maintainer__ = "Haryo Bagas Assyafah"
+__email__ = "haryobagasasyafah6@gmail.com"
+__status__ = "Production"
