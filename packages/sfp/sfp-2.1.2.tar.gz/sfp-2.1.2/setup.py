28d05282b7c4c42ff3d0a7f4d428fced66f6bf20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:32 +0000

    Add package sfp/2.1.2/sfp-2.1.2.tar.gz

---
+++ packages/sfp/sfp-2.1.2.tar.gz/sfp-2.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["rfc3986==1.5.0"]
+
+setup(
+    name="sfp",
+    version="2.1.2",
+    author="XenoCorn",
+    description="Python implementation of SFP",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xenocorn/PySFP",
+    packages=['sfp'],
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Internet",
+    ],
+)
