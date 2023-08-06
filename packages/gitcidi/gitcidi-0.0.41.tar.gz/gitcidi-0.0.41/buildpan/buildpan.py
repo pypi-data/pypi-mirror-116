297e47bc8c337ddec1bf18b795e3dcaa1d300a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:41 +0000

    Add package gitcidi/0.0.41/gitcidi-0.0.41.tar.gz

---
+++ packages/gitcidi/gitcidi-0.0.41.tar.gz/gitcidi-0.0.41/buildpan/buildpan.py
@@ -0,0 +1,24 @@
+import git
+import os
+import click
+import git
+import sys
+from github import Github
+import subprocess
+
+
+# cli_commands for the buildpan 
+
+from cli_commands import init
+from cli_commands import version
+
+@click.group(help="CLI tool to manage CI- CD of projects")
+def buildpan():
+    pass
+
+
+buildpan.add_command(init.init)
+buildpan.add_command(version.version)
+
+if __name__ == '__main__':
+    buildpan()
