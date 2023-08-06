e632d33e0eaf4fbf5f49a74c7442f87e78cbb69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:41 +0000

    Add package pyftp-annek/0.2.0/pyftp-annek-0.2.0.tar.gz

---
+++ packages/pyftp-annek/pyftp-annek-0.2.0.tar.gz/pyftp-annek-0.2.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['pyftp_annek']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.1,<9.0.0']
+
+entry_points = \
+{'console_scripts': ['pyftp = pyftp_annek.cli:main']}
+
+setup_kwargs = {
+    'name': 'pyftp-annek',
+    'version': '0.2.0',
+    'description': 'FTP Uploader',
+    'long_description': None,
+    'author': 'Michael MacKenna',
+    'author_email': 'mpmackenna@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
