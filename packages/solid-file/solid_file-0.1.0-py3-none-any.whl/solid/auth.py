25d9180f15e0ed4b99d17f66a27ede2145885b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:01 +0000

    Add package solid-file/0.1.0/solid_file-0.1.0-py3-none-any.whl

---
+++ packages/solid-file/solid_file-0.1.0-py3-none-any.whl/solid/auth.py
@@ -0,0 +1,25 @@
+import httpx
+
+
+class Auth:
+    def __init__(self):
+        self.client = httpx.Client()
+
+    @property
+    def is_login(self) -> bool:
+        return self.client.cookies.get('nssidp.sid') is not None
+
+    def login(self, idp, username, password):
+        # NSS only
+        url = '/'.join((idp, 'login/password'))
+
+        data = {
+            'username': username,
+            'password': password
+        }
+
+        r = self.client.post(url, data=data)
+        r.raise_for_status()
+        
+        if not self.is_login:
+            raise Exception('Cannot login.')
