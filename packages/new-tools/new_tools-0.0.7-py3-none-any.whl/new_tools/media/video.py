4bdaa4ed82c1d7d309f057c16af572edfd0d0665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:57 +0000

    Add package new-tools/0.0.7/new_tools-0.0.7-py3-none-any.whl

---
+++ packages/new-tools/new_tools-0.0.7-py3-none-any.whl/new_tools/media/video.py
@@ -0,0 +1,21 @@
+import time
+
+VIDEO_FORMAT = (".mp4", ".m4v", ".mov", ".avi", ".wmv", ".flv", ".qt", ".asf", ".mpeg", ".mpg", ".vob", ".mkv",
+                ".rm", ".rmvb", ".ts", ".dat")
+
+class FPS(object):
+    def __init__(self):
+        self.frame = 0
+        
+    def start(self):
+        self.start_time = time.time()
+        
+    def stop(self):
+        self.elapsed_time = time.time() - self.start_time
+    
+    def update(self):
+        self.frame += 1
+        
+    def fps(self):
+        self.fps_value = self.frame / self.elapsed_time
+        return self.fps_value
