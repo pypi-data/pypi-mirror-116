3eb6ee8570226323acfc41c2d11d2794ad0f32a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:46 +0000

    Add package instmuse/0.1.3/instmuse-0.1.3.tar.gz

---
+++ packages/instmuse/instmuse-0.1.3.tar.gz/instmuse-0.1.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['instmuse']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numuse>=0.1.1,<0.2.0']
+
+setup_kwargs = {
+    'name': 'instmuse',
+    'version': '0.1.3',
+    'description': 'Physical instrument library',
+    'long_description': "# Instmuse\n\nThis package is a set of tools designed to work with physical instruments, it uses numuse's notation.\n\nTo get started visit the [documentation](https://instmuse.readthedocs.io/en/latest/).\n\nIf you're able to find use in this project, then please consider [contributing](https://www.patreon.com/cuppajoeman).\n",
+    'author': 'ccn',
+    'author_email': 'ccn@cuppajoeman.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://gitlab.com/cuppajoeman/instmuse',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/instmuse/instmuse-0.1.3.tar.gz/instmuse-0.1.3/src/instmuse/instrument_music.py
@@ -0,0 +1,43 @@
+import numuse.converters
+import numuse.music
+from fractions import Fraction
+
+from src.instmuse.converters import generate_guitar_voicings_from_MG_shorthand
+
+if __name__ == "__main__":
+
+    b = 1
+    # half
+    h = 1 / 2
+    # thirds
+    t = Fraction(b, 3)
+    # two thirds
+    tt = 2 * t
+
+    # TODO develop the equiavlent form of rootedintervalcollection but for modulargridnotecollection
+    two_five_one = [
+        [("(X 5 X 5 5 5)", [4 * b])],
+        [("(X X 5 7 6 7)", [4 * b])],
+        [("(X 3 5 4 5 X)", [4 * b])],
+    ]
+
+    two_five_one_no_duration = "(X 5 X 5 6 5) (X X 5 7 6 7) (X 3 5 4 5 X)"
+
+    voicings = generate_guitar_voicings_from_MG_shorthand(two_five_one_no_duration)
+
+    for voicing in voicings:
+        print("voicing start")
+        for note in voicing.notes:
+            print(note.value % 12)
+        print("voicing end")
+
+    # print(generate_MGNCs_from_MG_shorthand("(X 5 X 5 5 5) (X X 5 7 6 7) (X 3 5 4 5 X)"))
+    # measures = numuse.converters.parse_music_measures(
+    #    two_five_one, generate_MGNCs_from_MG_shorthand
+    # )
+    # m = numuse.music.Music(measures, 120)
+    # for measure in m.measures:
+    #    for line in measure.m_lines:
+    #        for moment in line.m_moments:
+    #            # print(moment)
+    #            pass
