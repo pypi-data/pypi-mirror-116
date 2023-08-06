83e71787bb650c5bd5307ac90a84309f456cad2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:22 +0000

    Add package mfypublic/1.1.3/mfypublic-1.1.3-py3-none-any.whl

---
+++ packages/mfypublic/mfypublic-1.1.3-py3-none-any.whl/mfypublic/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding:utf8 -*- #
+#-----------------------------------------------------------------------------------
+# ProjectName:   MfyPublic
+# FileName:     __init__.py
+# Author:      MingFeiyang
+# Datetime:    2021-05-20 15:58
+#-----------------------------------------------------------------------------------
+
+from .read_data import read_excel_data,read_yaml_data,read_json_data
+from .get_log import get_logging
