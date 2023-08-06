383c13f3d125e19c612ea0ba3412657d596e433b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:20 +0000

    Add package kalenis-user-view/6.0.0/kalenis_user_view-6.0.0.tar.gz

---
+++ packages/kalenis-user-view/kalenis_user_view-6.0.0.tar.gz/kalenis_user_view-6.0.0/__init__.py
@@ -0,0 +1,18 @@
+# This file is part of kalenis_user_view module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+
+from trytond.pool import Pool
+from . import user_view
+from . import user
+from . import lang
+# from . import table
+
+
+def register():
+    Pool.register(
+        user_view.UserView,
+        user_view.UserViewField,
+        user.User,
+        lang.Lang,
+        module='user_view', type_='model')
