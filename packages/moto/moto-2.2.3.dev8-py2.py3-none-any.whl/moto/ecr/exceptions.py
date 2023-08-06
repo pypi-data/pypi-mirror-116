ebefb6e484f807bbcb8aed392ba56acf92df6100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:51 +0000

    Add package moto/2.2.3.dev8/moto-2.2.3.dev8-py2.py3-none-any.whl

---
+++ packages/moto/moto-2.2.3.dev8-py2.py3-none-any.whl/moto/ecr/exceptions.py
@@ -0,0 +1,103 @@
+from __future__ import unicode_literals
+from moto.core.exceptions import JsonRESTError
+
+
+class LifecyclePolicyNotFoundException(JsonRESTError):
+    code = 400
+
+    def __init__(self, repository_name, registry_id):
+        super().__init__(
+            error_type=__class__.__name__,
+            message=(
+                "Lifecycle policy does not exist "
+                f"for the repository with name '{repository_name}' "
+                f"in the registry with id '{registry_id}'"
+            ),
+        )
+
+
+class RegistryPolicyNotFoundException(JsonRESTError):
+    code = 400
+
+    def __init__(self, registry_id):
+        super().__init__(
+            error_type=__class__.__name__,
+            message=(
+                f"Registry policy does not exist in the registry with id '{registry_id}'"
+            ),
+        )
+
+
+class RepositoryAlreadyExistsException(JsonRESTError):
+    code = 400
+
+    def __init__(self, repository_name, registry_id):
+        super().__init__(
+            error_type=__class__.__name__,
+            message=(
+                f"The repository with name '{repository_name}' already exists "
+                f"in the registry with id '{registry_id}'"
+            ),
+        )
+
+
+class RepositoryNotEmptyException(JsonRESTError):
+    code = 400
+
+    def __init__(self, repository_name, registry_id):
+        super().__init__(
+            error_type=__class__.__name__,
+            message=(
+                f"The repository with name '{repository_name}' "
+                f"in registry with id '{registry_id}' "
+                "cannot be deleted because it still contains images"
+            ),
+        )
+
+
+class RepositoryNotFoundException(JsonRESTError):
+    code = 400
+
+    def __init__(self, repository_name, registry_id):
+        super().__init__(
+            error_type=__class__.__name__,
+            message=(
+                f"The repository with name '{repository_name}' does not exist "
+                f"in the registry with id '{registry_id}'"
+            ),
+        )
+
+
+class RepositoryPolicyNotFoundException(JsonRESTError):
+    code = 400
+
+    def __init__(self, repository_name, registry_id):
+        super().__init__(
+            error_type=__class__.__name__,
+            message=(
+                "Repository policy does not exist "
+                f"for the repository with name '{repository_name}' "
+                f"in the registry with id '{registry_id}'"
+            ),
+        )
+
+
+class ImageNotFoundException(JsonRESTError):
+    code = 400
+
+    def __init__(self, image_id, repository_name, registry_id):
+        super().__init__(
+            error_type=__class__.__name__,
+            message=(
+                f"The image with imageId {image_id} does not exist "
+                f"within the repository with name '{repository_name}' "
+                f"in the registry with id '{registry_id}'"
+            ),
+        )
+
+
+class InvalidParameterException(JsonRESTError):
+    code = 400
+
+    def __init__(self, message):
+        super().__init__(error_type=__class__.__name__, message=message)
