591394532ae8c96378bfb59aa59791ea5884138a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:43 +0000

    Add package vectice/0.0.8/vectice-0.0.8.tar.gz

---
+++ packages/vectice/vectice-0.0.8.tar.gz/vectice-0.0.8/vectice/api/model.py
@@ -0,0 +1,34 @@
+from typing import List, Optional
+from urllib.parse import urlencode
+
+from vectice.api.project import ProjectApi
+from vectice.entity import Model
+from .json_object import JsonObject
+from .Page import Page
+
+
+class ModelApi(ProjectApi):
+    def __init__(self, project_token: str, _token: Optional[str] = None):
+        super().__init__(project_token=project_token, _token=_token)
+        self._model_path = super().api_base_path + "/model"
+
+    @property
+    def api_base_path(self) -> str:
+        return self._model_path
+
+    def list_models(self, search: str = None, page_index=Page.index, page_size=Page.size) -> List[Model]:
+        queries = {"index": page_index, "size": page_size}
+        if search:
+            queries["search"] = search
+        models = self._get(self.api_base_path + "?" + urlencode(queries))["items"]
+        return [Model(model) for model in models]
+
+    def create_model(self, model: JsonObject) -> Model:
+        if model.get("name") is None:
+            raise ValueError('"name" must be provided in model.')
+        if model.get("type") is None:
+            model["type"] = "OTHER"
+        return Model(self._post(self.api_base_path, model))
+
+    def update_model(self, model_id: int, model: JsonObject) -> Model:
+        return Model(self._put(self.api_base_path + "/" + str(model_id), model))
---
+++ packages/vectice/vectice-0.0.8.tar.gz/vectice-0.0.8/vectice/api/model_version.py
@@ -0,0 +1,35 @@
+from typing import List, Optional
+from urllib.parse import urlencode
+
+from .json_object import JsonObject
+from .model import ModelApi
+from vectice.entity import ModelVersion
+from .Page import Page
+
+
+class ModelVersionApi(ModelApi):
+    def __init__(self, project_token: str, model_id: int, _token: Optional[str] = None):
+        super().__init__(project_token=project_token, _token=_token)
+        self._model_id = model_id
+        self._model_version_path = super().api_base_path + "/" + str(model_id) + "/version"
+
+    @property
+    def model_id(self) -> int:
+        return self._model_id
+
+    @property
+    def api_base_path(self) -> str:
+        return self._model_version_path
+
+    def list_model_versions(self, page_index=Page.index, page_size=Page.size) -> List[ModelVersion]:
+        queries = {"index": page_index, "size": page_size}
+        model_versions = self._get(self.api_base_path + "?" + urlencode(queries))["items"]
+        return [ModelVersion(model_version) for model_version in model_versions]
+
+    def create_model_version(self, model_version: JsonObject) -> ModelVersion:
+        if model_version.get("status") is None:
+            raise ValueError('"status" must be provided in model_version.')
+        return ModelVersion(self._post(self.api_base_path, model_version))
+
+    def update_model_version(self, model_id: int, model_version: JsonObject) -> ModelVersion:
+        return ModelVersion(self._put(self.api_base_path + "/" + str(model_id), model_version))
