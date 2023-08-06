2d4b8c0589ecd657806815a448fd4f59f2e5d891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:46 +0000

    Add package OSIx/0.4.0/OSIx-0.4.0.zip

---
+++ packages/OSIx/OSIx-0.4.0.zip/OSIx-0.4.0/OSIx.py
@@ -0,0 +1,130 @@
+"""
+OSIx - Open Source Intelligence eXplorer.
+
+By: Th3 0bservator
+"""
+
+import sys
+import importlib
+import os
+import types
+import logging
+import logging.config
+
+from configparser import ConfigParser
+from typing import Dict, List, Optional
+
+from core.base_module import BaseModule
+from core.chrome_driver_manager import ChromeDrivers
+from core.dir_manager import DirectoryManagerUtils
+
+logger = logging.getLogger()
+
+BANNER: str = '''
+OSIx - Open Source Intelligence eXplorer
+Version: 0.0.3
+By: Th3 0bservator
+'''
+
+
+class OSIx:
+    """OSIx Main Module."""
+
+    def __init__(self) -> None:
+        """Module Initialization."""
+
+        self.config: Optional[ConfigParser] = None
+        self.available_modules: List[str] = []
+
+    def main(self) -> int:
+        """Application Entrypoint."""
+
+        self.__setup_logging()
+
+        logger.info(BANNER)
+
+        if not self.check_python_version():
+            return 1
+
+        DirectoryManagerUtils.ensure_dir_struct('data/')
+        DirectoryManagerUtils.ensure_dir_struct('data/temp/')
+        DirectoryManagerUtils.ensure_dir_struct('data/export/')
+        self.__load_settings()
+        self.__list_modules()
+
+        if not self.config:
+            return -1
+
+        # Load Modules
+        args: Dict = {}
+        data: Dict = {}
+        logger.info('[*] Executing Pipeline:')
+        for pipeline_item in self.config['PIPELINE']['pipeline_sequence'].split('\n'):
+
+            if pipeline_item == '':
+                continue
+
+            logger.info(f'\t[+] {pipeline_item}')
+            pipeline_item_meta: List[str] = pipeline_item.split('.')
+
+            osix_module: types.ModuleType = importlib.import_module(f'modules.{pipeline_item_meta[0]}')
+            module_instance: BaseModule = getattr(osix_module, pipeline_item_meta[1])()
+            module_instance.run(
+                config=self.config,
+                args=args,
+                data=data
+                )
+
+        # Shutdown all Drivers
+        ChromeDrivers.shutdown()
+
+        return 0
+
+    def check_python_version(self) -> bool:
+        """Check if the Current Python Version is Supported."""
+
+        # Check Python Requirements
+        major = sys.version_info[0]
+        minor = sys.version_info[1]
+
+        python_version = str(sys.version_info[0]) + "." + str(sys.version_info[1]) + "." + str(sys.version_info[2])
+
+        if major != 3 or major == 3 and minor < 6:
+            logger.fatal('This application requires at least, Python 3.7.6')
+            logger.fatal(f'Current Installed Version is: {python_version}')
+            return False
+
+        return True
+
+    def __setup_logging(self) -> None:
+        """Setup Log Config."""
+
+        logging.config.fileConfig('logging.conf')
+
+    def __list_modules(self) -> None:
+        """
+        List All Available Modules.
+
+        :return: None
+        """
+
+        # Check Modules
+        logger.info('[*] Installed Modules:')
+        for file in os.listdir('modules'):
+            if file not in ('__init__.py', '__pycache__'):
+                logger.info(f'\t{file}')
+
+    def __load_settings(self) -> None:
+        """
+        Load the config.ini file into Settings Object.
+
+        :return: None
+        """
+
+        logger.info('[*] Loading Configurations:')
+        self.config = ConfigParser()
+        self.config.read('config.ini')
+
+
+if __name__ == '__main__':
+    exit(OSIx().main())
---
+++ packages/OSIx/OSIx-0.4.0.zip/OSIx-0.4.0/modules/input_args_handler.py
@@ -0,0 +1,134 @@
+"""Input Arguments Handler."""
+
+from configparser import ConfigParser
+from typing import Dict
+from uuid import uuid4
+
+import argparse
+import logging
+
+from core.base_module import BaseModule
+
+logger = logging.getLogger()
+
+
+class InputArgsHandler(BaseModule):
+    """Module That Handle the Input Arguments."""
+
+    __ARGS: Dict = {
+        'job_name': {
+            'param': '--job_name',
+            'type': str,
+            'action': 'store',
+            'help': 'Job Name. Used to Save/Restore State File.',
+            'default': uuid4().hex.upper()
+            },
+
+        'purge_temp_files': {
+            'param': '--purge_temp_files',
+            'type': str,
+            'action': 'store_true',
+            'help': 'Force Delete All Temporary Files',
+            'default': False
+            },
+
+        'btc_wallet': {
+            'param': '--btc_wallet',
+            'type': str,
+            'action': 'store',
+            'help': 'BitCoin Wallet Address',
+            'default': ''
+            },
+        'btc_get_transactions': {
+            'param': '--btc_get_transactions',
+            'type': str,
+            'action': 'store_true',
+            'help': 'Allow to Download All BitCoin Transactions from Wallet',
+            'default': ''
+            },
+        'export_btc_transactions_as_graphml': {
+            'param': '--export_btc_transactions_as_graphml',
+            'type': str,
+            'action': 'store_true',
+            'help': 'Allow to Export the BitCoin Transactions as GraphML',
+            'default': False
+            },
+        'export_btc_transactions_as_gephi': {
+            'param': '--export_btc_transactions_as_gephi',
+            'type': str,
+            'action': 'store_true',
+            'help': 'Allow to Export the BitCoin Transactions as Gephi File',
+            'default': True
+            },
+
+        'username': {
+            'param': '--username',
+            'type': str,
+            'action': 'store',
+            'help': 'Username to Search',
+            'default': ''
+            },
+        'username_scan': {
+            'param': '--username_scan',
+            'type': str,
+            'action': 'store_true',
+            'help': 'Allow the Executor to Scan the the Username in All Social Networks and WebSites',
+            'default': False
+            },
+        'username_allow_nsfw_scan': {
+            'param': '--username_allow_nsfw_scan',
+            'type': str,
+            'action': 'store_true',
+            'help': 'Allow the Executor to Scan the NSFW WebSites',
+            'default': False
+            },
+        'username_print_result': {
+            'param': '--username_print_result',
+            'type': str,
+            'action': 'store_true',
+            'help': 'Allow to Print the Result in sysout',
+            'default': True
+            },
+        'username_show_all': {
+            'param': '--username_show_all',
+            'type': str,
+            'action': 'store_true',
+            'help': 'Allow to Print all Results, otherwise, Print Only the Founded Ones.',
+            'default': False
+            },
+        'username_enable_dump_file': {
+            'param': '--username_enable_dump_file',
+            'type': str,
+            'action': 'store_true',
+            'help': 'Allow to Dump a Result file into data/export Folder.',
+            'default': False
+            },
+
+        }
+
+    def run(self, config: ConfigParser, args: Dict, data: Dict) -> None:
+        """Execute Module."""
+
+        parser = argparse.ArgumentParser(description='OSIx - Open Source Intelligence Explorer.')
+
+        # Add Parameters to Arg Parser
+        for arg in InputArgsHandler.__ARGS:
+            spec: Dict = InputArgsHandler.__ARGS[arg]
+
+            parser.add_argument(
+                spec['param'], action=spec['action'], dest=arg,
+                help=spec['help'], default=spec['default']
+                )
+
+        # Parse Args
+        input_args = parser.parse_args()
+
+        # Add to Result Args
+        for arg in InputArgsHandler.__ARGS:
+            args.update(
+                {arg: getattr(input_args, arg)}
+                )
+
+        # Print Settings
+        for key, value in args.items():
+            logger.info(f'\t\t{key} = {value}')
