8bb75ab48b47ebd08c0277c0f3e3123f48e8e6ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:22 +0000

    Add package smarter/0.1.3/smarter-0.1.3.tar.gz

---
+++ packages/smarter/smarter-0.1.3.tar.gz/smarter-0.1.3/setup.py
@@ -0,0 +1,27 @@
+"""Python setup.py for smarter package"""
+from pathlib import Path
+from setuptools import find_packages, setup
+
+setup(
+    name="smarter",
+    version="0.1.3",
+    description="Some objects could be smarter",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    author="Bruno Rocha",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[],
+    extras_require={
+        "test": [
+            "pytest",
+            "coverage",
+            "flake8",
+            "black",
+            "isort",
+            "pytest-cov",
+            "codecov",
+            "mypy",
+            "gitchangelog",
+        ],
+    },
+)
