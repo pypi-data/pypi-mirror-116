b6d0395e58df9d78a0cde0e320c74bd6c5b05d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:37 +0000

    Add package cbdevtools/0.0.3b0/cbdevtools-0.0.3b0.tar.gz

---
+++ packages/cbdevtools/cbdevtools-0.0.3b0.tar.gz/cbdevtools-0.0.3b0/cbdevtools/orpystedata.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+from pytest  import fixture
+
+from orpyste.data import ReadBlock
+
+
+THE_DATAS_FOR_TESTING = None
+
+def build_datas_block(
+    file: str,
+) -> None:
+    global THE_DATAS_FOR_TESTING
+
+    thisdir = Path(__file__).parent
+
+    whatistested = Path(__file__).stem
+    whatistested = whatistested.replace('test_', '')
+
+    THE_DATAS_FOR_TESTING = ReadBlock(
+        content = thisdir / f'{whatistested}.peuf',
+        mode    = {"keyval:: =": ":default:"}
+    )
+
+@fixture(scope = "module")
+def orpyste_fix_block(request):
+    THE_DATAS_FOR_TESTING.build()
+
+    def remove_extras():
+        THE_DATAS_FOR_TESTING.remove_extras()
+
+    request.addfinalizer(remove_extras)
