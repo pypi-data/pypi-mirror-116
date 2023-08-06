1be293f7d836773272baac35d5a3d81d83071fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:39 +0000

    Add package hisyamcalc/0.0.1/hisyamcalc-0.0.1.tar.gz

---
+++ packages/hisyamcalc/hisyamcalc-0.0.1.tar.gz/hisyamcalc-0.0.1/hisyamcalc/__init__.py
@@ -0,0 +1,13 @@
+def penjumlahan(num1, num2):
+    return num1 + num2
+
+def pengurangan(num1, num2):
+    return num1 - num2
+
+def perkalian(num1, num2):
+    return num1 * num1
+
+def pembagian(num1, num2):
+    return num1 / num2
+
+print(penjumlahan(10, 5))
