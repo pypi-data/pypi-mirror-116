3d64f3440099fc62f124f20716f26fab02aa6414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:07 +0000

    Add package perke/0.3.4/perke-0.3.4.tar.gz

---
+++ packages/perke/perke-0.3.4.tar.gz/perke-0.3.4/perke/utils/functions.py
@@ -0,0 +1,21 @@
+def is_alphanumeric(word: str, valid_punctuation_marks: str = '-') -> bool:
+    """
+    Check if a word contains only alpha-numeric
+    characters and valid punctuation marks.
+
+    Parameters
+    ----------
+    word: `str`
+        The given word
+
+    valid_punctuation_marks: `str`
+        Punctuation marks that are valid, defaults to `'-'`.
+
+    Returns
+    -------
+    result: `bool`
+        The result
+    """
+    for punctuation_mark in valid_punctuation_marks.split():
+        word = word.replace(punctuation_mark, '')
+    return word.isalnum()
