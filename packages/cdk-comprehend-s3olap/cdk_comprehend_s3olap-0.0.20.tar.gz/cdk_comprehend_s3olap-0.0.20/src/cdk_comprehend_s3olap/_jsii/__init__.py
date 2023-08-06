63ecbf9f38023d6d90a1a6959d93d13920a91718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:00 +0000

    Add package cdk-comprehend-s3olap/0.0.20/cdk_comprehend_s3olap-0.0.20.tar.gz

---
+++ packages/cdk-comprehend-s3olap/cdk_comprehend_s3olap-0.0.20.tar.gz/cdk_comprehend_s3olap-0.0.20/src/cdk_comprehend_s3olap/_jsii/__init__.py
@@ -0,0 +1,34 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_lambda_nodejs._jsii
+import aws_cdk.aws_logs._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.aws_s3_deployment._jsii
+import aws_cdk.aws_s3objectlambda._jsii
+import aws_cdk.aws_sam._jsii
+import aws_cdk.core._jsii
+import aws_cdk.custom_resources._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-comprehend-s3olap",
+    "0.0.20",
+    __name__[0:-6],
+    "cdk-comprehend-s3olap@0.0.20.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
