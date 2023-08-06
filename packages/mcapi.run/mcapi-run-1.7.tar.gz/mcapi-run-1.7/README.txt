54245e6a3b47c071175705f19dc9282354397228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:11 +0000

    Add package mcapi.run/1.7/mcapi-run-1.7.tar.gz

---
+++ packages/mcapi.run/mcapi-run-1.7.tar.gz/mcapi-run-1.7/README.txt
@@ -0,0 +1,1 @@
+a simple minecraft api for python!
---
+++ packages/mcapi.run/mcapi-run-1.7.tar.gz/mcapi-run-1.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Operating System :: OS Independent',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='mcapi-run',
+  version='1.7',
+  scripts=['mcapi-help.py'],
+  description='simple minecraft api made for the api.mcapi.run website || by overnice.exe',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Overnice.exe',
+  author_email='silkepilon2009@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='minecraft', 
+  packages=find_packages(),
+  install_requires=['requests', 'colorama'] 
+)
