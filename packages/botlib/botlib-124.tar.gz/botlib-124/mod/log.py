0648fc09add8baf2604fd842fa497bc723c51493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:34 +0000

    Add package botlib/124/botlib-124.tar.gz

---
+++ packages/botlib/botlib-124.tar.gz/botlib-124/mod/log.py
@@ -0,0 +1,21 @@
+# This file is in the Public Domain.
+
+"enter txt"
+
+from ob.obj import Object
+
+
+class Log(Object):
+    def __init__(self):
+        super().__init__()
+        self.txt = ""
+
+
+def log(event):
+    if not event.rest:
+        event.reply("log <txt>")
+        return
+    o = Log()
+    o.txt = event.rest
+    o.save()
+    event.reply("ok")
