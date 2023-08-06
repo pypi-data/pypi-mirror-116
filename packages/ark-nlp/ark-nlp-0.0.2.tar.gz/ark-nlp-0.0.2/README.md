90e073e40425e86357daa8d9c75eea471427aa9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:35 +0000

    Add package ark-nlp/0.0.2/ark-nlp-0.0.2.tar.gz

---
+++ packages/ark-nlp/ark-nlp-0.0.2.tar.gz/ark-nlp-0.0.2/README.md
@@ -0,0 +1,9 @@
+### A Quick Start
+
+To install the latest version of ark_utils run:
+
+
+```
+pip install ark-nlp
+```
+
---
+++ packages/ark-nlp/ark-nlp-0.0.2.tar.gz/ark-nlp-0.0.2/ark_nlp/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '0.0.2'
+__author__ = 'Xiang Wang'
+
+# List maintainers here
+__maintainers__ = [
+    'Xiang Wang',
+    'ChenJie Shen'
+]
+
+__all__ = [
+]
