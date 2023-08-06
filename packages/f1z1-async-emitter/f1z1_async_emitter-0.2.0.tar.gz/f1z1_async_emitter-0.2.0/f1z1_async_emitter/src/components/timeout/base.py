5f9917d8d9aab8e0370b5755f75ed588c39faecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:20 +0000

    Add package f1z1-async-emitter/0.2.0/f1z1_async_emitter-0.2.0.tar.gz

---
+++ packages/f1z1-async-emitter/f1z1_async_emitter-0.2.0.tar.gz/f1z1_async_emitter-0.2.0/f1z1_async_emitter/src/components/timeout/base.py
@@ -0,0 +1,19 @@
+# @Time     : 2021/7/24
+# @Project  : f1z1-g
+# @IDE      : PyCharm
+# @Author   : Angel
+# @Email    : 376355670@qq.com
+from abc import ABCMeta, abstractmethod
+
+
+class ITimeout(metaclass=ABCMeta):
+    __slots__ = "_st", "_timeout", "_timestamp"
+
+    @abstractmethod
+    def start(self) -> None:
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def is_timeout(self) -> bool:
+        raise NotImplementedError()
