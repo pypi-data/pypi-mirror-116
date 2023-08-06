99ce0697a0a74210a583a4d7b68a5ecf43a9a7dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:06 +0000

    Add package polars/0.8.23/polars-0.8.23.tar.gz

---
+++ packages/polars/polars-0.8.23.tar.gz/polars-0.8.23/tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+
+import polars as pl
+
+
+@pytest.fixture
+def df() -> pl.DataFrame:
+    return pl.DataFrame(
+        {
+            "bools": [False, True, False],
+            "bools_nulls": [None, True, False],
+            "int": [1, 2, 3],
+            "int_nulls": [1, None, 3],
+            "floats": [1.0, 2.0, 3.0],
+            "floats_nulls": [1.0, None, 3.0],
+            "strings": ["foo", "bar", "ham"],
+            "strings_nulls": ["foo", None, "ham"],
+        }
+    )
