8d0ddd061385e9883cc9fd290539b5bb4df5dd14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:38 +0000

    Add package coi/0.0.2/coi-0.0.2.tar.gz

---
+++ packages/coi/coi-0.0.2.tar.gz/coi-0.0.2/coi/utils.py
@@ -0,0 +1,25 @@
+from string import Template
+from pathlib import Path
+from argparse import Namespace
+
+
+def get_template_path(path=None) -> Path:
+    """
+
+    """
+    coi_path = Path(__file__).parents[1]
+    return coi_path / 'templates'
+
+
+# val = subprocess.check_call("./script.sh '%s'" % arg, shell=True)
+# subprocess.run()
+
+def get_cmd(args: Namespace) -> str:
+    """
+
+    """
+    # load template string
+    fname = get_template_path() / 'c-on-i'
+    with open(fname, "r") as f:
+        templ = Template(f.read())
+    return templ.substitute(vars(args))
