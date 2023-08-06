4f3d8ffc5277dbd68e992efb0f41821c61da86b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:55 +0000

    Add package upswingutil/1.5.3/upswingutil-1.5.3-py3-none-any.whl

---
+++ packages/upswingutil/upswingutil-1.5.3-py3-none-any.whl/upswingutil/db/firestoredb.py
@@ -0,0 +1,12 @@
+from firebase_admin import firestore, get_app
+
+
+class Firestore:
+    org_collection = 'Organizations'
+    sub_collection_properties = 'properties'
+
+    def __init__(self, app=None):
+        self.firestore_db = firestore.client(app=get_app(app)) if app else firestore.client()
+
+    def get_collection(self, name):
+        return self.firestore_db.collection(name)
