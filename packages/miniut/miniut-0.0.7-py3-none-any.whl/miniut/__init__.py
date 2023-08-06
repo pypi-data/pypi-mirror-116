9f8d416be808287bb49f975d0a558931318db9e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:46 +0000

    Add package miniut/0.0.7/miniut-0.0.7-py3-none-any.whl

---
+++ packages/miniut/miniut-0.0.7-py3-none-any.whl/miniut/__init__.py
@@ -0,0 +1,14 @@
+from miniut.decorators import (
+    singleton,
+    ttime
+)
+
+from miniut.metaclasses import (
+    SingletonMeta
+)
+
+from miniut.config import (
+    define_lang,
+    all_langs,
+    lang
+)
