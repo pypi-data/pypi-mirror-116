d769308aa5518e069dc04970079d257758b71ecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:24 +0000

    Add package zwcef/0.0.1/zwcef-0.0.1-py3-none-any.whl

---
+++ packages/zwcef/zwcef-0.0.1-py3-none-any.whl/zwcef/__version__.py
@@ -0,0 +1,8 @@
+__version__ = '0.0.1'
+__title__ = 'zwcef'
+__description__ = 'Person CEF spider'
+__url__ = 'https://github.com/njzhaowei/%s'%__title__
+__author__ = 'Zhao Wei'
+__author_email__ = 'yewberry@163.com'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2021 Zhao Wei'
