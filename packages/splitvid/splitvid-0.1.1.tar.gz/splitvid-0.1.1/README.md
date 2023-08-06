3302c81fd8c7df5eb7ae24dda370f36a3363e726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:29 +0000

    Add package splitvid/0.1.1/splitvid-0.1.1.tar.gz

---
+++ packages/splitvid/splitvid-0.1.1.tar.gz/splitvid-0.1.1/README.md
@@ -0,0 +1,24 @@
+# splitvid
+
+![splitvid.png](https://raw.githubusercontent.com/4thel00z/logos/master/splitvid.png)
+
+## Motivation
+
+I need a tool which can split my vids easily, so I can upload them to my story or whatever.
+
+## Installation
+
+```
+pip install splitvid
+```
+
+## Usage
+
+```
+>>> from splitvid import splitvid_into as split_into
+>>> split_into("parents.mkv", "parts", 10)
+```
+
+## License
+
+This project is licensed under the GPL-3 license.
---
+++ packages/splitvid/splitvid-0.1.1.tar.gz/splitvid-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['splitvid']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['plumbum>=1.7.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'splitvid',
+    'version': '0.1.1',
+    'description': 'Split videos with ffmpeg for a certain period',
+    'long_description': '# splitvid\n\n![splitvid.png](https://raw.githubusercontent.com/4thel00z/logos/master/splitvid.png)\n\n## Motivation\n\nI need a tool which can split my vids easily, so I can upload them to my story or whatever.\n\n## Installation\n\n```\npip install splitvid\n```\n\n## Usage\n\n```\n>>> from splitvid import splitvid_into as split_into\n>>> split_into("parents.mkv", "parts", 10)\n```\n\n## License\n\nThis project is licensed under the GPL-3 license.\n',
+    'author': '4thel00z',
+    'author_email': '4thel00z@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/4thel00z/splitvid',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
