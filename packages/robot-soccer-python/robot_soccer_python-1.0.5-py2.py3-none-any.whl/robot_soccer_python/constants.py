a4e2b941fb4f4032ec06fafb113ce58bcc7c67e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:52 +0000

    Add package robot-soccer-python/1.0.5/robot_soccer_python-1.0.5-py2.py3-none-any.whl

---
+++ packages/robot-soccer-python/robot_soccer_python-1.0.5-py2.py3-none-any.whl/robot_soccer_python/constants.py
@@ -0,0 +1,37 @@
+from math import pi
+# Simulation Parameters
+# soccer field (105,68) x 6
+SCREEN_WIDTH = 1000 
+SCREEN_HEIGHT = 650 
+PIX2M = 0.01  # factor to convert from pixels to meters
+M2PIX = 100.0  # factor to convert from meters to pixels
+
+# Sample Time Parameters
+FREQUENCY = 60.0  # simulation frequency
+SAMPLE_TIME = 1.0 / FREQUENCY  # simulation sample time
+
+# world Parameters
+GRAVITY_ACCLERATION = 9.8
+
+# Ball Parameters
+FRICTION_SLOWDOWN = 0.5
+REFLECTION = pi
+BALL_MASS = 0.450
+RADIUS_BALL = 0.05
+FACTOR_FRICTION = GRAVITY_ACCLERATION*FRICTION_SLOWDOWN*SAMPLE_TIME/100
+
+# players Parameters
+PLAYER_MASS = 75
+BACK_SPEED_COLISION = -0.1
+
+# Vision Parameters
+RADIAN_TO_DEGREE = 180 / pi
+
+# colors
+RED_COLOR = (255,0,0)
+GRAY_COLOR = (50, 50, 50)
+YELLOW_COLOR = (238, 203, 0)
+BLACK_COLOR = (0, 0, 0)
+WHITE_COLOR = (255,255,255)
+
+
