44f919e952d6c0abc1f1afa5a0ab7fa9629a9457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:41 +0000

    Add package pyforbes/1.0.0/PyForbes-1.0.0-py3-none-any.whl

---
+++ packages/pyforbes/PyForbes-1.0.0-py3-none-any.whl/pyforbes/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+Python ForbesLists Data Collection Package
+"""
+from pyforbes.lists import ForbesList
+
+__version__ = "0.0.1"
+__author__ = "laxmena"
+
+__all__ = ["ForbesList"]
