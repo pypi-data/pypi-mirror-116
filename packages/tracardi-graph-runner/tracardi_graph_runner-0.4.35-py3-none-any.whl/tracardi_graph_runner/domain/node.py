e2e527a3d6858f708c1d9d6d5eadbbb92bb2828b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:32 +0000

    Add package tracardi-graph-runner/0.4.35/tracardi_graph_runner-0.4.35-py3-none-any.whl

---
+++ packages/tracardi-graph-runner/tracardi_graph_runner-0.4.35-py3-none-any.whl/tracardi_graph_runner/domain/node.py
@@ -0,0 +1,27 @@
+from typing import List, Optional
+from pydantic import BaseModel
+from tracardi_plugin_sdk.action_runner import ActionRunner
+
+from .port_to_port_edges import PortToPortEdges
+
+
+class Graph(BaseModel):
+    in_edges: PortToPortEdges = PortToPortEdges()
+    out_edges: PortToPortEdges = PortToPortEdges()
+
+
+class Node(BaseModel):
+    id: str
+    start: Optional[bool] = False
+    debug: bool = False
+    inputs: Optional[List[str]] = []
+    outputs: Optional[List[str]] = []
+    className: str
+    module: str
+    init: Optional[dict] = {}
+    object: Optional[ActionRunner] = None
+    graph: Graph = Graph()
+
+    class Config:
+        arbitrary_types_allowed = True
+
