738fd0054e286822708753eabc73c8330c231de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:51 +0000

    Add package data-structures-nicorodc/0.2.3/data_structures_nicorodc-0.2.3-py3-none-any.whl

---
+++ packages/data-structures-nicorodc/data_structures_nicorodc-0.2.3-py3-none-any.whl/data_structures/__init__.py
@@ -0,0 +1,21 @@
+"""Some useful data structures for problem solving.
+
+Modules
+-------
+
+elements:
+    Basic elements to use in data structures.
+
+    Classes
+    -------
+    Number:
+        Immutable real numbers.
+
+stacks:
+    Stack-like data structures.
+
+    Classes
+    -------
+    Stack:
+        Basic stack data structure.
+"""
