ea89c256102ea9a28dc5fd411771ec86cffbb3b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:31 +0000

    Add package hectordistributions/0.2/hectordistributions-0.2.tar.gz

---
+++ packages/hectordistributions/hectordistributions-0.2.tar.gz/hectordistributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='hectordistributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['hectordistributions'],
+      zip_safe=False)
