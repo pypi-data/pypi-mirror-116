8d6547d7b4eed13d82ad0a909523ce2ce71dad99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:51 +0000

    Add package yarp-middleware/3.5.1.dev83/yarp_middleware-3.5.1.dev83-cp37-cp37m-manylinux_2_24_x86_64.whl

---
+++ packages/yarp-middleware/yarp_middleware-3.5.1.dev83-cp37-cp37m-manylinux_2_24_x86_64.whl/yarp/lib/cmake/YARP_priv_tinyxml/YARP_priv_tinyxmlConfig.cmake
@@ -0,0 +1,18 @@
+set(YARP_priv_tinyxml_VERSION 3.5.0+83-20210819.17+git2d989362b)
+
+
+####### Expanded from @PACKAGE_INIT@ by configure_package_config_file() #######
+####### Any changes to this file will be overwritten by the next CMake run ####
+####### The input file was YARP_priv_tinyxmlConfig.cmake.in                            ########
+
+get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../" ABSOLUTE)
+
+####################################################################################
+
+
+
+include("${CMAKE_CURRENT_LIST_DIR}/YARP_priv_tinyxmlTargets.cmake")
+
+
+
+
