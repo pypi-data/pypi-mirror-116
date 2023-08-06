486a868e681c3d214f65d6aa3b0bd88ebf76cbeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:47 +0000

    Add package pyi2c/0.0.4/pyi2c-0.0.4.tar.gz

---
+++ packages/pyi2c/pyi2c-0.0.4.tar.gz/pyi2c-0.0.4/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyi2c",
+    version="0.0.4",
+    author="Eunchong Kim",
+    author_email="chariskimec@gmail.com",
+    description="A useful i2c package for Pi",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/newini/pyi2c",
+    project_urls={
+        "Bug Tracker": "https://github.com/newini/pyi2c/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=["smbus2"],
+)
