2720263f701b388711d4609c81dc6a0db56addb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:32 +0000

    Add package telegaden/0.0.2/telegaden-0.0.2.tar.gz

---
+++ packages/telegaden/telegaden-0.0.2.tar.gz/telegaden-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='telegaden',
+    version='0.0.2',
+    packages=find_packages(),
+    url='https://github.com/Yakuden/telega',
+    license='MIT',
+    author='Yakuden',
+    author_email='yakudenn@gmail.com',
+    keywords='telegram telega client tglib',
+    description='Python Telegram TDLib sync client',
+    package_data={
+        'telega': ['td_lib/linux/libtdjson.so', 'td_lib/linux/libtdjson.so.1.4.0'],
+    },
+    python_requires=">=3.5",
+    install_requires=[]
+)
