92464fc3eec2f7ef06c374fe0033aabd7064ca41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:21 +0000

    Add package minos-apigateway/0.0.3/minos_apigateway-0.0.3-py3-none-any.whl

---
+++ packages/minos-apigateway/minos_apigateway-0.0.3-py3-none-any.whl/minos/api_gateway/rest/service.py
@@ -0,0 +1,33 @@
+"""minos.api_gateway.rest.service module."""
+
+import logging
+
+from aiohttp import (
+    web,
+)
+from aiomisc.service.aiohttp import (
+    AIOHTTPService,
+)
+
+from minos.api_gateway.common import (
+    MinosConfig,
+)
+from minos.api_gateway.rest import (
+    handler,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class ApiGatewayRestService(AIOHTTPService):
+    def __init__(self, address: str, port: int, config: MinosConfig):
+        self.config = config
+        super().__init__(address, port)
+
+    async def create_application(self) -> web.Application:
+        app = web.Application()
+        app["config"] = self.config
+
+        app.router.add_route("*", "/{endpoint:.*}", handler.orchestrate)
+
+        return app
