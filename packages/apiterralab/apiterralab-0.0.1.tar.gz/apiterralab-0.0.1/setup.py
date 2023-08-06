a4c8446ea2cea07038fed89129a182edfc97c74a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:23 +0000

    Add package apiterralab/0.0.1/apiterralab-0.0.1.tar.gz

---
+++ packages/apiterralab/apiterralab-0.0.1.tar.gz/apiterralab-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='apiterralab',
+   version='0.0.1',
+   description='breve descrição',
+   author='TerraLab',
+   author_email='terralab@gmail.com',
+   packages=['apiterralab'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
