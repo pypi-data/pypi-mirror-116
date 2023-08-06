94a4d63378ba65dfcaf24c60e44f541642d6a3fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:42 +0000

    Add package tuibox/1.0/tuibox-1.0.tar.gz

---
+++ packages/tuibox/tuibox-1.0.tar.gz/tuibox-1.0/README.md
@@ -0,0 +1,4 @@
+*TUIBOX*
+
+'A TUI based contact storage prototype(using ram section storage)'
+
---
+++ packages/tuibox/tuibox-1.0.tar.gz/tuibox-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup (
+    name = 'tuibox',
+    version = '1.0',
+    author = 'hlaxman',
+    author_email = "harshu8310@gmail.com",
+    packages = find_packages(),
+    )
