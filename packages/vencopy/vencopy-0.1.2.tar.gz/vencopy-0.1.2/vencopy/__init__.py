272623bbd549efdd60bfa42b0d034aa574201ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:19 +0000

    Add package vencopy/0.1.2/vencopy-0.1.2.tar.gz

---
+++ packages/vencopy/vencopy-0.1.2.tar.gz/vencopy-0.1.2/vencopy/__init__.py
@@ -0,0 +1,9 @@
+"""
+VencoPy.
+
+Vehicle Energy Consumption in Python. Electric vehicle fleet load shifting simulations.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Niklas Wulff'
+__credits__ = 'German Aerospace Center (DLR)'
