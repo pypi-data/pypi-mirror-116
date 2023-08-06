ef46793f343898d8836b52ff148c6c1cac4b2320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:33 +0000

    Add package deepedgeai/0.0.0/deepedgeai-0.0.0.tar.gz

---
+++ packages/deepedgeai/deepedgeai-0.0.0.tar.gz/deepedgeai-0.0.0/deepedgeai/about.py
@@ -0,0 +1,6 @@
+class About:
+    def __init__(self) -> None:
+        print("DeepEdge has emerged as world’s leading full-stack EdgeAI company,"
+         + "providing a broad range of platforms and services, enabling companies to rapidly" +
+        + "build and deploy products with EdgeAI capabilities.  ")
+        print("contact: projects@deepedge.ai")
---
+++ packages/deepedgeai/deepedgeai-0.0.0.tar.gz/deepedgeai-0.0.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name="deepedgeai",
+      version="0.0.0",
+      description="A DeepEdge.ai Projects",
+      packages=["deepedgeai"],
+      zip_safe=False)
