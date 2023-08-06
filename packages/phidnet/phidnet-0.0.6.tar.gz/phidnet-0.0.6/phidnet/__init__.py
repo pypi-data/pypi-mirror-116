5d4f6e4e9ef963ff98f21e347a7248d05f2f6e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:41 +0000

    Add package phidnet/0.0.6/phidnet-0.0.6.tar.gz

---
+++ packages/phidnet/phidnet-0.0.6.tar.gz/phidnet-0.0.6/phidnet/__init__.py
@@ -0,0 +1,19 @@
+__all__ = ['activation',
+           'error',
+           'model',
+           'network_data',
+           'one_hot_encode',
+           'set',
+           'feedforward',
+           'loss',
+           'gradient',
+           'optimizer',
+           'load',
+           'CNN',
+           'RNN',
+           'datasets']
+
+from phidnet import *
+from phidnet.matrix import matrix
+from phidnet.matrix.matrix import array
+from phidnet.matrix.matrix_class import Matrix
---
+++ packages/phidnet/phidnet-0.0.6.tar.gz/phidnet-0.0.6/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+
+with open("README.md", "r", encoding='UTF-8') as fh:
+    long_description = fh.read()
+
+
+
+setuptools.setup(
+    name="phidnet",
+    version="0.0.6",
+    author="Intipy",
+    author_email="jios6790@gmail.com",
+    description="Phidnet",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Intipy/phidnet.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
