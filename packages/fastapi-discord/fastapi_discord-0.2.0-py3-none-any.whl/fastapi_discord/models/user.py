110f1ffa48139ff597eb94662e2ffc6045e16d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:56 +0000

    Add package fastapi-discord/0.2.0/fastapi_discord-0.2.0-py3-none-any.whl

---
+++ packages/fastapi-discord/fastapi_discord-0.2.0-py3-none-any.whl/fastapi_discord/models/user.py
@@ -0,0 +1,22 @@
+from typing import Any, Optional
+
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    id: str
+    username: str
+    discriminator: str
+    avatar: str
+    avatar_url: Optional[str]
+    locale: str
+    email: Optional[str]
+    bot: Optional[bool]
+    mfa_enabled: bool
+    flags: int
+    premium_type: Optional[int]
+    public_flags: int
+
+    def __init__(self, **data: Any):
+        super().__init__(**data)
+        self.avatar_url = f"https://cdn.discordapp.com/avatars/{self.id}/{self.avatar}.png"
