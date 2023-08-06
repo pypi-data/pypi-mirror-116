646fc0c6d6765bc24122f7698cba251a22457a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:34 +0000

    Add package jsonrpcserver/5.0.0/jsonrpcserver-5.0.0.tar.gz

---
+++ packages/jsonrpcserver/jsonrpcserver-5.0.0.tar.gz/jsonrpcserver-5.0.0/README.md
@@ -0,0 +1,24 @@
+> :warning: **Note: This master branch is for development of the upcoming version 5**, which is currently in ~alpha~ ~beta~ [release candidate!](https://github.com/explodinglabs/jsonrpcserver/discussions/204) For the latest stable version, see the [4.x branch](https://github.com/explodinglabs/jsonrpcserver/tree/4.x). Also, please pin your dependency to "jsonrpcserver<5" until you're ready to upgrade to v5.
+
+# jsonrpcserver
+
+Process [JSON-RPC](http://www.jsonrpc.org/) requests in Python.
+
+![PyPI](https://img.shields.io/pypi/v/jsonrpcserver.svg)
+![Downloads](https://pepy.tech/badge/jsonrpcserver)
+![Coverage Status](https://coveralls.io/repos/github/explodinglabs/jsonrpcserver/badge.svg?branch=master)
+
+```python
+from jsonrpcserver import Success, method, serve
+
+@method
+def ping():
+    return Success("pong")
+
+if __name__ == "__main__":
+    serve()
+```
+
+Full documentation is at [jsonrpcserver.com](https://www.jsonrpcserver.com/).
+
+See also: [jsonrpcclient](https://github.com/explodinglabs/jsonrpcclient)
---
+++ packages/jsonrpcserver/jsonrpcserver-5.0.0.tar.gz/jsonrpcserver-5.0.0/setup.py
@@ -0,0 +1,43 @@
+"""setup.py"""
+from codecs import open as codecs_open
+from setuptools import setup  # type: ignore
+
+with codecs_open("README.md", "r", "utf-8") as f:
+    README = f.read()
+
+setup(
+    author="Beau Barker",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    description="Process JSON-RPC requests",
+    extras_require={
+        "tox": ["tox"],
+        "examples": [
+            "aiohttp",
+            "aiozmq",
+            "flask",
+            "flask-socketio",
+            "gmqtt",
+            "pyzmq",
+            "tornado",
+            "websockets",
+            "werkzeug",
+        ],
+    },
+    include_package_data=True,
+    install_requires=["jsonschema<4", "oslash<1"],
+    license="MIT",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    name="jsonrpcserver",
+    # Be PEP 561 compliant
+    # https://mypy.readthedocs.io/en/stable/installed_packages.html#making-pep-561-compatible-packages
+    package_data={"jsonrpcserver": ["request-schema.json", "py.typed"]},
+    zip_safe=False,
+    packages=["jsonrpcserver"],
+    url="https://github.com/explodinglabs/jsonrpcserver",
+    version="5.0.0",
+)
