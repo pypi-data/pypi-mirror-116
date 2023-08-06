e980afb1ff523f5a6ebb41cfe04decab58f2b974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:50 +0000

    Add package antelope-foreground/0.1.7/antelope_foreground-0.1.7.tar.gz

---
+++ packages/antelope-foreground/antelope_foreground-0.1.7.tar.gz/antelope_foreground-0.1.7/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'antelope_core>=0.1.7'
+]
+
+"""
+Revision history
+
+0.1.7 - 11 Aug 2021 - TRAVIS release
+
+0.1.3 - 30 Dec 2020 - First public release
+"""
+
+setup(
+    name="antelope_foreground",
+    version="0.1.7",
+    author="Brandon Kuczenski",
+    author_email="bkuczenski@ucsb.edu",
+    install_requires=requires,
+    url="https://github.com/AntelopeLCA/foreground",
+    summary="A foreground model building implementation",
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering"
+    ],
+    python_requires='>=3.6',
+    packages=find_packages()
+)
