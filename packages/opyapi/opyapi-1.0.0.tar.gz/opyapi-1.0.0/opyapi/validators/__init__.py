9111ec5bd140915a4b1e229fc90e978aa268aa86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:31 +0000

    Add package opyapi/1.0.0/opyapi-1.0.0.tar.gz

---
+++ packages/opyapi/opyapi-1.0.0.tar.gz/opyapi-1.0.0/opyapi/validators/__init__.py
@@ -0,0 +1,95 @@
+from typing import Any, List, Union, Callable
+
+from opyapi.errors import EqualityValidationError, TypeValidationError, EnumValidationError
+from .array_validators import (
+    validate_maximum_items,
+    validate_minimum_items,
+    validate_array,
+    validate_tuple,
+)
+from .combining_validators import (
+    validate_all_of,
+    validate_any_of,
+    validate_not,
+    validate_one_of,
+)
+from .number_validators import (
+    validate_exclusive_maximum,
+    validate_exclusive_minimum,
+    validate_maximum,
+    validate_minimum,
+    validate_multiple_of,
+    validate_number,
+    validate_integer,
+)
+from .object_validators import validate_object
+
+from .string_validators import (
+    validate_maximum_length,
+    validate_minimum_length,
+    validate_string_format,
+    validate_string_pattern,
+    validate_string,
+)
+
+
+def validate_equal(value: Any, expected: Any) -> Any:
+    if value == expected:
+        return value
+
+    raise EqualityValidationError(passed_value=value, expected_value=value)
+
+
+def validate_boolean(value: Any) -> bool:
+    if value is True or value is False:
+        return value
+
+    raise TypeValidationError(expected_type=bool, actual_type=type(value))
+
+
+def validate_enum(value: Any, values: List[Union[str, int, float, bool]]) -> Union[str, int, float, bool]:
+    for item in values:  # `if value in values` expression does casting and we dont want it
+        if isinstance(value, bool):
+            if item is value:
+                return value
+        else:
+            if item == value:
+                return value
+
+    raise EnumValidationError(expected_values=values)
+
+
+def validate_nullable(value: Any, validator: Callable) -> Any:
+    if value is None:
+        return None
+
+    return validator(value)
+
+
+__all__ = [
+    "validate_array",
+    "validate_tuple",
+    "validate_boolean",
+    "validate_enum",
+    "validate_equal",
+    "validate_nullable",
+    "validate_number",
+    "validate_integer",
+    "validate_string",
+    "validate_maximum_items",
+    "validate_minimum_items",
+    "validate_minimum_length",
+    "validate_maximum_length",
+    "validate_string_format",
+    "validate_string_pattern",
+    "validate_exclusive_maximum",
+    "validate_exclusive_minimum",
+    "validate_maximum",
+    "validate_minimum",
+    "validate_multiple_of",
+    "validate_any_of",
+    "validate_all_of",
+    "validate_not",
+    "validate_one_of",
+    "validate_object",
+]
---
+++ packages/opyapi/opyapi-1.0.0.tar.gz/opyapi-1.0.0/opyapi/validators/array_validators.py
@@ -0,0 +1,85 @@
+from typing import Callable, List, Any
+
+from opyapi.errors import (
+    AdditionalItemsValidationError,
+    MaximumItemsValidationError,
+    MinimumItemsValidationError,
+    UniqueItemsValidationError,
+    TypeValidationError,
+)
+
+
+def validate_minimum_items(value: list, expected_minimum: int) -> list:
+    if len(value) >= expected_minimum:
+        return value
+
+    raise MinimumItemsValidationError(expected_minimum=expected_minimum)
+
+
+def validate_maximum_items(value: list, expected_maximum: int) -> list:
+    if len(value) <= expected_maximum:
+        return value
+
+    raise MaximumItemsValidationError(expected_maximum=expected_maximum)
+
+
+def validate_tuple(value: list, item_validator: List[Callable], additional_items: Callable = None) -> list:
+    if not isinstance(value, list):
+        raise TypeValidationError(expected_type="array", actual_type=type(value))
+
+    list_length = len(value)
+    validators_length = len(item_validator)
+
+    if list_length > validators_length and additional_items is None:
+        raise AdditionalItemsValidationError()
+
+    for i in range(0, list_length):
+        if i < validators_length:
+            value[i] = item_validator[i](value[i])
+            continue
+
+        value[i] = additional_items(value[i])  # type: ignore
+
+    return value
+
+
+def validate_array(
+    value: list,
+    item_validator: Callable = None,
+    minimum_items: int = -1,
+    maximum_items: int = -1,
+    unique_items: bool = False,
+) -> list:
+    if not isinstance(value, list):
+        raise TypeValidationError(expected_type="array", actual_type=type(value))
+
+    result = []
+    if item_validator:
+        for item in value:
+            result.append(item_validator(item))
+    else:
+        result = value
+
+    # python fails to check in sets against bool and integers so we have to run this in two loops
+    if unique_items:
+        seen: List[Any] = []
+        for item in result:
+            for other in seen:
+                if item is other:
+                    raise UniqueItemsValidationError()
+            seen.append(item)
+
+    if minimum_items > -1:
+        validate_minimum_items(result, minimum_items)
+    if maximum_items > -1:
+        validate_maximum_items(result, maximum_items)
+
+    return result
+
+
+__all__ = [
+    "validate_maximum_items",
+    "validate_minimum_items",
+    "validate_tuple",
+    "validate_array",
+]
