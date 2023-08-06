3aa9f11f6d40e506e03a2d476c08870ad75918e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:36 +0000

    Add package je-auto-control/0.0.30/je_auto_control-0.0.30.tar.gz

---
+++ packages/je-auto-control/je_auto_control-0.0.30.tar.gz/je_auto_control-0.0.30/je_auto_control/__init__.py
@@ -0,0 +1,4 @@
+from je_auto_control.wrapper.auto_control_keyboard import *
+from je_auto_control.wrapper.auto_control_mouse import *
+from je_auto_control.wrapper.auto_control_screen import *
+from je_auto_control.wrapper.auto_control_image import *
