b6dd5574b0d635aa074c3a31082a0c5a1454d51d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:12 +0000

    Add package libbiomedit/0.4.1/libbiomedit-0.4.1.tar.gz

---
+++ packages/libbiomedit/libbiomedit-0.4.1.tar.gz/libbiomedit-0.4.1/libbiomedit/__init__.py
@@ -0,0 +1,10 @@
+from typing import Optional, TypeVar
+
+
+T = TypeVar("T")
+
+
+def unwrap(opt: Optional[T]) -> T:
+    if opt is None:
+        raise ValueError("Should not be None")
+    return opt
