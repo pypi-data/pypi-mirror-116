b305aa134c16e2f9e88ca1fd680c42bad6e138a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:31 +0000

    Add package markdown-up/1.2.5/markdown-up-1.2.5.tar.gz

---
+++ packages/markdown-up/markdown-up-1.2.5.tar.gz/markdown-up-1.2.5/src/markdown_up/__main__.py
@@ -0,0 +1,8 @@
+# Licensed under the MIT License
+# https://github.com/craigahobbs/markdown-up-py/blob/main/LICENSE
+
+from .main import main
+
+
+if __name__ == '__main__': # pragma: no cover
+    main()
