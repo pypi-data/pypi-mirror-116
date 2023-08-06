f50b7ddf2a1332ba4c8a20ca2e04a8641c25ab80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:17 +0000

    Add package dez-probability/0.1/dez-probability-0.1.tar.gz

---
+++ packages/dez-probability/dez-probability-0.1.tar.gz/dez-probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dez-probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dez_probability'],
+      author='Jacob Sherwood',
+      zip_safe=False)
