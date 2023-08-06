7d5978398765592c9b8e1deec8a5e6641d9808af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:45 +0000

    Add package finformula/0.0.1/finformula-0.0.1.tar.gz

---
+++ packages/finformula/finformula-0.0.1.tar.gz/finformula-0.0.1/setup.py
@@ -0,0 +1,19 @@
+# -*-coding:utf-8-*-
+import setuptools
+
+setuptools.setup(
+    name='finformula',
+    version='0.0.1',
+    author='Ding',
+    author_email='dingxuechun@hotmail.com',
+    description='Testing installation of Package',
+    url='https://git03.hundsun.com/AIQA/aiqa-ie/tree/dev/finformula',
+    license='MIT',
+    packages=['finformula'],
+    install_requires=
+    ['numpy==1.16.6', 'pandas==1.1.5',
+     'scipy==1.5.4', 'pymssql==2.1.4',
+     'sqlalchemy==1.3.18', 'datetime', 'dateutil'
+     ],
+)
+
