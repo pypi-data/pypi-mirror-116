f1da63627030dbbc9976f384392bc05e09ec04d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:54 +0000

    Add package boa-web/0.0.3/boa_web-0.0.3.tar.gz

---
+++ packages/boa-web/boa_web-0.0.3.tar.gz/boa_web-0.0.3/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+import setuptools
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+# The text of the README file
+README = (HERE / "README.md").read_text()
+# save requirements in a list 
+REQUIREMENTS = open('requirements.txt').read().splitlines()
+# This call to setup() does all the work
+setup(
+    name="boa_web",
+    version="0.0.3",
+    description="boa_web is electron.js for python",
+    keywords='flask, electron, eel, cefpython, html, css, javascript',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/atharva-naik/boa_web",
+    author="Atharva Naik",
+    author_email="atharvanaik2018@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    package_dir={'boa_web':'boa_web'},
+    packages=setuptools.find_packages(where='boa_web'),
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+)
