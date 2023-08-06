89a059e6495432740956e0a4453890bdc9b4f106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:20 +0000

    Add package sbbbxl-distrib/0.1/sbbbxl_distrib-0.1.tar.gz

---
+++ packages/sbbbxl-distrib/sbbbxl_distrib-0.1.tar.gz/sbbbxl_distrib-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='sbbbxl_distrib',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['sbbbxl_distrib'],
+      author = 'Saiphan Bruner',
+      zip_safe=False)
