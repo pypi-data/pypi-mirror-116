e6e057582b7faa01441534b596eebee4ab1c4090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:16 +0000

    Add package pycarus/0.4.1/pycarus-0.4.1-py3-none-any.whl

---
+++ packages/pycarus/pycarus-0.4.1-py3-none-any.whl/pycarus/geometry/mesh.py
@@ -0,0 +1,43 @@
+from typing import Tuple
+
+import numpy as np
+import torch
+from skimage.measure import marching_cubes  # type: ignore
+from torch import nn
+
+
+@torch.no_grad()  # type: ignore
+def sdf_to_mesh(
+    mlp: nn.Module,
+    grid_size: int = 256,
+    max_batch: int = 100_000,
+) -> Tuple[np.ndarray, np.ndarray]:
+    voxel_origin = [-1, -1, -1]
+    voxel_size = 2.0 / (grid_size - 1)
+
+    overall_index = torch.arange(0, grid_size ** 3, 1)
+    coords = torch.zeros(grid_size ** 3, 4)
+
+    coords[:, 2] = overall_index % grid_size
+    coords[:, 1] = (overall_index.long() / grid_size) % grid_size
+    coords[:, 0] = ((overall_index.long() / grid_size) / grid_size) % grid_size
+
+    coords[:, 0] = (coords[:, 0] * voxel_size) + voxel_origin[2]
+    coords[:, 1] = (coords[:, 1] * voxel_size) + voxel_origin[1]
+    coords[:, 2] = (coords[:, 2] * voxel_size) + voxel_origin[0]
+
+    num_samples = grid_size ** 3
+    start = 0
+
+    while start < num_samples:
+        end = min(start + max_batch, num_samples)
+        coords_subset = coords[start:end, :3].cuda()
+        sdf = mlp(coords_subset).cpu()
+        coords[start:end, 3] = sdf
+        start += max_batch
+
+    sdf = coords[:, 3].reshape(grid_size, grid_size, grid_size)
+
+    verts, faces, _, _ = marching_cubes(sdf.numpy(), level=0.0, spacing=[voxel_size] * 3)
+
+    return verts, faces
