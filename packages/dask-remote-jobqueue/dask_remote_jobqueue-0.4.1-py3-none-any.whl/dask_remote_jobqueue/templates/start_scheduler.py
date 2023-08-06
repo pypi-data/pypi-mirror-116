927589d49938d08eaeea39089ab34f82491d9514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:32 +0000

    Add package dask-remote-jobqueue/0.4.1/dask_remote_jobqueue-0.4.1-py3-none-any.whl

---
+++ packages/dask-remote-jobqueue/dask_remote_jobqueue-0.4.1-py3-none-any.whl/dask_remote_jobqueue/templates/start_scheduler.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2021 dciangot
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+import time
+import dask
+from dask_jobqueue import HTCondorCluster
+from dask_jobqueue.htcondor import HTCondorJob
+from signal import SIGTERM, signal
+
+# dask.config.set({"distributed.worker.memory.spill": False})
+# dask.config.set({"distributed.worker.memory.target": False})
+
+
+class MyHTCondorJob(HTCondorJob):
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            *args,
+            **kwargs,
+            python="source /usr/local/share/root6/bin/thisroot.sh ; /usr/bin/python3",
+        )
+        self.submit_command = "./job_submit.sh"
+        self.executable = "/bin/bash"
+
+
+cluster = HTCondorCluster(
+    job_cls=MyHTCondorJob,
+    cores=1,
+    memory="3 GB",
+    disk="1 GB",
+    scheduler_options={
+        "host": ":8989",
+    },
+    job_extra={
+        "+OWNER": '"condor"',
+        "log": "simple.log",
+        "output": "simple.out",
+        "error": "simple.error",
+    },
+    silence_logs="debug",
+)
+
+adapt = cluster.adapt(minimum=1, maximum=15)
+
+# cluster.scale(jobs=3)
+
+
+def closeme():
+    cluster.close()
+
+
+signal(SIGTERM, closeme)
+
+while True:
+    time.sleep(60)
