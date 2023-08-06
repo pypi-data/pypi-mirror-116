23d6c50816054f8be4740eac1bcabc68d74e0e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:23 +0000

    Add package covid-data/0.1.18/covid_data-0.1.18.tar.gz

---
+++ packages/covid-data/covid_data-0.1.18.tar.gz/covid_data-0.1.18/covid_data/__init__.py
@@ -0,0 +1,6 @@
+import covid_data  # noqa: F401
+from covid_data import db as db  # noqa: F401
+from covid_data import types as types  # noqa: F401
+from covid_data.covid_data import entrypoint as script_entrypoint  # noqa: F401
+from covid_data.db import queries as queries  # noqa: F401
+from covid_data.utils import places as places_utils  # noqa: F401
