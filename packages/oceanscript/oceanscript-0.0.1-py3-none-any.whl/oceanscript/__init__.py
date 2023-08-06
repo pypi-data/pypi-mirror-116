564973b86b4b1ccda893f08e3361f171a30a87b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:39 +0000

    Add package oceanscript/0.0.1/oceanscript-0.0.1-py3-none-any.whl

---
+++ packages/oceanscript/oceanscript-0.0.1-py3-none-any.whl/oceanscript/__init__.py
@@ -0,0 +1,7 @@
+"""
+Alpha - An extensive esolang built with a python interpreter.
+"""
+
+from .converters import decode, encode
+
+__version__ = "0.0.1"
---
+++ packages/oceanscript/oceanscript-0.0.1-py3-none-any.whl/oceanscript/errors.py
@@ -0,0 +1,28 @@
+class OceanScriptError(Exception):
+    """The base exception for all oceanscript related errors."""
+
+    pass
+
+
+class ForbiddenSectorError(OceanScriptError):
+    """Raised when the forbidden 27th box is used."""
+
+    def __init__(self):
+        self.char = "_>..."
+        super().__init__("Using '_>...' is forbidden")
+
+
+class ParserError(OceanScriptError):
+    """Raised when the decoder has trouble parsing."""
+
+    def __init__(self, failed):
+        self.failed = failed
+        super().__init__("Failed to parse.")
+
+
+class UnsupportedCharacterError(OceanScriptError):
+    """Raised when an unsupported character is provided to the encoder."""
+
+    def __init__(self, char):
+        self.char = char
+        super().__init__("Character '%s' is not supported" % char)
