9524895dd7613404fdd3f75c5d6ab64ee4c97680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:12 +0000

    Add package utils-lib/0.1.0.dev2021081500/utils-lib-0.1.0.dev2021081500.tar.gz

---
+++ packages/utils-lib/utils-lib-0.1.0.dev2021081500.tar.gz/utils-lib-0.1.0.dev2021081500/utils/convert/convert_string.py
@@ -0,0 +1,24 @@
+
+class ConvertString (object):
+    @staticmethod
+    def list_to_string (source_list, sep='|'):
+        """
+        Convert the list to string. (With special character)
+
+        @params [IN] source_list : The list of string convert to string.
+        @params [IN] sep         : The character use to seperate the different
+                                   element.
+
+        @return      string      : Combine the string to specific character.
+        """
+        string = ""
+        if len (source_list) == 0:
+            pass
+        else:
+            # 使用特殊符號作區隔
+            for sub_list in source_list:
+                string = string + sep + str (sub_list)
+
+            string = string[1:]
+
+        return string
