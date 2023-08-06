3f86af58d2f2e0048741382dcb91de4b96676d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:58 +0000

    Add package pydiscovergy/1.0.1/pydiscovergy-1.0.1.tar.gz

---
+++ packages/pydiscovergy/pydiscovergy-1.0.1.tar.gz/pydiscovergy-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pydiscovergy',
+    version='1.0.1',
+    packages=['pydiscovergy'],
+    url='https://github.com/jpbede/pydiscovergy',
+    license='MIT',
+    author='Jan-Philipp Benecke',
+    author_email='jan-philipp.benecke@jpbe.de',
+    description='Python library for interacting with discovergy smart meters api',
+    install_requires=['httpx', 'authlib'],
+    python_requires='>=3.6'
+)
