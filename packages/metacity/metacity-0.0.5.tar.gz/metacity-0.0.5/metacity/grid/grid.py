193898e313f81a35300a59d5eb87cd09e4d96094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:41 +0000

    Add package metacity/0.0.5/metacity-0.0.5.tar.gz

---
+++ packages/metacity/metacity-0.0.5.tar.gz/metacity-0.0.5/metacity/grid/grid.py
@@ -0,0 +1,35 @@
+from metacity.models.tiles.model import MetaTile
+from metacity.grid.config import RegularGridConfig
+from metacity.grid.slicer import RegularGridSlicer
+from metacity.grid.cache import RegularGridCache
+
+
+class RegularGrid:
+    def __init__(self, dirtree):
+        self.dirtree = dirtree
+
+
+    @property
+    def config(self):
+        return RegularGridConfig(self.dirtree)
+
+
+    @property
+    def slicer(self):
+        return RegularGridSlicer(self.config)
+
+
+    @property
+    def cache(self):
+        return RegularGridCache(self.config, self.dirtree)
+
+
+    @property
+    def tiles(self):
+        tile_name: str
+        for tile_name in self.dirtree.tile_names:
+            x, y = [ int(i) for i in tile_name.split("_") ]
+            tile = MetaTile()
+            tile.load(x, y, self.dirtree)
+            yield tile
+
