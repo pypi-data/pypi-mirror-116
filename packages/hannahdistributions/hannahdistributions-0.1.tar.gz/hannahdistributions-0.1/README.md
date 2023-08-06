f3178e48c6402177833dcfe0199ede00685816cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:59 +0000

    Add package hannahdistributions/0.1/hannahdistributions-0.1.tar.gz

---
+++ packages/hannahdistributions/hannahdistributions-0.1.tar.gz/hannahdistributions-0.1/README.md
@@ -0,0 +1,12 @@
+# AWS ML Foundation Course - *Practice upload on pypi*
+
+Submitted by: **Hannah Phung**
+
+## Functions
+
+* [x] Gaussian distribution
+* [x] Binomial distribution
+
+
+
+
---
+++ packages/hannahdistributions/hannahdistributions-0.1.tar.gz/hannahdistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='hannahdistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['hannahdistributions'],
+      zip_safe=False)
