c9f0cb9e526366ba7fe2b68496540c21919abe41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:53 +0000

    Add package gauss-binom-pdf/0.1/gauss_binom_pdf-0.1.tar.gz

---
+++ packages/gauss-binom-pdf/gauss_binom_pdf-0.1.tar.gz/gauss_binom_pdf-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gauss_binom_pdf',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gauss_binom_pdf'],
+      zip_safe=False)
