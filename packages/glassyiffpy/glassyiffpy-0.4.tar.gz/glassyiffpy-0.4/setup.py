43b0bdfd2da87d1bc4e9b6d414b2b5ca6576b705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:45 +0000

    Add package glassyiffpy/0.4/glassyiffpy-0.4.tar.gz

---
+++ packages/glassyiffpy/glassyiffpy-0.4.tar.gz/glassyiffpy-0.4/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'glassyiffpy',    
+  packages = ['glassyiffpy'],   
+  version = '0.4',    
+  license='MIT',       
+  description = 'Simple thingy to interact with yiff-party.com', 
+  author = 'Glass-Paramedic',                   
+  author_email = 'glass-paramedic@weddit.online',    
+  url = 'http://weddit.net',   
+  download_url = 'https://github.com/Glass-Paramedic/glassyiffpy/archive/refs/tags/v_04.tar.gz', 
+  keywords = ['yiff', 'furry', 'porn'],   
+  install_requires=[     
+          'requests',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',       
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
