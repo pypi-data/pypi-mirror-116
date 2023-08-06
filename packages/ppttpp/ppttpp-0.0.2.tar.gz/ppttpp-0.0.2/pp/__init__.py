c94eb119d5db3fadf8a07ec47f53548ff3b2a447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:07 +0000

    Add package ppttpp/0.0.2/ppttpp-0.0.2.tar.gz

---
+++ packages/ppttpp/ppttpp-0.0.2.tar.gz/ppttpp-0.0.2/pp/__init__.py
@@ -0,0 +1,1 @@
+from .tfile import hello
---
+++ packages/ppttpp/ppttpp-0.0.2.tar.gz/ppttpp-0.0.2/setup.py
@@ -0,0 +1,19 @@
+
+
+import setuptools
+
+
+setuptools.setup(
+    name="ppttpp",
+    version="0.0.2",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
