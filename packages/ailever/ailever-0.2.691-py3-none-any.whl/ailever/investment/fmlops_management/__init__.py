c0d1e454dae16e7f5633f37c1748d4cffd935f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:14 +0000

    Add package ailever/0.2.691/ailever-0.2.691-py3-none-any.whl

---
+++ packages/ailever/ailever-0.2.691-py3-none-any.whl/ailever/investment/fmlops_management/__init__.py
@@ -0,0 +1,5 @@
+from .forecasting_model_registry import ForecastingModelRegistryManager as FMR_Manager
+from .feature_store import FeatureStoreManager as FS_Manager
+from .metadata_store import MetadataStoreManager as MS_Manager
+from .source_repository import SourceRepositoryManager as SR_Manager
+
