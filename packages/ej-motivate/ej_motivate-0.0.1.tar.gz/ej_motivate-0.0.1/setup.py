c2e46f383ffadec2139ef5357ebb369feafa9210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:43 +0000

    Add package ej-motivate/0.0.1/ej_motivate-0.0.1.tar.gz

---
+++ packages/ej-motivate/ej_motivate-0.0.1.tar.gz/ej_motivate-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+__project__ = "ej_motivate"
+__version__ = "0.0.1"
+__description__ = "a Python module to motivate you"
+__packages__ = ["ej_motivate"]
+__author__ = "EJ"
+
+setup(
+    name = __project__,
+    version = __version__,
+    description = __description__,
+    packages = __packages__,
+    author = __author__,
+)
