b36578db48f67289f79cfefbc1068d51262439ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:08 +0000

    Add package spark-eda/0.0.2/spark_eda-0.0.2.tar.gz

---
+++ packages/spark-eda/spark_eda-0.0.2.tar.gz/spark_eda-0.0.2/spark_eda/univariate.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+
+def describe_pd(df_in, columns, deciles=False):
+    """
+    Function to union the basic stats results and deciles
+    :param df_in: the input dataframe
+    :param columns: the cloumn name list of the numerical variable
+    :param deciles: the deciles output
+    :return : the numerical describe info. of the input dataframe
+    """
+    if deciles:
+        percentiles = np.array(range(0, 110, 10))
+    else:
+        percentiles = [25, 50, 75]
+    percs = np.transpose([np.percentile(df_in.select(x).collect(), percentiles) for x in columns])
+    percs = pd.DataFrame(percs, columns=columns)
+    percs['summary'] = [str(p) + '%' for p in percentiles]
+    spark_describe = df_in.describe().toPandas()
+    new_df = pd.concat([spark_describe, percs], ignore_index=True)
+    new_df = new_df.round(2)
+    return new_df[['summary'] + columns]
