e6afb232ccadd3706017755d92e58e78dd3ba836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:09 +0000

    Add package yappa/0.4.15/yappa-0.4.15-py3-none-any.whl

---
+++ packages/yappa/yappa-0.4.15-py3-none-any.whl/yappa/handlers/asgi.py
@@ -0,0 +1,47 @@
+import json
+import logging
+from pathlib import Path
+
+import httpx
+
+from .common import DEFAULT_CONFIG_FILENAME, load_yaml
+from .wsgi import load_app, patch_response
+
+logger = logging.getLogger(__name__)
+
+
+async def call_app(app, event):
+    host_url = event["headers"].get("Host", "https://raw-function.net")
+    if not host_url.startswith("http"):
+        host_url = f"https://{host_url}"
+    async with httpx.AsyncClient(app=app,
+                                 base_url=host_url) as client:
+        request = client.build_request(
+            method=event["httpMethod"],
+            url=event["url"],
+            headers=event["headers"],
+            params=event["queryStringParameters"],
+            content=json.dumps(event["body"]).encode(),
+        )
+        response = await client.send(request)
+        return response
+
+
+try:
+    config = load_yaml(Path(Path(__file__).resolve().parent.parent,
+                            DEFAULT_CONFIG_FILENAME))
+
+    app = load_app(config.get("entrypoint"),
+                   config.get("DJANGO_SETTINGS_MODULE"))
+except ValueError:
+    logger.warning("Looks like broken Yappa config is used")
+
+
+async def handle(event, context):
+    if not event:
+        return {
+            'statusCode': 500,
+            'body': "got empty event",
+        }
+    response = await call_app(app, event)
+    return patch_response(response)
