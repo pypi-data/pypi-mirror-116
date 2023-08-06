8d79be5618de6627dd1b475486ec03794957c28f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:35 +0000

    Add package get-manaba/2.0.0/get-manaba-2.0.0.tar.gz

---
+++ packages/get-manaba/get-manaba-2.0.0.tar.gz/get-manaba-2.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='get-manaba',
+    version='2.0.0',
+    packages=['manaba'],
+    install_requires=["beautifulsoup4", "requests", "html5lib"],
+    url='https://github.com/book000/get-manaba',
+    license='MIT',
+    author='Tomachi',
+    author_email='tomachi@tomacheese.com',
+    maintainer='Tomachi',
+    maintainer_email='tomachi@tomacheese.com',
+    description='Library for get various information about manaba.',
+    long_description=open('README.md', encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
