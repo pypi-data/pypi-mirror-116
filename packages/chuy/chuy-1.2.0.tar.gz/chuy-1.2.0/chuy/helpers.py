6edf5533c2a7dd361142de6c7b5891515fc6563d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:43 +0000

    Add package chuy/1.2.0/chuy-1.2.0.tar.gz

---
+++ packages/chuy/chuy-1.2.0.tar.gz/chuy-1.2.0/chuy/helpers.py
@@ -0,0 +1,70 @@
+"""
+Here I define the methods that help me in the core.
+"""
+import os
+import json
+import sys
+
+from .decorators import keyboard_interrupt
+from .ui import colorized_print, colorized_input, cyan, magenta, yellow
+
+
+def get_config(configuration_file: str) -> dict:
+    """
+    Read the config and parse it to a Python dictionary.
+    """
+    try:
+        with open(configuration_file, "r") as reader:
+            return json.load(reader)
+    except json.decoder.JSONDecodeError:
+        colorized_print(" Your configuration is invalid!")
+        sys.exit(0)
+    except FileNotFoundError:
+        colorized_print(" I can't find your configuration file :(")
+        sys.exit(0)
+
+
+@keyboard_interrupt
+def list_commands(config: dict) -> None:
+    """
+    Print all the aliases defined in the config and their respective command.
+    """
+    colorized_print(" Project Commands:", cyan)
+
+    for item in config:
+        colorized_print(
+            f"""
+  - {item}
+      {yellow}$ {magenta}{config[item]}
+        """
+        )
+
+
+@keyboard_interrupt
+def get_commands(config: dict) -> list:
+    """
+    Get a list with all the commands to execute.
+    """
+
+    commands = []
+
+    for item in range(len(config)):
+        try:
+            commands.append(sys.argv[item])
+        except IndexError:
+            break
+
+    if len(commands) == 1:
+        list_commands(config)
+        return colorized_input("Which command do you want to run? ").split(" ")
+
+    return commands[1::]
+
+
+@keyboard_interrupt
+def exec_commands(command: str) -> None:
+    """
+    Print command with colors and then execute it.
+    """
+    colorized_print(f" $ {command} \n", magenta)
+    os.system(command)
