b9d1d641cf05c69e1a86373a73976c05bbaef183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:39 +0000

    Add package bioutensil/0.0.3/bioutensil-0.0.3-py3-none-any.whl

---
+++ packages/bioutensil/bioutensil-0.0.3-py3-none-any.whl/bioutensil/__init__.py
@@ -0,0 +1,24 @@
+from .version import __version__
+from .bioutensil import bam
+from .bioutensil import bash
+from .bioutensil import basic
+from .bioutensil import clustering
+from .bioutensil import cnv
+from .bioutensil import complex_haplotype
+from .bioutensil import constants
+from .bioutensil import converter
+from .bioutensil import csv_aux
+from .bioutensil import fasta
+from .bioutensil import gene
+from .bioutensil import genome
+from .bioutensil import genomic_region
+from .bioutensil import module
+from .bioutensil import myio
+from .bioutensil import myprint
+from .bioutensil import psl_tran
+from .bioutensil import sequence
+from .bioutensil import shell
+from .bioutensil import structure_variant
+from .bioutensil import vcf_aux
+
+name = 'bioutensil'
