58edb69754cdad4e630cda08d7d29456d314967c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:03 +0000

    Add package django-sage-painless/1.12.0/django-sage-painless-1.12.0.tar.gz

---
+++ packages/django-sage-painless/django-sage-painless-1.12.0.tar.gz/django-sage-painless-1.12.0/sage_painless/services/model_generator.py
@@ -0,0 +1,309 @@
+import os
+import time
+from pathlib import Path
+
+from django.conf import settings
+from django.core import management
+
+from sage_painless.classes.field import Field
+from sage_painless.classes.model import Model
+from sage_painless.classes.signal import Signal
+from sage_painless.utils.file_service import FileService
+from sage_painless.utils.git_service import GitSupport
+
+from sage_painless.utils.jinja_service import JinjaHandler
+from sage_painless.utils.json_service import JsonHandler
+from sage_painless.utils.pep8_service import Pep8
+
+from sage_painless import templates
+from sage_painless.utils.timing_service import TimingService
+
+
+class ModelGenerator(
+    JinjaHandler, JsonHandler, Pep8,
+    FileService, TimingService, GitSupport
+):
+    """Read models data from a Json file and stream it to app_name/models.py"""
+
+    MODELS_KEYWORD = 'models'
+    APPS_KEYWORD = 'apps'
+    FIELDS_KEYWORD = 'fields'
+    TYPE_KEYWORD = 'type'
+    ENCRYPTED_KEYWORD = 'encrypt'
+    STREAM_KEYWORD = 'stream'
+    VALIDATORS_KEYWORD = 'validators'
+    FUNC_KEYWORD = 'func'
+    ARG_KEYWORD = 'arg'
+
+    def __init__(self, *args, **kwargs):
+        """init"""
+        super().__init__(*args, **kwargs)
+
+    def get_table_fields(self, table):
+        """extract fields"""
+        return table.get(self.FIELDS_KEYWORD)
+
+    def extract_models(self, diagram):
+        """extract models"""
+        models = list()
+        signals = list()
+        for table_name in diagram.keys():
+            table = diagram.get(table_name)
+            fields = self.get_table_fields(table)
+
+            model = Model()
+            model.name = table_name
+            model_fields = list()
+
+            for field_name in fields.keys():
+                model_field = Field()
+                model_field.name = field_name
+                field_data = fields.get(field_name)
+
+                # Encrypt
+                model_field.encrypted = field_data.pop(self.ENCRYPTED_KEYWORD, False)  # field encryption
+                model_field.stream = field_data.pop(self.STREAM_KEYWORD, False)  # video field streaming
+
+                for key in field_data.keys():
+                    # Type
+                    if key == self.TYPE_KEYWORD:
+                        model_field.set_type(field_data.get(self.TYPE_KEYWORD))  # set type of Field (CharField, etc)
+
+                        # if field is one2one create Signal
+                        if model_field.type == 'OneToOneField':
+                            signal = Signal()
+                            signal.set_signal('post_save', table_name, field_data.get('to'), field_name)
+                            signals.append(signal)
+
+                    # Validator
+                    elif key == self.VALIDATORS_KEYWORD:
+                        for validator in field_data.get(self.VALIDATORS_KEYWORD):
+                            model_field.add_validator(validator.get(self.FUNC_KEYWORD), validator.get(self.ARG_KEYWORD))
+
+                    # Attributes
+                    else:
+                        value = field_data.get(key)
+                        model_field.add_attribute(key, value)
+
+                model_fields.append(model_field)
+
+            model.fields = model_fields
+            models.append(model)
+
+        return models, signals
+
+    def check_validator_support(self, models):
+        """check models have validator"""
+        for model in models:
+            for field in model.fields:
+                if len(field.validators) > 0:
+                    return True
+
+        return False
+
+    def check_signal_support(self, models):
+        """check models have one2one"""
+        for model in models:
+            for field in model.fields:
+                if field.type == 'OneToOneField':
+                    return True
+
+        return False
+
+    def check_encryption_support(self, models):
+        """check models have encrypted field"""
+        for model in models:
+            for field in model.fields:
+                if field.encrypted:
+                    return True
+
+        return False
+
+    def get_fk_model_names(self, models):
+        """check models have fk,m2m,one2one,... and return model names"""
+        model_names = list()
+        for model in models:
+            for field in model.fields:
+                if field.type in ['ManyToManyField', 'OneToOneField', 'ForeignKey']:
+                    model_names.append(field.get_attribute('to'))
+
+        return model_names
+
+    def create_app_if_not_exists(self, app_name):
+        if not os.path.exists(f'{settings.BASE_DIR}/{app_name}/'):
+            management.call_command('startapp', app_name)
+
+    def generate_models(self, diagram_path, cache_support=False, git_support=False):
+        """stream models to app_name/models/model_name.py
+        generate signals, mixins, services
+        template:
+            sage_painless/templates/models.txt
+            sage_painless/templates/signals.txt
+            sage_painless/templates/mixins.txt
+            sage_painless/templates/services.txt
+        """
+        start_time = time.time()
+        diagram = self.load_json(diagram_path)
+
+        for app_name in diagram.get(self.APPS_KEYWORD).keys():
+            models_diagram = diagram.get(self.APPS_KEYWORD).get(app_name).get(
+                self.MODELS_KEYWORD)  # get models data for current app
+            models, signals = self.extract_models(models_diagram)  # extract models and signals from diagram data
+
+            # initialize
+            self.create_app_if_not_exists(app_name)
+            self.create_dir_for_app_if_not_exists('models', app_name)
+            self.create_file_if_not_exists(f'{settings.BASE_DIR}/{app_name}/models/__init__.py')
+            self.delete_file_if_exists(f'{settings.BASE_DIR}/{app_name}/models.py')
+            if git_support:
+                self.init_repo(settings.BASE_DIR)
+
+            # mixins & services
+            if cache_support:
+                self.stream_to_template(
+                    output_path=f'{settings.BASE_DIR}/{app_name}/mixins.py',
+                    template_path=os.path.abspath(templates.__file__).replace('__init__.py', 'mixins.txt'),
+                    data={
+                        'cache_support': cache_support
+                    }
+                )
+
+                self.stream_to_template(
+                    output_path=f'{settings.BASE_DIR}/{app_name}/services.py',
+                    template_path=os.path.abspath(templates.__file__).replace('__init__.py', 'services.txt'),
+                    data={
+                        'cache_support': cache_support
+                    }
+                )
+
+                self.fix_pep8(f'{settings.BASE_DIR}/{app_name}/mixins.py')
+                self.fix_pep8(f'{settings.BASE_DIR}/{app_name}/services.py')
+                if git_support:
+                    self.commit_file(
+                        f'{settings.BASE_DIR}/{app_name}/services.py',
+                        f'feat ({app_name}--services): Create cache service'
+                    )
+                    self.commit_file(
+                        f'{settings.BASE_DIR}/{app_name}/mixins.py',
+                        f'feat ({app_name}--mixins): Create cache mixin'
+                    )
+
+            # models
+            for model in models:
+                model_file_name = f'{model.name.lower()}.py'  # model file name is lower of model name (e.g Category
+                # -> category.py)
+                self.stream_to_template(
+                    output_path=f'{settings.BASE_DIR}/{app_name}/models/{model_file_name}',
+                    template_path=os.path.abspath(templates.__file__).replace('__init__.py', 'models.txt'),
+                    data={
+                        'app_name': app_name,
+                        'models': [model],
+                        'validator_support': self.check_validator_support([model]),
+                        'cache_support': cache_support,
+                        'fk_models': self.get_fk_model_names([model]),
+                        'encrypt_support': self.check_encryption_support([model])
+                    }
+                )
+                self.fix_pep8(f'{settings.BASE_DIR}/{app_name}/models/{model_file_name}')
+                if git_support:
+                    self.commit_file(
+                        f'{settings.BASE_DIR}/{app_name}/models/{model_file_name}',
+                        f'feat ({app_name}--models): Create {model.name} model'
+                    )
+
+            # signals
+            if self.check_signal_support(models):
+                self.stream_to_template(
+                    output_path=f'{settings.BASE_DIR}/{app_name}/signals.py',
+                    template_path=os.path.abspath(templates.__file__).replace('__init__.py', 'signals.txt'),
+                    data={
+                        'app_name': app_name,
+                        'models': models,
+                        'signals': signals,
+                        'signal_support': True,
+                        'cache_support': cache_support
+                    }
+                )
+
+                self.stream_to_template(
+                    output_path=f'{settings.BASE_DIR}/{app_name}/__init__.py',
+                    template_path=os.path.abspath(templates.__file__).replace('__init__.py', '__init__.txt'),
+                    data={
+                        'app_name': app_name
+                    }
+                )
+
+                self.stream_to_template(
+                    output_path=f'{settings.BASE_DIR}/{app_name}/apps.py',
+                    template_path=os.path.abspath(templates.__file__).replace('__init__.py', 'apps.txt'),
+                    data={
+                        'app_name': app_name,
+                        'signal_support': True
+                    }
+                )
+
+                self.fix_pep8(f'{settings.BASE_DIR}/{app_name}/signals.py')
+                self.fix_pep8(f'{settings.BASE_DIR}/{app_name}/apps.py')
+                self.fix_pep8(f'{settings.BASE_DIR}/{app_name}/__init__.py')
+                if git_support:
+                    self.commit_file(
+                        f'{settings.BASE_DIR}/{app_name}/signals.py',
+                        f'feat ({app_name}--signals): Create model OneToOne signals'
+                    )
+                    self.commit_file(
+                        f'{settings.BASE_DIR}/{app_name}/apps.py',
+                        f'feat ({app_name}--signals): Add signals to apps.py'
+                    )
+                    self.commit_file(
+                        f'{settings.BASE_DIR}/{app_name}/__init__.py',
+                        f'feat ({app_name}--signals): Add signals to __init__'
+                    )
+
+            elif cache_support:
+                self.stream_to_template(
+                    output_path=f'{settings.BASE_DIR}/{app_name}/signals.py',
+                    template_path=os.path.abspath(templates.__file__).replace('__init__.py', 'signals.txt'),
+                    data={
+                        'app_name': app_name,
+                        'models': models,
+                        'signals': signals,
+                        'signal_support': True,
+                        'cache_support': cache_support
+                    }
+                )
+
+                self.stream_to_template(
+                    output_path=f'{settings.BASE_DIR}/{app_name}/__init__.py',
+                    template_path=os.path.abspath(templates.__file__).replace('__init__.py', '__init__.txt'),
+                    data={
+                        'app_name': app_name
+                    }
+                )
+
+                self.stream_to_template(
+                    output_path=f'{settings.BASE_DIR}/{app_name}/apps.py',
+                    template_path=os.path.abspath(templates.__file__).replace('__init__.py', 'apps.txt'),
+                    data={
+                        'app_name': app_name,
+                        'signal_support': True
+                    }
+                )
+
+                self.fix_pep8(f'{settings.BASE_DIR}/{app_name}/signals.py')
+                self.fix_pep8(f'{settings.BASE_DIR}/{app_name}/apps.py')
+                self.fix_pep8(f'{settings.BASE_DIR}/{app_name}/__init__.py')
+                if git_support:
+                    self.commit_file(
+                        f'{settings.BASE_DIR}/{app_name}/signals.py',
+                        f'feat ({app_name}--signals): Create cache update signals'
+                    )
+                    self.commit_file(
+                        f'{settings.BASE_DIR}/{app_name}/apps.py',
+                        f'feat ({app_name}--signals): Add signals to apps.py'
+                    )
+                    self.commit_file(
+                        f'{settings.BASE_DIR}/{app_name}/__init__.py',
+                        f'feat ({app_name}--signals): Add signals to __init__'
+                    )
+        end_time = time.time()
+        return True, 'models generated ({:.3f} ms)'.format(self.calculate_execute_time(start_time, end_time))
---
+++ packages/django-sage-painless/django-sage-painless-1.12.0.tar.gz/django-sage-painless-1.12.0/sage_painless/services/readme_generator.py
@@ -0,0 +1,135 @@
+import os
+import time
+from pathlib import Path
+from platform import python_version
+
+import django
+from django.apps import apps
+from django.conf import settings
+
+from sage_painless import templates
+from sage_painless.utils.file_service import FileService
+from sage_painless.utils.git_service import GitSupport
+from sage_painless.utils.jinja_service import JinjaHandler
+from sage_painless.utils.json_service import JsonHandler
+from sage_painless.utils.pep8_service import Pep8
+from sage_painless.utils.timing_service import TimingService
+
+
+class ReadMeGenerator(
+    JinjaHandler, JsonHandler, Pep8,
+    FileService, TimingService, GitSupport
+):
+    """Generate README.md for project"""
+
+    APPS_KEYWORD = 'apps'
+    SPACE = '    '
+    BRANCH = '│   '
+    TEE = '├── '
+    LAST = '└── '
+    IGNORE_DIRS = [
+        'venv', '.pytest_cache',
+        '.tox', '.vscode', 'dist',
+        '.git', '__pycache__', '_build',
+        '_static', '_templates',
+    ]
+
+    def __init__(self, *args, **kwargs):
+        """init"""
+        super().__init__(*args, **kwargs)
+
+    def get_built_in_app_names(self):
+        """django built-in apps"""
+        return [app.verbose_name for app in apps.get_app_configs() if app.name.startswith('django.')]
+
+    def get_installed_module_names(self):
+        """extra installed modules to setting"""
+        return [app.verbose_name for app in apps.get_app_configs() if not app.name.startswith('django.')]
+
+    def get_project_name(self):
+        """get project root name"""
+        base_dir = settings.BASE_DIR
+        return base_dir.name if hasattr(base_dir, 'name') else base_dir
+
+    def get_project_version(self):
+        """get current project version if set"""
+        return getattr(settings, 'VERSION', '1.0.0')
+
+    def get_django_version(self):
+        """get current Django version"""
+        return django.get_version()
+
+    def merge(self, list_a: list, list_b: list):
+        """merge 2 lists"""
+        return list(set(list_a + list_b))
+
+    def has_docker_support(self):
+        """is project dockerized"""
+        compose_file_yml = Path(f'{settings.BASE_DIR}/docker-compose.yml')
+        compose_file_yaml = Path(f'{settings.BASE_DIR}/docker-compose.yaml')
+
+        if compose_file_yml.is_file() or compose_file_yaml.is_file():
+            return True
+
+        return False
+
+    def make_tree(self, dir_path: Path, prefix: str = ''):
+        """create project root tree structure"""
+        contents = list(dir_path.iterdir())
+        pointers = [self.TEE] * (len(contents) - 1) + [self.LAST]
+
+        for pointer, path in zip(pointers, contents):
+            if path.name not in self.IGNORE_DIRS:
+                yield prefix + pointer + path.name
+                if path.is_dir():
+                    extension = self.BRANCH if pointer == self.TEE else self.SPACE
+                    yield from self.make_tree(path, prefix=prefix + extension)
+
+    def generate(self, diagram_path, git_support=False):
+        """stream README.md to docs/sage_painless/git/README.md
+        template:
+            sage_painless/templates/README.txt
+        """
+        start_time = time.time()
+        diagram = self.load_json(diagram_path)
+
+        # initialize
+        self.create_dir_if_not_exists('docs')
+        self.create_dir_if_not_exists('docs/sage_painless')
+        self.create_dir_if_not_exists('docs/sage_painless/git')
+        if git_support:
+            self.init_repo(settings.BASE_DIR)
+
+        modules = self.get_installed_module_names()
+        django_apps = self.get_built_in_app_names()
+
+        project_name = self.get_project_name()
+        project_version = self.get_project_version()
+
+        django_version = self.get_django_version()
+        py_version = python_version()
+
+        project_structure = self.make_tree(settings.BASE_DIR)
+
+        self.stream_to_template(
+            output_path=f'{settings.BASE_DIR}/docs/sage_painless/git/README.md',
+            template_path=os.path.abspath(templates.__file__).replace('__init__.py', 'README.txt'),
+            data={
+                'project_name': project_name,
+                'built_in_apps': django_apps,
+                'installed_modules': modules,
+                'django_version': django_version,
+                'py_version': py_version,
+                'project_version': project_version,
+                'docker_support': self.has_docker_support(),
+                'structure': project_structure
+            }
+        )
+        if git_support:
+            self.commit_file(
+                f'{settings.BASE_DIR}/docs/sage_painless/git/README.md',
+                f'docs (git): Create README.md'
+            )
+
+        end_time = time.time()
+        return True, 'README generated ({:.3f} ms)'.format(self.calculate_execute_time(start_time, end_time))
