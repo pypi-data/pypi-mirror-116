3a4d4a1c4d848f3ad4efa8d950256b6653e6a268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:58 +0000

    Add package staircase/2.0.0rc1/staircase-2.0.0rc1-py3-none-any.whl

---
+++ packages/staircase/staircase-2.0.0rc1-py3-none-any.whl/staircase/core/stats/__init__.py
@@ -0,0 +1,37 @@
+from staircase.core.accessor import CachedAccessor
+from staircase.core.stats.statistic import _max as max
+from staircase.core.stats.statistic import _min as min
+from staircase.core.stats.statistic import (
+    agg,
+    corr,
+    cov,
+    integral,
+    mean,
+    median,
+    mode,
+    std,
+    value_sums,
+    values_in_range,
+    var,
+)
+
+
+def add_methods(cls):
+    from staircase.core.stats.distribution import Dist
+
+    # cls._get_integral_and_mean = _get_integral_and_mean
+    cls.value_sums = value_sums
+    cls.values_in_range = values_in_range
+    cls.cov = cov
+    cls.corr = corr
+    cls.agg = agg
+    # cls._get_dist = _get_dist
+
+    cls.integral = integral
+    cls.mean = mean
+    cls.median = median
+    cls.mode = mode
+    cls.std = std
+    cls.var = var
+
+    cls.dist = CachedAccessor("dist", Dist)
