6733d024f850e22d7762283eb736a7f084b6dede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:43 +0000

    Add package sortasurvey/1.1.0/sortasurvey-1.1.0.tar.gz

---
+++ packages/sortasurvey/sortasurvey-1.1.0.tar.gz/sortasurvey-1.1.0/sortasurvey/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+from .observing import *
+from .pipeline import *
+from .sample import *
+from .survey import *
+from .utils import *
+
+__all__ = ['cli', 'observing', 'pipeline', 'sample', 'survey', 'utils']
+
+__version__ = '1.1.0'
+
+_ROOT = os.path.abspath(os.getcwd())
+INPDIR = os.path.join(_ROOT, 'info')
+OUTDIR = os.path.join(_ROOT, 'results')
