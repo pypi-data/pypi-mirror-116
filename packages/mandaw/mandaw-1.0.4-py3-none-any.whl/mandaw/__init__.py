ef299afeb73ca0184af4066cb6cbf26e625e02a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:05 +0000

    Add package mandaw/1.0.4/mandaw-1.0.4-py3-none-any.whl

---
+++ packages/mandaw/mandaw-1.0.4-py3-none-any.whl/mandaw/__init__.py
@@ -0,0 +1,6 @@
+from mandaw.main import Mandaw
+from mandaw.gameobject import GameObject
+from mandaw.line import Line
+from mandaw.color import Color
+from mandaw.sprite import Sprite
+from mandaw.text import Text
