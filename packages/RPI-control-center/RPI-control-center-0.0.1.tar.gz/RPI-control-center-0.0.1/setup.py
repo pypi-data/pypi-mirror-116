97194ea324f37a7a049d28c5c97e18adaec5847b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:31 +0000

    Add package RPI-control-center/0.0.1/RPI-control-center-0.0.1.tar.gz

---
+++ packages/RPI-control-center/RPI-control-center-0.0.1.tar.gz/RPI-control-center-0.0.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="RPI-control-center",
+    version="0.0.1",
+    author="Mohamed Debbagh",
+    author_email="moha7108@protonmail.com",
+    description="Python library for interfacing Raspberry pi with connected relays/Actuator in a non-blocking multiprocess threading manor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/moha7108/3-channel-control",
+    project_urls={
+        "Bug Tracker": "https://gitlab.com/moha7108/3-channel-control/README.md",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Development Status :: 2 - Pre-Alpha",
+    ],
+    license='GNU GPLv3',
+    packages=['rpi_control_center'],
+    python_requires=">=3.6",
+    install_requires=[
+          'logzero',
+          'RPi.GPIO'
+      ],
+
+)
