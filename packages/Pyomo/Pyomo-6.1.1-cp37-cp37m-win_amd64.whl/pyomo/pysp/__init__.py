b0373059b5719d624fddbb4aca6677b2e0c3b22e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:06 +0000

    Add package Pyomo/6.1.1/Pyomo-6.1.1-cp37-cp37m-win_amd64.whl

---
+++ packages/Pyomo/Pyomo-6.1.1-cp37-cp37m-win_amd64.whl/pyomo/pysp/__init__.py
@@ -0,0 +1,34 @@
+#  ___________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright 2017 National Technology and Engineering Solutions of Sandia, LLC
+#  Under the terms of Contract DE-NA0003525 with National Technology and
+#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
+#  rights in this software.
+#  This software is distributed under the 3-clause BSD License.
+#  ___________________________________________________________________________
+
+import logging
+import sys
+from pyomo.common.deprecation import deprecation_warning
+
+# Only implement the deprecation imports nose is NOT running
+if 'nose' not in sys.modules and 'nose2' not in sys.modules:
+    try:
+        from pysp import *
+        # Redirect all (imported) pysp modules into the pyomo.pysp namespace
+        for mod in list(sys.modules):
+            if mod.startswith('pysp.'):
+                sys.modules['pyomo.'+mod] = sys.modules[mod]
+                # Warn the user
+        deprecation_warning(
+            "PySP has been removed from pyomo.pysp namespace.  "
+            "Please import PySP directly from the pysp namespace.",
+            version='6.0')
+    except ImportError:
+        raise ImportError(
+            "No module named 'pyomo.pysp'.  "
+            "Beginning in Pyomo 6.0, PySP is distributed as a separate "
+            "package.  Please see https://github.com/Pyomo/pysp for "
+            "information on downloading and installing PySP")
+
