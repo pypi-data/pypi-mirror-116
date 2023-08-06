7800ade371ce82ce94798d8f9d83de2e25d45c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:55 +0000

    Add package dj003/0.1.0/DJ003-0.1.0-py3-none-any.whl

---
+++ packages/dj003/DJ003-0.1.0-py3-none-any.whl/DJ003/run.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+  @Time : 2021/8/6 17:41 
+  @Auth : 任阳光
+  @File : run.py
+  @IDE  : PyCharm
+  @firm : 湖南麒麟信息工程技术有限公司
+  @web  : http://www.kylinos.com.cn/
+-------------------------------------------------
+"""
+from str_pic import run
+
+
+def main():
+    print("This is a demo!")
+
+
+run.run()
