a1ae89827553394abaf8bcaf4ab25f24bb1f347b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:07 +0000

    Add package vex-cog-utils/1.4.9/vex_cog_utils-1.4.9-py3-none-any.whl

---
+++ packages/vex-cog-utils/vex_cog_utils-1.4.9-py3-none-any.whl/vexcogutils/consts.py
@@ -0,0 +1,4 @@
+DOCS_BASE = "This cog has docs! Check them out at\nhttps://vex-cogs.rtfd.io/en/latest/cogs/{}.html"
+
+CHECK = "\N{HEAVY CHECK MARK}\N{VARIATION SELECTOR-16}"
+CROSS = "\N{CROSS MARK}"
