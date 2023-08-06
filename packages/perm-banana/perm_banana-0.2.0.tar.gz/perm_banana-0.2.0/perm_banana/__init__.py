e3d0f690d89f6e5771503a2b102642997a788299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:19 +0000

    Add package perm-banana/0.2.0/perm_banana-0.2.0.tar.gz

---
+++ packages/perm-banana/perm_banana-0.2.0.tar.gz/perm_banana-0.2.0/perm_banana/__init__.py
@@ -0,0 +1,7 @@
+from .CheckStrategy import *
+from .banana import banana
+from .MetaCheck import MetaCheck
+from .Check import Check
+from .Permission import Permission
+from .PermissionHandler import PermissionHandler
+from .NameConflictException import NameConflictException
