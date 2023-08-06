1adfb098b93eb242fd45cc6231d58dbad8604610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:18 +0000

    Add package ezufw/0.0.9/ezufw-0.0.9-py3-none-any.whl

---
+++ packages/ezufw/ezufw-0.0.9-py3-none-any.whl/ezufw/__init__.py
@@ -0,0 +1,19 @@
+from .ezufw import (
+    rules,
+    enable,
+    disable,
+    status,
+    execute,
+    reset,
+
+    delete_by_port,
+    delete_by_ip,
+
+    deny,
+    denyA,
+    deny_from,
+
+    allow_from,
+
+    insert
+)
