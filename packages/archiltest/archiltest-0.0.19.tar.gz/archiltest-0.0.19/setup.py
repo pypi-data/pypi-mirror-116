4ddc434f25569a8f24d87029e38d4f0bf70b4ca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:25 +0000

    Add package archiltest/0.0.19/archiltest-0.0.19.tar.gz

---
+++ packages/archiltest/archiltest-0.0.19.tar.gz/archiltest-0.0.19/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='archiltest',
+    version='0.0.19',
+    description='TODO',
+    long_description=open('README.txt').read(),
+    include_package_data=True,
+    url='',
+    author='Archil Chachanidze',
+    author_email='archikochachanidze@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='TODO',
+    install_requires=[''],
+)
