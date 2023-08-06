73117932cdf85d3102b02b24e23c0d82517093be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:54 +0000

    Add package hepfile/0.1.1/hepfile-0.1.1.tar.gz

---
+++ packages/hepfile/hepfile-0.1.1.tar.gz/hepfile-0.1.1/src/hepfile/__init__.py
@@ -0,0 +1,10 @@
+"""
+A file description modeled after the ROOT analysis toolkit and common use-cases in High Energy Physics. Implemented in HDF5, hepfile stands for Heterogeneous Entries in Parallel-file. 
+"""
+
+__version__ = "0.1.1"
+
+__all__ = ("__version__",)
+
+from hepfile.read import *
+from hepfile.write import *
