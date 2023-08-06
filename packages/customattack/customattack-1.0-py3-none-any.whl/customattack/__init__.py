e9c5112aaa65d8be40cce519fe198331c278356a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:22 +0000

    Add package customattack/1.0/customattack-1.0-py3-none-any.whl

---
+++ packages/customattack/customattack-1.0-py3-none-any.whl/customattack/__init__.py
@@ -0,0 +1,27 @@
+from .attack_args import AttackArgs, CommandLineAttackArgs
+# from .augment_args import AugmenterArgs
+from .dataset_args import DatasetArgs
+from .model_args import ModelArgs
+# from .training_args import TrainingArgs, CommandLineTrainingArgs
+from .attack import Attack
+from .attacker import Attacker
+# from .trainer import Trainer
+from . import (
+    commands
+)
+# from . import (
+#     attack_recipes,
+#     attack_results,
+#     augmentation,
+#     commands,
+#     constraints,
+#     datasets,
+#     goal_function_results,
+#     goal_functions,
+#     loggers,
+#     models,
+#     search_methods,
+#     shared,
+#     transformations,
+# )
+name = "customattack"
