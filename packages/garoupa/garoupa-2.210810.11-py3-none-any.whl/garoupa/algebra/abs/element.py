cde7d2afbd2ffa04311f7551b295eca9a4019793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:06 +0000

    Add package garoupa/2.210810.11/garoupa-2.210810.11-py3-none-any.whl

---
+++ packages/garoupa/garoupa-2.210810.11-py3-none-any.whl/garoupa/algebra/abs/element.py
@@ -0,0 +1,112 @@
+#  Copyright (c) 2021. Davi Pereira dos Santos
+#  This file is part of the garoupa project.
+#  Please respect the license - more about this in the section (*) below.
+#
+#  garoupa is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  garoupa is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with garoupa.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  (*) Removing authorship by any means, e.g. by distribution of derived
+#  works or verbatim, obfuscated, compiled or rewritten versions of any
+#  part of this work is a crime and is unethical regarding the effort and
+#  time spent here.
+import operator
+from abc import abstractmethod, ABC
+from dataclasses import dataclass
+from functools import reduce
+from math import log
+
+from garoupa import Hash
+
+
+@dataclass
+class Element(ABC):
+    i: int
+    order: int
+    _id = None
+    _hash = None
+
+    def __post_init__(self):
+        """
+        Usage
+        >>> from garoupa.algebra.dihedral import Ds
+        >>> Ds(64**2, 64**32).bits - 1
+        192.0
+        """
+        self.name = f"{self.__class__.__name__.lower()}_{self.i}"
+        self.bits = log(self.order, 2)
+
+    @abstractmethod
+    def __mul__(self, other):
+        pass
+
+    def __xor__(self, other):
+        """
+        Usage
+        >>> from garoupa.algebra.dihedral import Ds
+        >>> Ds(64**2,64**5) ^ 3
+        ds4096
+        """
+        return reduce(operator.mul, [self] * other)
+
+    __pow__ = __xor__
+
+    def __repr__(self):
+        """
+        Usage
+        >>> from garoupa.algebra.dihedral import Ds
+        >>> Ds(64**3, 64**5) ^ 3
+        ds262144
+        """
+        return self.name
+
+    def __eq__(self, other):
+        """
+        Usage
+        >>> from garoupa.algebra.dihedral import Ds
+        >>> Ds(64**2, 64**5) ^ 3 == Ds(4096, 64**5)
+        True
+        """
+        return self.name == other.name
+
+    def __hash__(self):
+        """
+        Usage
+        >>> from garoupa.algebra.dihedral import Ds
+        >>> isinstance(hash(Ds(64**2,64**5)), int)
+        True
+        """
+        return hash(repr(self))
+
+    @property
+    def hash(self):
+        """
+        Usage
+        >>> from garoupa.algebra.dihedral import Ds
+        >>> Ds(64**2,64**5).hash.id
+        '0000000000000000000000000000000000000000000000000000000000000100'
+        """
+        if self._hash is None:
+            self._hash = Hash.fromn(self.i)
+        return self._hash
+
+    @property
+    def id(self):
+        """
+        Usage
+        >>> from garoupa.algebra.dihedral import Ds
+        >>> Ds(64**2,64**5).id
+        '0000000000000000000000000000000000000000000000000000000000000100'
+        """
+        if self._id is None:
+            self._id = self.hash.id
+        return self._id
---
+++ packages/garoupa/garoupa-2.210810.11-py3-none-any.whl/garoupa/math.py
@@ -0,0 +1,212 @@
+#  Copyright (c) 2021. Davi Pereira dos Santos
+#  This file is part of the garoupa project.
+#  Please respect the license - more about this in the section (*) below.
+#
+#  garoupa is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  garoupa is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with garoupa.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  (*) Removing authorship by any means, e.g. by distribution of derived
+#  works or verbatim, obfuscated, compiled or rewritten versions of any
+#  part of this work is a crime and is unethical regarding the effort and
+#  time spent here.
+
+##############################################################################
+# Abstract Algebra helper functions
+##############################################################################
+
+def int2pmat(number, side):
+    """Convert number into permutation.
+
+    Pads to side.
+
+    Usage
+    >>> int2pmat(4, 4)
+    [0, 2, 1, 3]
+
+    Parameters
+    ----------
+    number
+    side
+
+    Returns
+    -------
+
+    """
+    available = list(range(side))
+    mat = []
+    for i in range(side, 0, -1):
+        number, r = divmod(number, i)
+        mat.append(available.pop(r))
+    mat.extend(available)
+    return mat
+
+
+def pmat2int(matrix):
+    """Convert permutation to number.
+
+    Usage
+    >>> pmat2int([0, 2, 1, 3])
+    4
+
+    Parameters
+    ----------
+    matrix
+
+    Returns
+    -------
+
+    """
+    radix = len(matrix)
+    available = list(range(radix))
+    i = 1
+    res = 0
+    for row in matrix:
+        idx = available.index(row)
+        del available[idx]
+        res += idx * i
+        i *= radix
+        radix -= 1
+    return res
+
+
+def pmat_mult(a, b):
+    """Multiply two permutations.
+
+    Parameters
+    ----------
+    a
+        list of positive integers plus zero
+    b
+        list of positive integers plus zero
+
+    Returns
+    -------
+
+    """
+    if len(a) != len(b):
+        raise Exception("a and b should have same length.")
+    return [a[x] for x in b]
+
+
+def pmat_transpose(m):
+    """Transpose a permutation.
+
+    Original author (CC BY-SA 4.0 LICENSE):
+    https://codereview.stackexchange.com/questions/241511/how-to-efficiently-fast-calculate-the-transpose-of-a-permutation-matrix-in-p/241524?noredirect=1#comment473994_241524
+
+    Parameters
+    ----------
+    m
+        list of positive integers plus zero
+
+    Returns
+    -------
+        list of positive integers plus zero
+    """
+    n = len(m)
+    tr_ls = [0] * n
+
+    for l in m:
+        tr_ls[n - 1 - m[l]] = n - 1 - l
+
+    return tr_ls
+
+
+def pmat_inv(m):
+    size = len(m)
+    r = list(range(size))
+    for i in range(size):
+        r[m[i]] = i
+    return r
+
+
+##############################################################################
+# Hash class helper functions
+##############################################################################
+
+def m4m(a, b, mod):
+    """Multiply two unitriangular matrices 4x4 modulo 'mod'.
+
+    'a' and 'b' given as lists in the format: [a5, a4, a3, a2, a1, a0]
+
+    1 a4 a1 a0
+    0  1 a2 a3
+    0  0  1 a5
+    0  0  0  1
+
+    >>> a, b = [51,18340,56,756,456,344], [781,2340,9870,1234,9134,3134]
+    >>> m4m(b, m4inv(b, 4294967291), 4294967291) == [0,0,0,0,0,0]
+    True
+    >>> c = m4m(a, b, 4294967291)
+    >>> m4m(c, m4inv(b, 4294967291), 4294967291) == a
+    True
+    """
+    return [
+        (a[0] + b[0]) % mod,
+        (a[1] + b[1]) % mod,
+        (a[2] + b[2] + a[3] * b[0]) % mod,
+        (a[3] + b[3]) % mod,
+        (a[4] + b[4] + a[1] * b[3]) % mod,
+        (a[5] + b[5] + a[1] * b[2] + a[4] * b[0]) % mod
+    ]
+
+
+def m4inv(m, mod):
+    """Inverse of unitriangular matrix modulo 'mod'
+
+    'm' given as a list in the format: [a5, a4, a3, a2, a1, a0]
+
+    1 a4 a1 a0
+    0  1 a2 a3
+    0  0  1 a5
+    0  0  0  1
+
+    Based on https://groupprops.subwiki.org/wiki/Unitriangular_matrix_group:UT(4,p)
+
+    >>> e = [42821,772431,428543,443530,42121,7213]
+    >>> m4inv(m4inv(e, 4294967291), 4294967291)==e
+    True
+    """
+    return [
+        -m[0] % mod,
+        -m[1] % mod,
+        (m[3] * m[0] - m[2]) % mod,
+        -m[3] % mod,
+        (m[1] * m[3] - m[4]) % mod,
+        (m[1] * m[2] + m[4] * m[0] - m[1] * m[3] * m[0] - m[5]) % mod
+    ]
+
+
+def int2m4(num, mod):
+    """
+    >>> e = [42821,772431,428543,443530,42121,7213]
+    >>> e == int2m4(m42int(e,4294967291), 4294967291)
+    True
+    """
+    m = [0, 0, 0, 0, 0, 0]
+    num, m[5] = divmod(num, mod)
+    num, m[4] = divmod(num, mod)
+    num, m[3] = divmod(num, mod)
+    num, m[2] = divmod(num, mod)
+    num, m[1] = divmod(num, mod)
+    _, m[0] = divmod(num, mod)
+    return m
+
+
+def m42int(m, mod):
+    """
+    >>> n = 986723489762345987253897254295863
+    >>> m42int(int2m4(n, 4294967291), 4294967291) == n
+    True
+    """
+    return m[5] + m[4] * mod + m[3] * (mod ** 2) + m[2] * (mod ** 3) + m[1] * (mod ** 4) + m[0] * (mod ** 5)
