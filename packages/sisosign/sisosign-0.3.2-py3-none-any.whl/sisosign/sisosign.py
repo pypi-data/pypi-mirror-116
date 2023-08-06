ae146b0f9de3860674965641f2bad83fc63dc8f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:50 +0000

    Add package sisosign/0.3.2/sisosign-0.3.2-py3-none-any.whl

---
+++ packages/sisosign/sisosign-0.3.2-py3-none-any.whl/sisosign/sisosign.py
@@ -0,0 +1,26 @@
+import random
+from typing import List
+
+import click
+
+
+@click.command()
+@click.argument("people", nargs=-1)
+def sisosign(people: List[str]) -> None:
+    """Can't decide who should stay on the story?
+    Why not have a computer make these tough choices for you?
+
+    PEOPLE is a list of one or more names. Only one will be chosen to stay.
+
+    If one of your PEOPLE's names is more than one word,
+    please use "double quotes" around their name.
+    """
+    if len(people) < 1:
+        print("Please provide at least one name or use the --help option.")
+        exit(1)
+    p = random.choice(people)
+    print(f"{p} should stay")
+
+
+if __name__ == "__main__":
+    sisosign()
