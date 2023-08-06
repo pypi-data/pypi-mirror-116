89f9d47eaa0e2afd9a5655859e6251fb00ec3836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:17 +0000

    Add package owl-pipeline-server/0.6.0/owl-pipeline-server-0.6.0.tar.gz

---
+++ packages/owl-pipeline-server/owl-pipeline-server-0.6.0.tar.gz/owl-pipeline-server-0.6.0/README.md
@@ -0,0 +1,5 @@
+# Owl Job Scheduler
+
+Owl is a job scheduler that allows to submit user jobs to a Kubernetes cluster from anywhere.
+
+Documentation in https://eddienko.github.io/owl-pipeline/
---
+++ packages/owl-pipeline-server/owl-pipeline-server-0.6.0.tar.gz/owl-pipeline-server-0.6.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import find_packages, setup
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+with open("requirements.txt") as requirements_file:
+    requirements = [item.strip() for item in requirements_file.readlines()]
+
+with open("classifiers.txt") as classifiers_file:
+    classifiers = [item.strip() for item in classifiers_file.readlines()]
+
+setup_requirements = ["pytest-runner", "flake8"]
+
+test_requirements = ["coverage", "pytest", "pytest-cov", "pytest-mock"]
+
+setup(
+    author="Eduardo Gonzalez Solares",
+    author_email="e.gonzalezsolares@gmail.com",
+    classifiers=classifiers,
+    description="Owl Pipeline Server Framework",
+    entry_points={
+        "console_scripts": [
+            "owl-server=owl_server.cli:main",
+            "owl-migrate=owl_server.migrations.migrate:main",
+        ]
+    },
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    long_description=readme,
+    keywords="owl, pipeline, dask, kubernetes, python",
+    name="owl-pipeline-server",
+    packages=find_packages(include=["owl_server*"]),
+    setup_requires=setup_requirements,
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/eddienko/owl-pipeline-server",
+    version="0.6.0",
+    zip_safe=False,
+    python_requires=">=3.7",
+)
