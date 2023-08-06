404868fe7d346334ed991de610c0b36a936d92a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:35 +0000

    Add package deepedgeai/0.0.1/deepedgeai-0.0.1.tar.gz

---
+++ packages/deepedgeai/deepedgeai-0.0.1.tar.gz/deepedgeai-0.0.1/deepedgeai/about.py
@@ -0,0 +1,4 @@
+class About:
+    def __init__(self) -> None:
+        print("DeepEdge has emerged as world’s leading full-stack EdgeAI company, providing a broad range of platforms and services, enabling companies to rapidl build and deploy products with EdgeAI capabilities.  ")
+        print("contact: projects@deepedge.ai")
