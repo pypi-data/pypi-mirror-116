8628e8a73e9cebac1b86dee87f61fc24d2361859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:38 +0000

    Add package Extre/0.23/Extre-0.23.tar.gz

---
+++ packages/Extre/Extre-0.23.tar.gz/Extre-0.23/Extre/func.py
@@ -0,0 +1,35 @@
+
+import asyncio
+import os
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+from Extre.connections import client_connection, redis_connection
+from Extre import *
+
+async def bash(cmd):
+    process = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await process.communicate()
+    err = stderr.decode().strip()
+    out = stdout.decode().strip()
+    return out, err
+
+
+def time_formatter(milliseconds: int) -> str:
+    """Inputs time in milliseconds, to get beautified time,
+    as string"""
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = (
+        ((str(days) + " day(s), ") if days else "")
+        + ((str(hours) + " hour(s), ") if hours else "")
+        + ((str(minutes) + " minute(s), ") if minutes else "")
+        + ((str(seconds) + " second(s), ") if seconds else "")
+        + ((str(milliseconds) + " millisecond(s), ") if milliseconds else "")
+    )
+    return tmp[:-2]
