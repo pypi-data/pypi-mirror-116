ab0efdcfc3b5e2e8855c181588cdc57f10e98b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:28 +0000

    Add package ctpa-cirs063/0.1.0.0/ctpa_cirs063-0.1.0.0.tar.gz

---
+++ packages/ctpa-cirs063/ctpa_cirs063-0.1.0.0.tar.gz/ctpa_cirs063-0.1.0.0/README.md
@@ -0,0 +1,3 @@
+# CT_Phantom_Analysis
+
+Fully automatic analysis for several CT phantoms
---
+++ packages/ctpa-cirs063/ctpa_cirs063-0.1.0.0.tar.gz/ctpa_cirs063-0.1.0.0/setup.py
@@ -0,0 +1,41 @@
+
+
+from setuptools import setup, find_packages
+README = 'README.md'
+
+DESCRIPTION = 'Phantom analysis toolkit to fully automatically analyse several CT phantoms'
+NAME = 'ctpa_cirs063'
+
+VERSION = "0.1.0.0"
+
+
+def readme():
+    with open(README) as f:
+        return f.read()
+
+
+setup(name=NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English'
+      ],
+      keywords='image images medical dicom CT phantom',
+      url='',
+      author='Niels van der Werf',
+      author_email='n.vanderwerf@erasmusmc.nl',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'pydicom',
+          'scipy',
+          'numpy',
+      ],
+      include_package_data=True,
+      zip_safe=False)
