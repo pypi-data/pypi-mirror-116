b56c4941668f2c69cb9c49bdaedfa6fdf415d6f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:02 +0000

    Add package pypixiv/0.2.5/pypixiv-0.2.5.tar.gz

---
+++ packages/pypixiv/pypixiv-0.2.5.tar.gz/pypixiv-0.2.5/setup.py
@@ -0,0 +1,18 @@
+import setuptools  # type: ignore
+from setuptools import setup  # type: ignore
+
+import pypixiv
+
+setup(
+    name="pypixiv",
+    author="Ryu JuHeon",
+    author_email="SaidBySolo@gmail.com",
+    url="https://github.com/Saebasol/pypixiv",
+    version=pypixiv.__version__,
+    long_description=open("README.md", "rt", encoding="UTF8").read(),
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),  # type: ignore
+    package_data={"pypixiv": ["py.typed"]},
+    description="Pixiv ajax wrapper",
+    python_requires=">=3.9",
+)
