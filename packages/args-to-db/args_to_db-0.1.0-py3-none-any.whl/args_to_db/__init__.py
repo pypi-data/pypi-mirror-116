abc4babaa226ac3dac4b5f8f5a5068abe7938a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:14 +0000

    Add package args-to-db/0.1.0/args_to_db-0.1.0-py3-none-any.whl

---
+++ packages/args-to-db/args_to_db-0.1.0-py3-none-any.whl/args_to_db/__init__.py
@@ -0,0 +1,8 @@
+from .run import run
+# from .args import all_combinations
+from .result import write_results
+from .args import arg, flag, option
+
+__all__ = [
+    'run', 'write_results', 'arg', 'flag', 'option'
+]
