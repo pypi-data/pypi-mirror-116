a4e72f9fdc033b0a96c62d0085477a32b12f5c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:38 +0000

    Add package hi-lasso/1.0.0/hi_lasso-1.0.0.tar.gz

---
+++ packages/hi-lasso/hi_lasso-1.0.0.tar.gz/hi_lasso-1.0.0/hi_lasso/glmnet_model.py
@@ -0,0 +1,51 @@
+import glmnet
+import numpy as np
+
+"""
+This is a Python wrapper for the fortran library used in the R package glmnet. 
+While the library includes linear, logistic, Cox, Poisson, and multiple-response Gaussian, 
+only linear and logistic are implemented in this package.
+
+The API follows the conventions of Scikit-Learn, so it is expected to work with tools from that ecosystem.
+
+https://github.com/civisanalytics/python-glmnet
+"""
+
+
+def AdaptiveLasso(X, y, logistic=False, sample_weight=None, weight_Adaptive=None):
+    """
+    Adaptive Lasso with cross-validation for otpimal lambda
+    """
+    if logistic:
+        enet = glmnet.LogitNet(standardize=False, fit_intercept=False, n_splits=5, scoring='accuracy', alpha=1)
+        enet.fit(X, y, relative_penalties=adaptive_weights, sample_weight = sample_weight)
+    else:
+        enet = glmnet.ElasticNet(standardize=False, fit_intercept=False,
+                                 n_splits=5, scoring='mean_squared_error', alpha=1)
+        enet.fit(X, y, relative_penalties=adaptive_weights, sample_weight=sample_weight)
+    return enet.coef_
+
+
+def ElasticNet(X, y, logistic=False, sample_weight=None):
+    """
+    Elastic Net with cross-validation for otpimal alpha and lambda
+    """
+    mses = np.array([])
+    cv_result_dict = {}
+    if logistic:
+        for i in np.arange(0, 1.1, 0.1):
+            cv_enet = glmnet.LogitNet(standardize=False, fit_intercept=False, n_splits=5, scoring='accuracy',
+                                        alpha=i).fit(X, y, sample_weight=sample_weight)
+            cv_enet.fit(X, y, sample_weight=sample_weight)
+            mses = np.append(mses, cv_enet.cv_mean_score_.max())
+            cv_result_dict[f'cv_result_{i}'] = cv_enet
+    else:
+        for i in np.arange(0, 1.1, 0.1):
+            cv_enet = glmnet.ElasticNet(standardize=False, fit_intercept=False, n_splits=5, scoring='mean_squared_error',
+                                        alpha=i).fit(X, y, sample_weight=sample_weight)
+            cv_enet.fit(X, y, sample_weight=sample_weight)
+            mses = np.append(mses, cv_enet.cv_mean_score_.max())
+            cv_result_dict[f'cv_result_{i}'] = cv_enet
+        
+    cv_max_model = cv_result_dict[f'cv_result_{np.argmax(mses)}']
+    return cv_max_model.coef_
