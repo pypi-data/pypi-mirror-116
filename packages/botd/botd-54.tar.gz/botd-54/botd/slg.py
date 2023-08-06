f42e9c1e7a37e730fd901d080e30bec2f4eb0bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:11 +0000

    Add package botd/54/botd-54.tar.gz

---
+++ packages/botd/botd-54.tar.gz/botd-54/botd/slg.py
@@ -0,0 +1,6 @@
+# This file is in the Public Domain.
+
+"I ask the prosecutor to prosecute the king and members of the chamber for three genocide crimes mentioned -  EM_T04_OTP-CR-117_19 - http://gcid.rtfd.io #ggz !!"
+
+def slg(event):
+    event.reply(__doc__)
