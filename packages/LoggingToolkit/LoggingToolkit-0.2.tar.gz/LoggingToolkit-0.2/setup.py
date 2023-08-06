c3b307eb8813a4ea676531b195295212bb5dd84a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:55 +0000

    Add package LoggingToolkit/0.2/LoggingToolkit-0.2.tar.gz

---
+++ packages/LoggingToolkit/LoggingToolkit-0.2.tar.gz/LoggingToolkit-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+name='LoggingToolkit',
+version=0.2,
+license='MIT',
+author='Surendra Babu Bapatla',
+author_email='ben10.surendra@gmail.com',
+package_dir={'':'src'},
+#url=r'',
+keywords='example project',
+#install_requires=[''],
+packages=find_packages('src'),
+)
