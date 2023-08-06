6468918afc471138b4facb53722501e8e8e548a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:01 +0000

    Add package f1z1-async-emitter/0.2.4/f1z1_async_emitter-0.2.4.tar.gz

---
+++ packages/f1z1-async-emitter/f1z1_async_emitter-0.2.4.tar.gz/f1z1_async_emitter-0.2.4/setup.py
@@ -0,0 +1,15 @@
+# @Time     : 2021/5/26
+# @Project  : f1z1-g
+# @IDE      : PyCharm
+# @Author   : Angel
+# @Email    : 376355670@qq.com
+from scripts.build import build_package, requires
+
+name = "f1z1_async_emitter"
+version = "0.2.4"
+emitter_requires = requires + [
+    "f1z1-async-runner>=0.1.0"
+]
+
+if __name__ == '__main__':
+    build_package(name, version, filename="README.md", install_requires=emitter_requires)
