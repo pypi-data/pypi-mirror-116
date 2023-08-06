193f31a695cf3c47a36d47147ea0b2aec1cee58c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:47 +0000

    Add package pbskrishna/0.0.1/pbskrishna-0.0.1.tar.gz

---
+++ packages/pbskrishna/pbskrishna-0.0.1.tar.gz/pbskrishna-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Just a Simple PushBullet Wrapper for Organisational use'
+LONG_DESCRIPTION = 'Just a Simple PushBullet Wrapper for Organisational use, Still under development.'
+
+# Setting up
+setup(
+    name="pbskrishna",
+    version=VERSION,
+    author=" abhinanddreddrive (Red Drive)",
+    author_email="<abhi.at.gaming@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pushbullet.py','pyodbc','requests'],
+    keywords=['python', 'hello'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
