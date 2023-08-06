822d2fdab33f740054549bc0fbf262a9ade641e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:32 +0000

    Add package spark-tensorflow-distributor/1.0.0/spark_tensorflow_distributor-1.0.0.tar.gz

---
+++ packages/spark-tensorflow-distributor/spark_tensorflow_distributor-1.0.0.tar.gz/spark_tensorflow_distributor-1.0.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="spark_tensorflow_distributor",
+  version="1.0.0",
+  author="sarthfrey",
+  author_email="sarth.frey@gmail.com",
+  description="This package helps users do distributed training with TensorFlow on their Spark clusters.",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/tensorflow/ecosystem/tree/master/spark/spark-tensorflow-distributor",
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Development Status :: 1 - Planning",
+    "Intended Audience :: Developers",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3",
+    "Natural Language :: English",
+    "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Software Development :: Version Control :: Git",
+  ],
+  python_requires='>=3.6',
+)
