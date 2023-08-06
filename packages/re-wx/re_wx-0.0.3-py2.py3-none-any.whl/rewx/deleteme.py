a88ff99d28907c975993e00bd4cdc5bfbb52ca88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:47 +0000

    Add package re-wx/0.0.3/re_wx-0.0.3-py2.py3-none-any.whl

---
+++ packages/re-wx/re_wx-0.0.3-py2.py3-none-any.whl/rewx/deleteme.py
@@ -0,0 +1,20 @@
+
+from random import random, randint
+
+import math
+
+#
+# for i in range(1, 15):
+#     res = math.pow((1 - .3), i)
+#     nothit = 1 - res
+#     print(f"Odds to hit after {i} shots: {nothit * 100}")
+
+
+
+
+print(1 - math.pow(5.0/6, 3))
+
+'''
+1 - (5/6)^n = 0.42129629629629617
+'''
+
