e8cd9c64bfe40b52f8588d02897605005cfb6fab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:19 +0000

    Add package MiniContourFinder/1.0.3/MiniContourFinder-1.0.3.tar.gz

---
+++ packages/MiniContourFinder/MiniContourFinder-1.0.3.tar.gz/MiniContourFinder-1.0.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MiniContourFinder",
+    version="1.0.3",
+    author="Ian S Gilman",
+    author_email="ian.gilman@yale.edu",
+    description="Lightweight image segmentation software for biological images",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wino6687/conda-demo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "opencv >= 4.5.2",
+        "pillow >= 6.2.1",
+        "pyperclip >= 1.7.0",
+        "PyQt5 >= 5.12.3",
+        "pytesseract >= 0.3.0",
+        "tqdm >= 4.36.1"
+    ]
+)
