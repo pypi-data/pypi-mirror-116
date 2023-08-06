0d20a6e2932a7c1f172878f7176e47f4ad09efc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:59 +0000

    Add package genesequence/1.0/genesequence-1.0.tar.gz

---
+++ packages/genesequence/genesequence-1.0.tar.gz/genesequence-1.0/README.md
@@ -0,0 +1,2 @@
+# Genesequence_package
+AWS Scholarship Capstone Project - A light-weight bioinformatics package
---
+++ packages/genesequence/genesequence-1.0.tar.gz/genesequence-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='genesequence',
+      version='1.0',
+      description='Bioinformatics mini library for manipulating DNA/RNA sequences',
+      packages=['genesequence'],
+      author='Callis Ezenwaka',
+      author_email='callisezenwaka@outlook.com',
+      zip_safe=False)
