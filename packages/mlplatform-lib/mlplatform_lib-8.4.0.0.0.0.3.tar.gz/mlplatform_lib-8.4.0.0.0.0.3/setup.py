924c57837523ec47dc791a45de6e5d11c2860ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:43 +0000

    Add package mlplatform-lib/8.4.0.0.0.0.3/mlplatform_lib-8.4.0.0.0.0.3.tar.gz

---
+++ packages/mlplatform-lib/mlplatform_lib-8.4.0.0.0.0.3.tar.gz/mlplatform_lib-8.4.0.0.0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+# version = {hyperdata_version}.{mlplatform_version}.{union_test_version}.{human_number}.{test_number}
+# ex)        8.4.0             .0                   .0.2.1
+# mansu: 0
+# jooyoun: 1
+# yunho: 2
+setup(
+    name="mlplatform_lib",
+    version="8.4.0.0.0.0.3",
+    description="mlplatform_lib",
+    author="mlplatform_lib",
+    author_email="mansu_kim@tmax.co.kr",
+    url="",
+    download_url="",
+    install_requires=["pandas>=1.0.1", "requests>=2.25.1", "dataclasses_json>=0.5.4"],
+    packages=find_packages(exclude=[]),
+    keywords=["mlplatform_lib"],
+    python_requires=">=3.7",
+    package_data={},
+    zip_safe=False,
+    classifiers=["Programming Language :: Python :: 3.7"],
+)
