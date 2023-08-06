8a487ffcc79fa9c758724cd90f244f446cbb4708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:20 +0000

    Add package honda-dashboard/0.1.0/honda-dashboard-0.1.0.tar.gz

---
+++ packages/honda-dashboard/honda-dashboard-0.1.0.tar.gz/honda-dashboard-0.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='honda-dashboard',
+    version='0.1.0',
+    description='OBD2 Honda honda-dashboard.',
+    url='https://github.com/todstoychev/dashboard',
+    author='Todor Todorov',
+    author_email='todstoychev@gmail.com',
+    license='BSD 2-clause',
+    packages=['honda-dashboard'],
+    install_requires=['qtawesome',
+                      'pyqt5',
+                      'obd'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
