0ff7472036084efc10bbf664d382780568f951fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:26 +0000

    Add package clrfterm/1.0.4/clrfterm-1.0.4.tar.gz

---
+++ packages/clrfterm/clrfterm-1.0.4.tar.gz/clrfterm-1.0.4/clrfterm/__init__.py
@@ -0,0 +1,31 @@
+__version__ = "1.0.4"
+
+from sys import stdout
+from typing import Union
+
+from clrfterm.ansi import (
+    Style,
+    Foreground,
+    Background,
+)
+from clrfterm.win import enable_ansi
+
+
+def rprint(
+        text: str,
+        *,
+        foreground: Union[Foreground, str] = "",
+        style: Union[Style, str] = "",
+        background: Union[Background, str] = ""
+) -> None:
+    """
+    stdout.write auto-reset wrapper
+    """
+    stdout.write(foreground + style + background + text + Style.RESET + "\n")
+
+
+def reset() -> None:
+    """
+    function reset
+    """
+    stdout.write(Style.RESET.value)
