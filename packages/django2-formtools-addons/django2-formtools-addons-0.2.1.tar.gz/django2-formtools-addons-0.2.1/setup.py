8a973863c2071a1ea067f39598d9c52a640154c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:32 +0000

    Add package django2-formtools-addons/0.2.1/django2-formtools-addons-0.2.1.tar.gz

---
+++ packages/django2-formtools-addons/django2-formtools-addons-0.2.1.tar.gz/django2-formtools-addons-0.2.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import re
+import sys
+
+import setuptools
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+__version__ = '0.2.1'
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+setuptools.setup(
+    name='django2-formtools-addons',
+    version=__version__,
+    description="""'Addons for Django 2 Formtools'""",
+    long_description=readme + '\n\n' + history,
+    author='VikingCo',
+    author_email='technology@vikingco.com',
+    url='https://github.com/hpe95/django-formtools-addons',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=["django >= 2.1", "six >= 1.9"],
+    extras_require={},
+    license="BSD",
+    zip_safe=False,
+    keywords='django2-formtools-addons',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
