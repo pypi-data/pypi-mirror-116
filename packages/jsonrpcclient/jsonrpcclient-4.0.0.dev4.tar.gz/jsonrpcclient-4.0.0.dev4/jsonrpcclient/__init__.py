e350f9ab1839da8caf9c3c19b3f5210f0bfbc201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:25 +0000

    Add package jsonrpcclient/4.0.0.dev4/jsonrpcclient-4.0.0.dev4.tar.gz

---
+++ packages/jsonrpcclient/jsonrpcclient-4.0.0.dev4.tar.gz/jsonrpcclient-4.0.0.dev4/jsonrpcclient/__init__.py
@@ -0,0 +1,12 @@
+from jsonrpcclient.requests import (
+    notification,
+    notification_dict,
+    request,
+    request_dict,
+    request_dict_hexadecimal,
+    request_dict_random,
+    request_dict_uuid,
+    request_hex,
+    request_random,
+    request_uuid,
+)
