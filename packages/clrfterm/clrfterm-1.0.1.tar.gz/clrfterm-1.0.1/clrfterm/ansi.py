6f7295cbfa1af030c3b7528d3ad8ab1e0d41ce18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:19 +0000

    Add package clrfterm/1.0.1/clrfterm-1.0.1.tar.gz

---
+++ packages/clrfterm/clrfterm-1.0.1.tar.gz/clrfterm-1.0.1/clrfterm/ansi.py
@@ -0,0 +1,44 @@
+from enum import Enum
+
+
+class AnsiEscape(str, Enum):
+    """
+    Base class
+    """
+
+
+class Foreground(AnsiEscape):
+    """
+    ANSI escape code for foreground
+    """
+    BLACK         = "\033[30m"
+    RED           = "\033[31m"
+    GREEN         = "\033[32m"
+    YELLOW        = "\033[33m"
+    BLUE          = "\033[34m"
+    MAGENTA       = "\033[35m"
+    CYAN          = "\033[36m"
+    WHITE         = "\033[37m"
+
+
+class Backgrounds(AnsiEscape):
+    """
+    ANSI escape code for backgrounds
+    """
+    BLACK         = "\033[40m"
+    RED           = "\033[41m"
+    GREEN         = "\033[42m"
+    YELLOW        = "\033[43m"
+    BLUE          = "\033[44m"
+    MAGENTA       = "\033[45m"
+    CYAN          = "\033[46m"
+    WHITE         = "\033[47m"
+
+
+class Styles(AnsiEscape):
+    """
+    ANSI escape code for styles
+    """
+    BOLD          = "\033[1m"
+    UNDERLINE     = "\033[4m"
+    RESET         = "\033[0m"
