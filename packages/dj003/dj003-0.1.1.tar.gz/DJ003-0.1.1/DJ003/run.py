604faf6afcf7f1e38a559fff156294463ffccce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:34 +0000

    Add package dj003/0.1.1/dj003-0.1.1.tar.gz

---
+++ packages/dj003/dj003-0.1.1.tar.gz/DJ003-0.1.1/DJ003/run.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+  @Time : 2021/8/6 17:41 
+  @Auth : 任阳光
+  @File : run.py
+  @IDE  : PyCharm
+  @firm : 湖南麒麟信息工程技术有限公司
+  @web  : http://www.kylinos.com.cn/
+-------------------------------------------------
+"""
+
+
+def main():
+    print("This is a demo!")
+
+
+main()
