b823b77af20faf01a5688d09fcd8e186186c3efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:42 +0000

    Add package jupyterlab-tabnine/0.0.17/jupyterlab_tabnine-0.0.17.tar.gz

---
+++ packages/jupyterlab-tabnine/jupyterlab_tabnine-0.0.17.tar.gz/jupyterlab_tabnine-0.0.17/src/consts.ts
@@ -0,0 +1,10 @@
+export const CHAR_LIMIT = 100_00;
+
+export const API_VERSION = "3.5.34";
+
+export const COMPLETION_CHARS =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ(=[%/{+#.,\\<+-|&*=$#@!";
+
+export const MAX_RESULTS = 5;
+
+export const TABNINE_LOGO = "⌬";
