cfbceab480a2e9712668b5232960493f834cd9fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:46 +0000

    Add package index.py/0.19.0rc1/index.py-0.19.0rc1.tar.gz

---
+++ packages/index.py/index.py-0.19.0rc1.tar.gz/index.py-0.19.0rc1/indexpy/applications.py
@@ -0,0 +1,268 @@
+from __future__ import annotations
+
+import copy
+import dataclasses
+import functools
+import inspect
+import sys
+import traceback
+from functools import reduce
+from pathlib import PurePath
+from types import AsyncGeneratorType
+from typing import (
+    Any,
+    AsyncGenerator,
+    Awaitable,
+    Callable,
+    Dict,
+    Iterable,
+    List,
+    Mapping,
+    NoReturn,
+    Optional,
+    Type,
+    TypeVar,
+    Union,
+)
+
+if sys.version_info[:2] < (3, 8):
+    from typing_extensions import Literal
+else:
+    from typing import Literal
+
+from baize.asgi import Receive, Scope, Send
+from baize.datastructures import URL
+from baize.typing import ASGIApp, ServerSentEvent
+from baize.utils import cached_property
+
+from .debug import DebugMiddleware
+from .exceptions import ErrorView, ExceptionContextManager, HTTPException
+from .requests import HttpRequest, WebSocket, request_var, websocket_var
+from .responses import (
+    FileResponse,
+    HttpResponse,
+    JSONResponse,
+    PlainTextResponse,
+    RedirectResponse,
+    SendEventResponse,
+    convert_response,
+)
+from .routing import BaseRoute, NoMatchFound, Router
+from .templates import BaseTemplates
+from .utils import F, State
+
+NoArgumentCallable = Union[Callable[[], Any], Callable[[], Awaitable[Any]]]
+
+
+@dataclasses.dataclass
+class Lifespan:
+    on_startup: List[NoArgumentCallable] = dataclasses.field(default_factory=list)
+    on_shutdown: List[NoArgumentCallable] = dataclasses.field(default_factory=list)
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        """
+        Handle ASGI lifespan messages, which allows us to manage application
+        startup and shutdown events.
+        """
+        message = await receive()
+        assert message["type"] == "lifespan.startup"
+        try:
+            for handler in self.on_startup:
+                result = handler()
+                if inspect.isawaitable(result):
+                    await result
+        except BaseException:
+            msg = traceback.format_exc()
+            await send({"type": "lifespan.startup.failed", "message": msg})
+            raise
+        await send({"type": "lifespan.startup.complete"})
+
+        message = await receive()
+        assert message["type"] == "lifespan.shutdown"
+        try:
+            for handler in self.on_shutdown:
+                result = handler()
+                if inspect.isawaitable(result):
+                    await result
+        except BaseException:
+            msg = traceback.format_exc()
+            await send({"type": "lifespan.shutdown.failed", "message": msg})
+            raise
+        await send({"type": "lifespan.shutdown.complete"})
+
+
+@dataclasses.dataclass
+class FactoryClass:
+    http: Type[HttpRequest] = HttpRequest
+    websocket: Type[WebSocket] = WebSocket
+
+
+T_NoArgumentCallable = TypeVar("T_NoArgumentCallable", bound=NoArgumentCallable)
+
+
+class Index:
+    def __init__(
+        self,
+        *,
+        debug: bool = False,
+        templates: Optional[BaseTemplates] = None,
+        on_startup: List[NoArgumentCallable] = [],
+        on_shutdown: List[NoArgumentCallable] = [],
+        routes: Iterable[BaseRoute] = [],
+        exception_handlers: Dict[int | Type[Exception], ErrorView] = {},
+        factory_class: FactoryClass = FactoryClass(),
+    ) -> None:
+        self.should_exit = False
+        self.__dict__["debug"] = debug
+        self.factory_class = factory_class
+        self.templates = templates
+        self.router = Router(routes)
+        self.lifespan = Lifespan(copy.copy(on_startup), copy.copy(on_shutdown))
+        self.exception_contextmanager = ExceptionContextManager(exception_handlers)
+        self.asgi_middlewares: List[F] = []
+        # We expect to be able to catch all code errors, so as an ASGI middleware.
+        self.app_with_debug = DebugMiddleware(
+            app=self.build_app_with_middlewares(), debug=self.debug
+        )
+
+    @property
+    def debug(self) -> bool:
+        return self.__dict__.get("debug", False)
+
+    @debug.setter
+    def debug(self, value: bool) -> None:
+        self.__dict__["debug"] = bool(value)
+        self.app_with_debug.debug = bool(value)
+
+    def build_app_with_middlewares(self) -> ASGIApp:
+        return reduce(lambda a, m: m(a), self.asgi_middlewares, self.app)
+
+    def add_middleware(self, middleware_class: type, **options: Any) -> None:
+        """
+        Add ASGI middleware
+        """
+        self.asgi_middlewares.append(F(middleware_class, **options))
+        self.app_with_debug.app = self.build_app_with_middlewares()
+
+    def add_exception_handler(
+        self, exc_class_or_status_code: int | Type[Exception], handler: ErrorView
+    ) -> None:
+        self.exception_contextmanager.add_exception_handler(
+            exc_class_or_status_code, handler
+        )
+
+    def exception_handler(
+        self, exc_class_or_status_code: int | Type[Exception]
+    ) -> Callable[[ErrorView], ErrorView]:
+        def decorator(func: ErrorView) -> ErrorView:
+            self.add_exception_handler(exc_class_or_status_code, func)
+            return func
+
+        return decorator
+
+    def on_startup(self, func: T_NoArgumentCallable) -> T_NoArgumentCallable:
+        self.lifespan.on_startup.append(func)
+        return func
+
+    def on_shutdown(self, func: T_NoArgumentCallable) -> T_NoArgumentCallable:
+        self.lifespan.on_shutdown.append(func)
+        return func
+
+    @cached_property
+    def state(self) -> State:
+        return State()
+
+    @cached_property
+    def response_convertor(self):
+        @functools.singledispatch
+        def automatic(*args: Any) -> HttpResponse:
+            raise TypeError(
+                f"Cannot find automatic handler for this type: {type(args[0])}"
+            )
+
+        @automatic.register(HttpResponse)
+        def raw_response(response: HttpResponse, *args: Any) -> HttpResponse:
+            return response
+
+        @automatic.register(type(None))
+        def _none(ret: Type[None]) -> NoReturn:
+            raise TypeError(
+                "Get 'None'. Maybe you need to add a return statement to the function."
+            )
+
+        @automatic.register(tuple)
+        @automatic.register(list)
+        @automatic.register(dict)
+        def _json(
+            body: tuple | list | dict,
+            status: int = 200,
+            headers: Mapping[str, str] = None,
+        ) -> JSONResponse:
+            return JSONResponse(body, status, headers)
+
+        @automatic.register(str)
+        @automatic.register(bytes)
+        def _plain_text(
+            body: str | bytes,
+            status: int = 200,
+            headers: Mapping[str, str] = None,
+        ) -> PlainTextResponse:
+            return PlainTextResponse(body, status, headers)
+
+        @automatic.register(AsyncGeneratorType)
+        def _send_event(
+            generator: AsyncGenerator[ServerSentEvent, None],
+            status: int = 200,
+            headers: Mapping[str, str] = None,
+        ) -> SendEventResponse:
+            return SendEventResponse(generator, status, headers)
+
+        @automatic.register(PurePath)
+        def _file(filepath: PurePath, download_name: str = None) -> FileResponse:
+            return FileResponse(str(filepath), download_name=download_name)
+
+        @automatic.register(URL)
+        def _redirect(
+            url: URL, status: int = 307, headers: Mapping[str, str] = None
+        ) -> RedirectResponse:
+            return RedirectResponse(url, status_code=status, headers=headers)
+
+        return automatic
+
+    async def app(self, scope: Scope, receive: Receive, send: Send) -> None:
+        scope_type: Literal["lifespan", "http", "websocket"] = scope["type"]
+        return await getattr(self, scope_type)(scope, receive, send)
+
+    async def http(self, scope: Scope, receive: Receive, send: Send) -> None:
+        request = self.factory_class.http(scope, receive, send)
+        token = request_var.set(request)
+        try:
+            async with self.exception_contextmanager:
+                try:
+                    path_params, handler = self.router.search("http", request["path"])
+                    request["path_params"] = path_params
+                    response = convert_response(await handler())
+                except NoMatchFound:
+                    raise HTTPException(404)
+                else:
+                    return await response(scope, receive, send)
+        finally:
+            request_var.reset(token)
+
+    async def websocket(self, scope: Scope, receive: Receive, send: Send) -> None:
+        websocket = self.factory_class.websocket(scope, receive, send)
+        token = websocket_var.set(websocket)
+        try:
+            path_params, handler = self.router.search("websocket", websocket["path"])
+            websocket["path_params"] = path_params
+        except NoMatchFound:
+            return await websocket.close(1001)
+        else:
+            return await handler()
+        finally:
+            websocket_var.reset(token)
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        scope["app"] = self
+
+        await self.app_with_debug(scope, receive, send)
