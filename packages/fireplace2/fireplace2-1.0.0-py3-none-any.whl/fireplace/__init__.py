4d383740b981725bab6e4772d912c84c214517f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:58 +0000

    Add package fireplace2/1.0.0/fireplace2-1.0.0-py3-none-any.whl

---
+++ packages/fireplace2/fireplace2-1.0.0-py3-none-any.whl/fireplace/__init__.py
@@ -0,0 +1,5 @@
+import pkg_resources
+
+__author__ = "Jerome Leclanche"
+__email__ = "jerome@leclan.ch"
+__version__ = pkg_resources.require("fireplace2")[0].version
---
+++ packages/fireplace2/fireplace2-1.0.0-py3-none-any.whl/fireplace/rules.py
@@ -0,0 +1,12 @@
+"""
+Base game rules (events, etc)
+"""
+from .cards.utils import *
+
+HEAVILY_ARMORED = [Predamage(SELF, lambda i: i > 1).on(Predamage(SELF, 1))]
+
+
+class WeaponRules:
+    base_events = [
+        Attack(FRIENDLY_HERO).after(Hit(SELF, 1))
+    ]
