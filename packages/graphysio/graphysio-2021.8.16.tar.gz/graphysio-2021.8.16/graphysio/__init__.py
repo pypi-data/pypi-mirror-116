305daaaa62b92425dd895b29f8c86931dc71036e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:03 +0000

    Add package graphysio/2021.8.16/graphysio-2021.8.16.tar.gz

---
+++ packages/graphysio/graphysio-2021.8.16.tar.gz/graphysio-2021.8.16/graphysio/__init__.py
@@ -0,0 +1,17 @@
+"""Graphical time series visualizer and analyzer."""
+
+__version__ = '2021.08.16'
+
+__all__ = [
+    'algorithms',
+    'dialogs',
+    'exporter',
+    'legend',
+    'mainui',
+    'puplot',
+    'tsplot',
+    'utils',
+    'types',
+    'ui',
+    'transformations',
+]
