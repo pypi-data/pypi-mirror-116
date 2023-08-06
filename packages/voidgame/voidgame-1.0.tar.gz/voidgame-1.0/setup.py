08962f32e00c61a35e6e852efd57d33294196cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:51 +0000

    Add package voidgame/1.0/voidgame-1.0.tar.gz

---
+++ packages/voidgame/voidgame-1.0.tar.gz/voidgame-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+	name="voidgame",
+	version="1.0",
+	author="Joshua Blavatt",
+	author_email="josh@joshuablavatt.dev",
+	description="A simple interface for a unique game",
+	url="https://github.com/jpblavatt/voidgame",
+	license="MIT",
+	python_requires=">=3.8",
+	py_modules=["voidgame"],
+)
