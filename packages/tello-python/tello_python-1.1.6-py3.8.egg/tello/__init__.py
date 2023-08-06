6d83681ba66c1c2bb4cc2a786d348b36e40a5939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:09 +0000

    Add package tello-python/1.1.6/tello_python-1.1.6-py3.8.egg

---
+++ packages/tello-python/tello_python-1.1.6-py3.8.egg/tello/__init__.py
@@ -0,0 +1,9 @@
+__title__ = 'tello-python'
+__author__ = 'C灵C'
+__liscence__ = 'MIT'
+__copyright__ = 'Copyright 2021 C灵C'
+__version__ = '1.1.6'
+__all__ = ['tello', 'stats']
+
+from .tello import Tello
+from .stats import Stats
