1346b9b6d1fc5c5aa6a2c2d0c6c8c62267e8f0e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:37 +0000

    Add package jusTextnext/2.2.6/jusTextnext-2.2.6.tar.gz

---
+++ packages/jusTextnext/jusTextnext-2.2.6.tar.gz/jusTextnext-2.2.6/setup.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) 2011 Jan Pomikalek
+
+This software is licensed as described in the file LICENSE.rst.
+"""
+
+from __future__ import with_statement
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open("README.rst") as readme:
+    with open("CHANGELOG.rst") as changelog:
+        long_description = readme.read() + "\n\n" + changelog.read()
+
+
+setup(
+    name="jusTextnext",
+    version="2.2.6",
+    description="Heuristic based boilerplate removal tool",
+    long_description=long_description,
+    author="Jan Pomikálek",
+    author_email="jan.pomikalek@gmail.com",
+    maintainer="Muhammad Azamuddin",
+    maintainer_email="mas.azamuddin@gmail.com",
+    url="https://github.com/azamuddin/jusTextnext",
+    license="The BSD 2-Clause License",
+    install_requires=[
+        'lxml >= 4.4.2',
+    ],
+    tests_require=[
+        "pytest",
+        "pytest-cov",
+        "coverage",
+    ],
+    packages=["justextnext"],
+    package_data={"justextnext": ["stoplists/*.txt"]},
+    classifiers=(
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Pre-processors",
+        "Topic :: Text Processing :: Filters",
+        "Topic :: Text Processing :: Markup :: HTML",
+    ),
+)
