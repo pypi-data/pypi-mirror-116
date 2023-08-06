246ba82841daac9340d2b7333bd5a6beed14d5ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:22 +0000

    Add package stonkgs/0.1.4/stonkgs-0.1.4.tar.gz

---
+++ packages/stonkgs/stonkgs-0.1.4.tar.gz/stonkgs-0.1.4/src/stonkgs/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+"""STonKGs."""
+
+from .api.api import (
+    infer_cell_lines,
+    infer_concat,
+    infer_correct_binary,
+    infer_correct_multiclass,
+    infer_diseases,
+    infer_locations,
+    infer_species,
+)
+from .models.stonkgs_finetuning import STonKGsForSequenceClassification
+from .models.stonkgs_for_embeddings import get_stonkgs_embeddings, preprocess_df_for_embeddings
+from .models.stonkgs_model import STonKGsForPreTraining
+
+__all__ = [
+    "get_stonkgs_embeddings",
+    "preprocess_df_for_embeddings",
+    "STonKGsForPreTraining",
+    "STonKGsForSequenceClassification",
+    "infer_cell_lines",
+    "infer_concat",
+    "infer_correct_binary",
+    "infer_correct_multiclass",
+    "infer_diseases",
+    "infer_locations",
+    "infer_species",
+]
