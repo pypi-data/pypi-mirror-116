f3e3a602c0f738755cea8c7a9c27c488caab1fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:57 +0000

    Add package PyRPStream/0.4.0/PyRPStream-0.4.0.tar.gz

---
+++ packages/PyRPStream/PyRPStream-0.4.0.tar.gz/PyRPStream-0.4.0/PyRPStream/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.4.0'
+__author__ = 'Robert James, Fiona Alder'
+__author_email__ = 'robert.james.19@ucl.ac.uk'
+
+from .utils import *
+from .socket_thread import *
+from .stream_data import *
