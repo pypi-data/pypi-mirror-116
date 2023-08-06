a9fbee008df9c54efa08c58a661a58eee2140360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:35 +0000

    Add package EasyMyWeb/0.2/EasyMyWeb-0.2-py3-none-any.whl

---
+++ packages/EasyMyWeb/EasyMyWeb-0.2-py3-none-any.whl/easymyweb/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from typing import *
+from .runner import run as runner_run
+
+
+class EasyMyWeb(Flask):
+    def __init__(
+        self,
+        import_name: str,
+        *args, **kwargs
+    ):
+        super().__init__(import_name=import_name,
+                         *args, **kwargs)
+
+    def run(self, host: Optional[str] = None, port: Optional[int] = None, debug=False, **kwargs):
+        if debug:
+            self.logger.warning(
+                "EasyMyWebRunner does not support debug mode. Using fallback server and it may cause a lower performance. Also, please disable debug in production environment.")
+            Flask.run(self, host=host, port=port, debug=debug, **kwargs)
+        else:
+            runner_run(self, host=host, port=port, **kwargs)
