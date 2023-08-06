5612f8219d976e2b6e46da56ee9173fbcd580d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:44 +0000

    Add package PyViCare/2.6/PyViCare-2.6.tar.gz

---
+++ packages/PyViCare/PyViCare-2.6.tar.gz/PyViCare-2.6/PyViCare/PyViCareCachedService.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+import threading
+from PyViCare.PyViCareService import ViCareService, readFeature
+
+
+class ViCareTimer:
+    # class is used to replace logic in unittest
+    def now(self):
+        return datetime.now()
+
+
+class ViCareCachedService(ViCareService):
+
+    def __init__(self, oauth_manager, accessor, cacheDuration):
+        ViCareService.__init__(self, oauth_manager, accessor)
+        self.__cacheDuration = cacheDuration
+        self.__cache = None
+        self.__cacheTime = None
+        self.__lock = threading.Lock()
+
+    def getProperty(self, property_name):
+        data = self.__get_or_update_cache()
+        entities = data["data"]
+        return readFeature(entities, property_name)
+
+    def setProperty(self, property_name, action, data):
+        response = super().setProperty(property_name, action, data)
+        self.clear_cache()
+        return response
+
+    def __get_or_update_cache(self):
+        with self.__lock:
+            if self.is_cache_invalid():
+                self.__cache = self.fetch_all_features()
+                self.__cacheTime = ViCareTimer().now()
+            return self.__cache
+
+    def is_cache_invalid(self):
+        return self.__cache is None or self.__cacheTime is None or (ViCareTimer().now() - self.__cacheTime).seconds > self.__cacheDuration
+
+    def clear_cache(self):
+        with self.__lock:
+            self.__cache = None
+            self.__cacheTime = None
