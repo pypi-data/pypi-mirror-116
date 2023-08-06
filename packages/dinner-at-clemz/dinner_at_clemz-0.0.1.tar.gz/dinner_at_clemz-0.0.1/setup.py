035bf1aed3ee81a7ff3d38f232fb315697ad6b7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:45 +0000

    Add package dinner-at-clemz/0.0.1/dinner_at_clemz-0.0.1.tar.gz

---
+++ packages/dinner-at-clemz/dinner_at_clemz-0.0.1.tar.gz/dinner_at_clemz-0.0.1/setup.py
@@ -0,0 +1,35 @@
+"""Setup for dinner at clementi package."""
+
+from setuptools import setup,find_packages
+
+__pkg_name__ = 'dinner_at_clemz'
+
+
+with open('README.md') as f:
+    README = f.read()
+
+setup(
+    author="meja_bundar_dover_crescent",
+    maintainer_email="dinner_at_clementi@gmail.com",
+    name=__pkg_name__,
+    license="MIT",
+    description='dinner_at_clementi is a python package that is essential for launching the next scalable unicorn in South East Asia.',
+    version='v0.0.1',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/chrishendra93/xpore',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+            ],
+    python_requires=">=3.5",
+    entry_points={'console_scripts': ["display_message=src.main:main"]},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries',
+        'Intended Audience :: Other Audience',
+    ],
+)
---
+++ packages/dinner-at-clemz/dinner_at_clemz-0.0.1.tar.gz/dinner_at_clemz-0.0.1/src/messages/all_messages.py
@@ -0,0 +1,143 @@
+from .tomodachi import Tomodachi
+
+
+class Eko(Tomodachi):
+
+    def __init__(self):
+        name = "Eko"
+        designation = "ex_rumah_kita" 
+        message = '''Dittt!! All the best and good luck di colum yah!! We’re gonna miss u here but see u man in 2023!'''
+        super(Eko, self).__init__(name, designation, message)
+
+
+class Emil(Tomodachi):
+
+    def __init__(self):
+        name = "Emil"
+        designation = "mantan_calon_pacar_ida"
+        message = '''Dit all the best ya di Columbia university. Semoga lu bisa enjoy your stay disana dan bisa mencapai cita2 lu. See you at the top broh'''
+        super(Emil, self).__init__(name, designation, message)
+
+
+class Marco(Tomodachi):
+
+    def __init__(self):
+        name = "Marco"
+        designation = "temen_seperjuangan_acsi"
+        message = '''Dit, wishing you all the success in the US, you have done well in all the things that you do and I am sure you will do so in the future. Keep in touch and look forward to hearing more success stories '''
+        super(Marco, self).__init__(name, designation, message)
+
+
+class Ida(Tomodachi):
+
+    def __init__(self):
+        name = "Ida"
+        designation = "mantan_calon_pacar_emil"
+        message = '''Dude, it’s the end of an era! Truly happy (and a lil bit jealous) for you and your exciting new chapter ahead! Deeply treasure the friendship we have, with the emo-rgencies
+          conversations every now and then. It is always easy to have real talks with you. 
+          I’m gonna miss you loads, dude! I believe in friendships that will not decay no matter how long and how far apart. That’s us! Congratulations!!! I’ll see you when I see you 😬'''
+        super(Ida, self).__init__(name, designation, message)
+
+
+class Karin(Tomodachi):
+    
+    def __init__(self):
+        name = "Karin"
+        designation = "uhuk_uhuk"
+        message = '''Halo Dit, all the best for ur masters in NYC! Travel as much as you can, mumpung bisa (but get brass knuckles/pepper spray just in case). Stay happy and healthy. Keep in touch! :)'''
+        super(Karin, self).__init__(name, designation, message)
+
+
+class Rika(Tomodachi):
+    
+    def __init__(self):
+        name = "Rika"
+        designation = "permanent_resident_rumah_kita"
+        message = '''Have fun ya dittt for ur next chapter in life ✨🤩 Excited for what the future has in store for you in the US!'''
+        super(Rika, self).__init__(name, designation, message)
+
+
+class Zefa(Tomodachi):
+
+    def __init__(self):
+        name = "Zefa"
+        designation = ""
+        message = '''Adit!!! Lo ud mau cabut aja ☹️ gw inget pertama kali kita ktmu pas sec school orientation yg yall anak2 ACSI bikin sndiri sm anak2 MGS, smntara gw the odd one out dari sekolah lain yg asal ikutan. 
+          Idk about others tapi menurut gw lo itu quite wholesome...
+          at least vs batchmates lain yg mayan shady 🤣 all the best in your further studies! Be careful dan carry pepper spray or smth - it's real wild out there.'''
+        super(Zefa, self).__init__(name, designation, message)
+
+
+class Jason(Tomodachi):
+
+    def __init__(self):
+        name = "Jason"
+        designation = "punya_toko_hamster"
+        message = '''All the best Dit di US! -Jason,2021'''
+        super(Jason, self).__init__(name, designation, message)
+
+
+class Kafi(Tomodachi):
+
+    def __init__(self):
+        name = "Kafi"
+        designation = "kaaaf_matiin_lampu"
+        message = '''Yoooo Dit! Best of luck with your future endeavors, and all the best di US! Been fun with you around, and thanks a lot for always being a gracious host for our shenanigans. Till we meet again!'''
+        super(Kafi, self).__init__(name, designation, message)
+
+
+class Billy(Tomodachi):
+
+    def __init__(self):
+        name = "Billy"
+        designation = ""
+        message = '''Semangat ya dit menempuh hidup baru di US. 😬 Just know that even though it might be tough (or might be very chill and fun), you can always find me via whatsapp to talk about anything.
+          Stay safe dit jangan terlalu barbar..
+          See you when I see you!!\
+            '''
+        super(Billy, self).__init__(name, designation, message)
+
+
+class Indrik(Tomodachi):
+
+    def __init__(self):
+        name = "Indrik"
+        designation = "dinning_table_ic_rumah_kita"
+        message = '''all the best Dit, smoga bs enjoy master nya ga 'traumatized' kaya pas sama wang fei dulu hahaha smoga bsa enjoy the community and the environment there too, 
+          bisa ketemu temen2 baru yang asik dan baik2 walaupun gabisa ngalahin kita2 disini :) 
+          keep growing juga dan semangat LDR nya lagi, jangan lupain kita kalo uda sukses dan semoga level sudoku lu bisa ngalahin acang disana'''
+        super(Indrik, self).__init__(name, designation, message)
+
+
+class Toto(Tomodachi):
+
+    def __init__(self):
+        name = "Toto"
+        designation = "aircon_cooking_ic_rumah_kita"
+        message = '''Hi Dit, thanks for being a really good friend, for being a good listener, for the many discussions that we had,
+          and for the life and career advice yang I will keep in mind as I move on with life. Hopefully you'll
+          find what you're looking for in the states, semoga you'll be blessed with more friends that will stick with you through the toughest time over there. 
+          All the best for your study, for your life, future career, and for maintaining your relationship with others, but especially with your loved ones.
+          If life is just like a shinkansen trip to death like what you said, at least I'm thankful that I have you as a fellow passenger.'''
+        super(Toto, self).__init__(name, designation, message)
+
+
+class Acang(Tomodachi):
+
+    def __init__(self):
+        name = "Acang"
+        designation = "ic_segala_hal_yang_perlu_bayar_except_internet_rumah_kita"
+        message = '''Hi dit, goodluck yow masternya di US, smoga lu have fun disana, sukses, met a lot of friends dan terutama makan yang banyak biar makin subur :D 
+          Its been a fun few years thanks for being an awesome housemate, ntar klo uda lulus pas balik let us know biar kita bs catch up
+          Smoga LDR lu sama Tania juga going strong walau timezonenya lbh jauh, cepetan nikah dit hehe
+          All the best and God bless :)'''
+        super(Acang, self).__init__(name, designation, message)
+
+
+class Bena(Tomodachi):
+
+    def __init__(self):
+        name = "Bena"
+        designation = "satpam_rumah_kita"
+        message = '''Hey dit! ga kerasa kita udah had our last ever monthly splurge. safe travels, take care and have fun in the states! who knows maybe next splurge boogaloo kita bakal some deep dish/philly cheese steak'''
+        super(Bena, self).__init__(name, designation, message)
