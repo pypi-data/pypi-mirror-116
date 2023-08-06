eaec113bba385f14555fb8cac550a74397cc6f50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:19 +0000

    Add package ormx/0.1.4.7/ormx-0.1.4.7.tar.gz

---
+++ packages/ormx/ormx-0.1.4.7.tar.gz/ormx-0.1.4.7/ormx/types.py
@@ -0,0 +1,20 @@
+DESC = 'DESC'
+ASC = 'ASC'
+
+ORDER_BY_PARAMS = [
+    ASC,
+    DESC
+]
+
+WHERE_OPTS = [
+    "<", "<<", "<=",
+    ">=", ">>", ">",
+    "=", "==", "!=", "<>"
+]
+
+__all__ = [
+    'DESC',
+    'ASC',
+    'ORDER_BY_PARAMS',
+    'WHERE_OPTS'
+]
