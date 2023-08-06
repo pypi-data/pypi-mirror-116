837d5e211871ca583ae57573f1f4bf1c6c261d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:47 +0000

    Add package livemark/0.37.0/livemark-0.37.0-py2.py3-none-any.whl

---
+++ packages/livemark/livemark-0.37.0-py2.py3-none-any.whl/livemark/plugins/preset/plugin.py
@@ -0,0 +1,40 @@
+from ...plugin import Plugin
+from ...exception import LivemarkException
+
+
+# NOTE:
+# We need to add more presets and improve existent ones
+# For example, we can make "compact" preset more minimalistic
+class PresetPlugin(Plugin):
+    profile = {
+        "type": "object",
+        "requried": ["name"],
+        "properties": {
+            "name": {"type": "string"},
+        },
+    }
+
+    def process_config(self, config):
+        preset = self.config.setdefault("name", self.config.get("self", "standard"))
+
+        # Update document
+        if preset == "standard":
+            config["brand"]["self"] = True
+            config["toc"]["self"] = True
+            config["search"]["self"] = True
+            config["stats"]["self"] = True
+            config["signs"]["self"] = True
+            config["rating"]["self"] = True
+            config["about"]["self"] = True
+            config["links"]["self"] = True
+            config["display"]["self"] = True
+        elif preset == "compact":
+            config["toc"]["self"] = True
+            config["search"]["self"] = True
+            config["stats"]["self"] = True
+            config["signs"]["self"] = True
+            config["rating"]["self"] = True
+            config["links"]["self"] = True
+            config["display"]["self"] = True
+        else:
+            raise LivemarkException(f"Not supported preset: {preset}")
