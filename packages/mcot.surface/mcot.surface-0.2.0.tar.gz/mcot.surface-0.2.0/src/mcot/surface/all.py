bd1afe6be83fcac48e9ceb5b510012d6224eeac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:06 +0000

    Add package mcot.surface/0.2.0/mcot.surface-0.2.0.tar.gz

---
+++ packages/mcot.surface/mcot.surface-0.2.0.tar.gz/mcot.surface-0.2.0/src/mcot/surface/all.py
@@ -0,0 +1,5 @@
+from . import grid
+from ._write_gifti import write_gifti
+from .cortex import Cortex, CorticalLayer, read_HCP
+from .cortical_mesh import BrainStructure, CorticalMesh, get_brain_structure
+from .mesh import Mesh2D
