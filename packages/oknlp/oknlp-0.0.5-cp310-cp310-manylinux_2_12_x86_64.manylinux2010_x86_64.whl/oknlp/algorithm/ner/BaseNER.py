d71d29e5311e0a3121bb26a597e38fb176f7968d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:53 +0000

    Add package oknlp/0.0.5/oknlp-0.0.5-cp310-cp310-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/oknlp/oknlp-0.0.5-cp310-cp310-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/oknlp/algorithm/ner/BaseNER.py
@@ -0,0 +1,28 @@
+from typing import List, Dict, Union
+from ..abc import Algorithm
+
+
+class BaseNER(Algorithm):
+    def __call__(self, sents: List[str]) -> List[List[Dict[str, Union[str, int]]]]:
+        """
+        Args:
+            sents: 输入的句子列表。
+        Returns:
+            返回一个和输入长度相同的列表，其中每一项对应其命名实体结果。
+
+            每个命名实体识别结果会包含多个实体，每个实体由一个包含三个字段的字典表示：
+
+            * type
+            * begin
+            * end
+
+        更多信息请参考 :doc:`命名实体识别 - 示例</examples/ner>`
+    
+        Examples:
+            >>> import oknlp
+            >>> ner = oknlp.ner.get_by_name()
+            >>> ner(['我爱北京天安门'])
+            [[{'type': 'LOC', 'begin': 2, 'end': 4}, {'type': 'LOC', 'begin': 4, 'end': 7}]]
+
+        """
+        return super().__call__(sents)
