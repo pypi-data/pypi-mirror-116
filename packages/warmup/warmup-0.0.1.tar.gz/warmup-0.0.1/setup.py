32c164617f9c98c96338c13dae23554c6c496d7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:12 +0000

    Add package warmup/0.0.1/warmup-0.0.1.tar.gz

---
+++ packages/warmup/warmup-0.0.1.tar.gz/warmup-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(name='warmup',
+    version='0.0.1',
+    license='MIT License',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='',
+    keywords='',
+    description=u'',)
