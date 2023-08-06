505fb5009cb88378205e33643b6c807f41d40e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:59 +0000

    Add package bwsample/0.6.8/bwsample-0.6.8.tar.gz

---
+++ packages/bwsample/bwsample-0.6.8.tar.gz/bwsample-0.6.8/bwsample/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.6.8'
+
+from .sampling import sample
+from .counting import count
+from .ranking import rank
+from .utils import (to_scipy, add_dok, adjustscore)
+from .maxdiff import scoring_orme
