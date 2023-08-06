c51f211dfedfdf4e49716562247a6de5ddaadd8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:02 +0000

    Add package preserves/0.7.0rc1/preserves-0.7.0rc1-py3-none-any.whl

---
+++ packages/preserves/preserves-0.7.0rc1-py3-none-any.whl/preserves/__init__.py
@@ -0,0 +1,10 @@
+from .values import Float, Symbol, Record, ImmutableDict, Embedded, preserve
+from .values import Annotated, is_annotated, strip_annotations, annotate
+
+from .error import DecodeError, EncodeError, ShortPacket
+
+from .binary import Decoder, Encoder, decode, decode_with_annotations, encode
+from .text import Parser, Formatter, parse, parse_with_annotations, stringify
+
+loads = parse
+dumps = stringify
