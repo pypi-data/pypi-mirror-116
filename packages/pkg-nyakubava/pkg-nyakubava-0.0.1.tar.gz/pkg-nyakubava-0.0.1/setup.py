6bd52ff02bb8df202e11c5eadea5773f8f77ca72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:56 +0000

    Add package pkg-nyakubava/0.0.1/pkg-nyakubava-0.0.1.tar.gz

---
+++ packages/pkg-nyakubava/pkg-nyakubava-0.0.1.tar.gz/pkg-nyakubava-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pkg-nyakubava',
+      version='0.0.1',
+      description='Gaussian distributions',
+      packages=['pkg-nyakubava'],
+      author = 'Natallia Yakubava',
+      auyhor_email = 'nyakubava@gmail.com',
+      zip_safe=False)
