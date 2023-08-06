5211213dae07992be56bc54f495f22f24f0197ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:27 +0000

    Add package pylode/2.12.0/pyLODE-2.12.0.tar.gz

---
+++ packages/pylode/pyLODE-2.12.0.tar.gz/pyLODE-2.12.0/pylode/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "2.12.0"
+
+from .common import *
+from .profiles import OntDoc, Prof, VocPub, NMPF, PROFILES, RDF_MEDIA_TYPES
+
+__all__ = [
+    "__version__",
+    "OntDoc",
+    "Prof",
+    "VocPub",
+    "NMPF",
+    "PROFILES",
+    "RDF_MEDIA_TYPES",
+    "MakeDocco"
+]
