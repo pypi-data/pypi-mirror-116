359f4927d1a0d9b5ee33dd18555a33390c4fe63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:50 +0000

    Add package labml-app/0.0.4/labml_app-0.0.4.tar.gz

---
+++ packages/labml-app/labml_app-0.0.4.tar.gz/labml_app-0.0.4/labml_app/settings.py
@@ -0,0 +1,15 @@
+from labml.internal.computer.configs import computer_singleton
+
+SERVER_URL = 'http://localhost:5000'
+WEB_URL = 'http://localhost:5000'
+AUTH0_DOMAIN = ''
+SLACK_BOT_TOKEN = ''
+SLACK_CHANNEL = ''
+SENTRY_DSN = ''
+FLOAT_PROJECT_TOKEN = 'floating_project'
+SAMPLES_PROJECT_TOKEN = 'samples'
+LABML_VERSION = '0.4.112'
+INDICATOR_LIMIT = 100
+IS_MIX_PANEL = False
+IS_LOCAL_SETUP = True
+IS_LOGIN_REQUIRED = False
