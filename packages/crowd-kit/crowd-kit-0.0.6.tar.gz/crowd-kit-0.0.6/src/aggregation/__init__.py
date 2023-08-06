341e4cf9798b838b0f19f19e6e21310462d6526c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:49 +0000

    Add package crowd-kit/0.0.6/crowd-kit-0.0.6.tar.gz

---
+++ packages/crowd-kit/crowd-kit-0.0.6.tar.gz/crowd-kit-0.0.6/src/aggregation/__init__.py
@@ -0,0 +1,15 @@
+from .bradley_terry import BradleyTerry
+from .dawid_skene import DawidSkene
+from .gold_majority_vote import GoldMajorityVote
+from .hrrasa import HRRASA, TextHRRASA
+from .m_msr import MMSR
+from .majority_vote import MajorityVote
+from .noisy_bt import NoisyBradleyTerry
+from .rasa import RASA, TextRASA
+from .segmentation_majority_vote import SegmentationMajorityVote
+from .segmentation_rasa import SegmentationRASA
+from .wawa import Wawa
+from .zero_based_skill import ZeroBasedSkill
+
+__all__ = ['DawidSkene', 'MajorityVote', 'MMSR', 'Wawa', 'GoldMajorityVote', 'ZeroBasedSkill', 'HRRASA', 'RASA',
+           'BradleyTerry', 'NoisyBradleyTerry', 'TextRASA', 'TextHRRASA', 'SegmentationMajorityVote', 'SegmentationRASA']
