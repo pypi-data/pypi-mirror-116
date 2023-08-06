544cb8f8804431c62b45569378a4c0f0ee187b42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:14 +0000

    Add package podcast-scraper/0.0.2/podcast-scraper-0.0.2.tar.gz

---
+++ packages/podcast-scraper/podcast-scraper-0.0.2.tar.gz/podcast-scraper-0.0.2/podcast_scraper/cli.py
@@ -0,0 +1,40 @@
+import click
+
+from podcast_scraper.france_culture import FranceCulture
+from podcast_scraper.monde_diplo import MondeDiplo
+
+
+@click.group()
+def click_france_culture():
+    pass
+
+
+@click_france_culture.command()
+@click.option(
+    "--url",
+    default="https://www.franceculture.fr/emissions/carbone-14-le-magazine-de-larcheologie",
+)
+def france_culture(url):
+    """Get podcast url for France Culture"""
+    FranceCulture().print_urls(url)
+
+
+@click.group()
+def click_monde_diplo():
+    pass
+
+
+@click_monde_diplo.command()
+@click.option(
+    "--url",
+    default="https://www.monde-diplomatique.fr/audio?debut_sons=0#pagination_sons",
+)
+def monde_diplo(url):
+    """Get podcast url for Monde Diplo"""
+    MondeDiplo().print_urls(url)
+
+
+cli = click.CommandCollection(sources=[click_france_culture, click_monde_diplo])
+
+if __name__ == "__main__":
+    cli()
