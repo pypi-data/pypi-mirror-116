9577c0596dbe1e69ad84e1c145e54bba13f7b3a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:59 +0000

    Add package pypixiv/0.2.3/pypixiv-0.2.3-py3-none-any.whl

---
+++ packages/pypixiv/pypixiv-0.2.3-py3-none-any.whl/pypixiv/abc.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+from typing import Any
+
+
+class BasePixiv:
+    # error: str
+    # message: str
+    # body: dict[str, Any] | list[dict[str, Any]]
+
+    def __init__(self, response: Any) -> None:
+        self.__response = response
+
+    @property
+    def error(self) -> str:
+        return self.__response["error"]
+
+    @property
+    def message(self) -> str:
+        return self.__response["message"]
+
+    @property
+    def body(self) -> Any:
+        return self.__response["body"]
+
+
+class BaseRanking:
+    def __init__(self, response: Any) -> None:
+        self.__response = response
+
+    @property
+    def contents(self) -> list[Any]:
+        return self.__response["contents"]
+
+    @property
+    def mode(self) -> str:
+        return self.__response["mode"]
+
+    @property
+    def content(self) -> str:
+        return self.__response["content"]
+
+    @property
+    def page(self) -> int:
+        return self.__response["page"]
+
+    @property
+    def prev(self) -> bool:
+        return self.__response["prev"]
+
+    @property
+    def next(self) -> int:
+        return self.__response["next"]
+
+    @property
+    def date(self) -> str:
+        return self.__response["date"]
+
+    @property
+    def prev_date(self) -> str:
+        return self.__response["prev_date"]
+
+    @property
+    def next_date(self) -> str | bool:
+        return self.__response["next_date"]
+
+    @property
+    def rank_total(self) -> int:
+        return self.__response["rank_total"]
---
+++ packages/pypixiv/pypixiv-0.2.3-py3-none-any.whl/pypixiv/client.py
@@ -0,0 +1,35 @@
+from typing import Literal, Optional
+
+from aiohttp.client import ClientSession
+
+from pypixiv.http import PixivHttpClient
+from pypixiv.illust.model import IllustInfo
+from pypixiv.user.model import UserFullInfo, UserInfo
+from pypixiv.ranking.model import RankingInfo
+
+
+class PixivClient(PixivHttpClient):
+    """
+    Pixiv API client class.
+    """
+
+    def __init__(
+        self, session: Optional[ClientSession] = None, lang: str = "ko"
+    ) -> None:
+        super().__init__(session=session, lang=lang)
+
+    async def userinfo(self, user_id: int):
+        return UserInfo(await self.get_userinfo(user_id))
+
+    async def full_userinfo(self, user_id: int):
+        return UserFullInfo(await self.get_full_userinfo(user_id))
+
+    async def illustinfo(self, illust_id: int):
+        return IllustInfo(await self.get_illust(illust_id))
+
+    async def ranking(
+        self,
+        mode: Literal["daily", "weekly", "monthly"] = "daily",
+        date: Optional[str] = None,
+    ):
+        return RankingInfo(await self.get_ranking(mode, date))
