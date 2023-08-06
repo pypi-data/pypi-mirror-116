84e88068a8ee0f1f05f2c21dc95de92f55c11b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:21 +0000

    Add package common-check-widget.backend-lib/1.0.1/common_check_widget.backend_lib-1.0.1-py3-none-any.whl

---
+++ packages/common-check-widget.backend-lib/common_check_widget.backend_lib-1.0.1-py3-none-any.whl/common_check_widget/backend_lib/__init__.py
@@ -0,0 +1,63 @@
+'''
+# common-check-widget-backend-lib [![NPM](https://img.shields.io/npm/v/jsii-code-samples)](https://www.npmjs.com/package/common-check-widget-backend-lib) [![PyPI](https://img.shields.io/pypi/v/aws-jsiisamples.jsii-code-samples)](https://pypi.org/project/common-check-widget-backend-lib) [![Maven](https://img.shields.io/maven-central/v/software.aws.jsiisamples.jsii/jsii-code-samples)](https://search.maven.org/artifact/common.check.widget.backend.lib/backend-ib) [![NuGet](https://img.shields.io/nuget/v/AWSSamples.Jsii)](https://www.nuget.org/packages/CCW.Jsii%22)
+
+> An common-check-widget-backend-lib package authored in TypeScript that gets published as GitHub packages for Node.js, Python etc
+
+## License
+
+This library is licensed under the MIT-0 License. See the LICENSE file.
+
+## Coming soon
+'''
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from ._jsii import *
+
+
+class CommonCheckWidget(
+    metaclass=jsii.JSIIMeta,
+    jsii_type="common-check-widget-backend-lib.CommonCheckWidget",
+):
+    def __init__(self) -> None:
+        jsii.create(CommonCheckWidget, self, [])
+
+    @jsii.member(jsii_name="ccw")
+    def ccw(self, encryption_file: typing.Any, callback: typing.Any) -> None:
+        '''
+        :param encryption_file: -
+        :param callback: -
+        '''
+        return typing.cast(None, jsii.invoke(self, "ccw", [encryption_file, callback]))
+
+    @jsii.member(jsii_name="ccwasync")
+    def ccwasync(self, encryption_file: typing.Any) -> typing.Any:
+        '''
+        :param encryption_file: -
+        '''
+        return typing.cast(typing.Any, jsii.ainvoke(self, "ccwasync", [encryption_file]))
+
+    @jsii.member(jsii_name="cwsignin")
+    def cwsignin(self, callback: typing.Any) -> None:
+        '''
+        :param callback: -
+        '''
+        return typing.cast(None, jsii.invoke(self, "cwsignin", [callback]))
+
+    @jsii.member(jsii_name="cwsigninasync")
+    def cwsigninasync(self) -> builtins.bool:
+        return typing.cast(builtins.bool, jsii.ainvoke(self, "cwsigninasync", []))
+
+
+__all__ = [
+    "CommonCheckWidget",
+]
+
+publication.publish()
