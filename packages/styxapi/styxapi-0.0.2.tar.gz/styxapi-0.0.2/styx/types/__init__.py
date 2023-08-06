60957f2bb634b70f8b2db2e2fe22a56a26850a1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:38 +0000

    Add package styxapi/0.0.2/styxapi-0.0.2.tar.gz

---
+++ packages/styxapi/styxapi-0.0.2.tar.gz/styxapi-0.0.2/styx/types/__init__.py
@@ -0,0 +1,16 @@
+from .base import (
+    Any,
+    Boolean,
+    Const,
+    Integer,
+    List,
+    Object,
+    OneOf,
+    Reference,
+    String,
+    Type,
+    Unset)
+
+from .validation import validate
+from .json_schema import json_schema
+from .typescript import to_ts
---
+++ packages/styxapi/styxapi-0.0.2.tar.gz/styxapi-0.0.2/styx/types/base.py
@@ -0,0 +1,54 @@
+
+class Type:
+    def visit(self, visitor, *args):
+        func = getattr(visitor, f'visit_{ type(self).__name__.lower() }')
+        return func(self, *args)
+
+    def __str__(self):
+        return type(self).__name__
+
+
+class String(Type):
+    pass
+
+
+class Integer(Type):
+    pass
+
+
+class Boolean(Type):
+    pass
+
+
+class Object(Type):
+    def __init__(self, fields):
+        self.fields = fields
+
+
+class List(Type):
+    def __init__(self, type_):
+        self.type = type_
+
+
+class Any(Type):
+    pass
+
+
+class Const(Type):
+    def __init__(self, value):
+        self.value = value
+
+
+class Reference(Type):
+    def __init__(self, name):
+        self.name = name
+
+
+class OneOf(Type):
+    def __init__(self, *types):
+        self.types = types
+
+
+class Unset(Type):
+    class UNSET:
+        pass
