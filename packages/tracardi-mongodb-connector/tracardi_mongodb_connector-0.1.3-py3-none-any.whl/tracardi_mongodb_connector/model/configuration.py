aa0ce23c74695c8d3ae5646d71c36acb8f485316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:37 +0000

    Add package tracardi-mongodb-connector/0.1.3/tracardi_mongodb_connector-0.1.3-py3-none-any.whl

---
+++ packages/tracardi-mongodb-connector/tracardi_mongodb_connector-0.1.3-py3-none-any.whl/tracardi_mongodb_connector/model/configuration.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from tracardi.domain.entity import Entity
+
+
+class MongoConfiguration(BaseModel):
+    uri: str
+    timeout: int = 5000
+
+
+class MongoResource(BaseModel):
+    database: str = None
+    collection: str = None
+
+
+class PluginConfiguration(BaseModel):
+    source: Entity
+    mongo: MongoResource
