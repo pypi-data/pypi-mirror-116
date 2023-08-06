7ac6f40a0a9179fb04da9375ebf2141b69a30787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:21 +0000

    Add package aiff-kit/0.0.1/aiff_kit-0.0.1.tar.gz

---
+++ packages/aiff-kit/aiff_kit-0.0.1.tar.gz/aiff_kit-0.0.1/aiff_kit/__init__.py
@@ -0,0 +1,1 @@
+name = "aiff_kit"
---
+++ packages/aiff-kit/aiff_kit-0.0.1.tar.gz/aiff_kit-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'aiff_kit',
+    version = '0.0.1',
+    keywords='aiff',
+    description = 'a library for extract the information',
+    license = 'MIT License',
+    #url = 'https://github.com/Gutier14/CAAFinder',
+    author = 'zdcao',
+    author_email = 'zdcao121@qq.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)
