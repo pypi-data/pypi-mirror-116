dd7ae539acc3013619756c3b88eecd5d0fa7b238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:00 +0000

    Add package StoaCalculator/0.0.7/StoaCalculator-0.0.7.tar.gz

---
+++ packages/StoaCalculator/StoaCalculator-0.0.7.tar.gz/StoaCalculator-0.0.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='StoaCalculator',
+    version='0.0.7',
+    description='A powerful stoa calculator',
+    long_description_content_type='text/markdown',
+    url='',
+    author='Jorge Eldis Gonzalez',
+    author_email='jorgeeldisg30@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='calculator',
+    packages=find_packages(),
+    install_requires=['']
+)
---
+++ packages/StoaCalculator/StoaCalculator-0.0.7.tar.gz/StoaCalculator-0.0.7/test.py
@@ -0,0 +1,2 @@
+import stoacalculator
+stoacalculator.mean(25)
