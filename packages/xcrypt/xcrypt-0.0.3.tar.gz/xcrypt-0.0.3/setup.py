f52d365ce00674a2d51e1119f8bb9d37224514dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:51 +0000

    Add package xcrypt/0.0.3/xcrypt-0.0.3.tar.gz

---
+++ packages/xcrypt/xcrypt-0.0.3.tar.gz/xcrypt-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="xcrypt",
+    version="0.0.3",
+    author="kgsensei",
+    author_email="ceojeremy@rainydais.com",
+    description="A Python encryption library.",
+    long_description="XCrypt is a lightweight Python library for encrypting data.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/kgsensei/XCrypt",
+    project_urls={
+        "Bug Tracker": "https://github.com/kgsensei/XCrypt/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+	py_modules=["xcrypt"],
+    python_requires=">=3.6",
+)
