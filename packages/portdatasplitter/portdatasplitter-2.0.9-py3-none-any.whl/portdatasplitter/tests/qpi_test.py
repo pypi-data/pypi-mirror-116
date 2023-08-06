8b06ba88829ff1f81a28d7fe3de45b81c7ead6c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:55 +0000

    Add package portdatasplitter/2.0.9/portdatasplitter-2.0.9-py3-none-any.whl

---
+++ packages/portdatasplitter/portdatasplitter-2.0.9-py3-none-any.whl/portdatasplitter/tests/qpi_test.py
@@ -0,0 +1,32 @@
+import unittest
+from qdk.main import QDK
+from time import sleep
+
+
+class QdkTest(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(QdkTest, self).__init__(*args, **kwargs)
+        self.qdk = QDK('192.168.100.118', 2292)
+        self.qdk.make_connection()
+
+    def test_set_null(self):
+        res = self.qdk.execute_method('set_manual_value', manual_value=1000,
+                                get_response=True)
+        print('res', res)
+        weight = self.qdk.execute_method('get_value', get_response=True)
+        print('weight', weight)
+        sleep(2)
+        weight = self.qdk.execute_method('get_value', get_response=True)
+        print('weight', weight)
+        res = self.qdk.execute_method('set_manual_value', manual_value=0,
+                                get_response=True)
+        print('res', res)
+        weight = self.qdk.execute_method('get_value', get_response=True)
+        print('weight', weight)
+        sleep(2)
+        weight = self.qdk.execute_method('get_value', get_response=True)
+        print('weight', weight)
+
+
+
+
