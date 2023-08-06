748e5924f77f989bd7b3b99b7b028289a421a6fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:59 +0000

    Add package botlib/127/botlib-127-py3-none-any.whl

---
+++ packages/botlib/botlib-127-py3-none-any.whl/bot/bus.py
@@ -0,0 +1,61 @@
+# This file is placed in the Public Domain.
+
+"list of bots"
+
+from .obj import Object
+
+class Bus(Object):
+
+    objs = []
+
+    def __iter__(self):
+        return iter(Bus.objs)
+
+    @staticmethod
+    def add(obj):
+        if obj not in Bus.objs:
+            Bus.objs.append(obj)
+
+    @staticmethod
+    def announce(txt):
+        for h in Bus.objs:
+            if "announce" in dir(h):
+                h.announce(txt)
+
+    @staticmethod
+    def byorig(orig):
+        for o in Bus.objs:
+            if Object.__dorepr__(o) == orig:
+                return o
+
+    @staticmethod
+    def byfd(fd):
+        for o in Bus.objs:
+            if o.fd and o.fd == fd:
+                return o
+
+    @staticmethod
+    def bytype(typ):
+        for o in Bus.objs:
+            if isinstance(o, type):
+                return o
+
+    def first(otype=None):
+        if Bus.objs:
+            if not otype:
+                return Bus.objs[0]
+            for o in Bus.objs:
+                if otype in str(type(o)):
+                    return o
+
+    @staticmethod
+    def resume():
+        for o in Bus.objs:
+            o.resume()
+
+    @staticmethod
+    def say(orig, channel, txt):
+        for o in Bus.objs:
+            if Object.__dorepr__(o) == orig:
+                o.say(channel, txt)
+
