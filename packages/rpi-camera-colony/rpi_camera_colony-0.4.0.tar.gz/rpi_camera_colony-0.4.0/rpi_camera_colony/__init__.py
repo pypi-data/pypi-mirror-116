0d5401a6eb101b49a1197dc1dc8ba58a9e16b4f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:44 +0000

    Add package rpi-camera-colony/0.4.0/rpi_camera_colony-0.4.0.tar.gz

---
+++ packages/rpi-camera-colony/rpi_camera_colony-0.4.0.tar.gz/rpi_camera_colony-0.4.0/rpi_camera_colony/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# Author: Lars B. Rollik <L.B.Rollik@protonmail.com>
+# License: BSD 3-Clause
+import logging
+
+__version__ = "0.4.0"
+__author__ = "Lars B. Rollik"
+
+level = "DEBUG"
+logger = logging.getLogger()
+logger.setLevel(getattr(logging, level))
+
+formatter = logging.Formatter(
+    "%(asctime)s - %(levelname)s"
+    " - %(processName)s %(filename)s:%(lineno)s"
+    " - %(message)s"
+)
+formatter.datefmt = "%Y-%m-%d %H:%M:%S %p"
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(getattr(logging, level))
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
