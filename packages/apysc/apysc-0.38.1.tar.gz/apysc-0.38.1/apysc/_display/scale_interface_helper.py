757bf9dbac98677d6f3f1bdf8e8719d723ae6218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:50 +0000

    Add package apysc/0.38.1/apysc-0.38.1.tar.gz

---
+++ packages/apysc/apysc-0.38.1.tar.gz/apysc-0.38.1/apysc/_display/scale_interface_helper.py
@@ -0,0 +1,32 @@
+"""Helper interfaces for the scale interfaces.
+"""
+
+from typing import Union
+
+import apysc as ap
+from apysc._type.expression_string import ExpressionString
+
+
+def get_point_key_for_expression(
+        coordinate: Union[int, ap.Int]) -> ExpressionString:
+    """
+    Get a key string for the expression from the x and y coordinates.
+
+    Parameters
+    ----------
+    coordinate : int or Int
+        X or y coordinate.
+
+    Returns
+    -------
+    key_exp_str : ExpressionString
+        Key expression string.
+    """
+    from apysc._type.variable_name_interface import VariableNameInterface
+    if isinstance(coordinate, VariableNameInterface):
+        coordinate_str: str = coordinate.variable_name
+    else:
+        coordinate_str = str(coordinate)
+    key_exp_str: ExpressionString = ExpressionString(
+        value=f'String({coordinate_str})')
+    return key_exp_str
