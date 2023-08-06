a00d4db05bf9b8762c1f9570b353ec47a68250d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:17 +0000

    Add package flake8-too-many/0.1.0/flake8_too_many-0.1.0-py3-none-any.whl

---
+++ packages/flake8-too-many/flake8_too_many-0.1.0-py3-none-any.whl/flake8_too_many/options.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Options:
+    """List of configurable options of the `flake8-too-many` plugin."""
+
+    max_function_arguments: int
+    max_function_return_stmts: int
+    max_function_return_values: int
+    max_unpacking_targets: int
