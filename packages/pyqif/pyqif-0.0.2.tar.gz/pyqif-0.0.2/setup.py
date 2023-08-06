bbdfb339d7d0e28b8ffe354f00e80605d3edb223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:09 +0000

    Add package pyqif/0.0.2/pyqif-0.0.2.tar.gz

---
+++ packages/pyqif/pyqif-0.0.2.tar.gz/pyqif-0.0.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyqif",
+    version="0.0.2",
+    author="Lucas Starling",
+    author_email="lucastarling1@gmail.com",
+    description="A tool for calculating the hyper distribution from pushing a prior into a channel. Quantitative Information Flow.",
+    long_description=long_description,
+    url="https://github.com/LucasStarlingdePaulaSalles/hyper-distributor",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    py_modules=["pyqif"], 
+    requires=['numpy'],
+    install_requires=['numpy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'pyqif=pyqif:main',
+        ],
+    },
+)
