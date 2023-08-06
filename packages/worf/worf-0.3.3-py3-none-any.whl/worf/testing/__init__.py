74ef7b5c1448355a52cecf337f986b4e0739c0d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:44 +0000

    Add package worf/0.3.3/worf-0.3.3-py3-none-any.whl

---
+++ packages/worf/worf-0.3.3-py3-none-any.whl/worf/testing/__init__.py
@@ -0,0 +1,6 @@
+from worf.testing.legacy import (  # noqa: F401
+    assertion_factory,
+    bundle_factory,
+    verify_model_interface,
+)
+from worf.testing.serializers import verify_serializer_interface  # noqa: F401
