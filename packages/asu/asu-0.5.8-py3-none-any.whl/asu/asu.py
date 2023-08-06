2199126c4c19cf2da13383d7cc1c80d3b475f8d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:33 +0000

    Add package asu/0.5.8/asu-0.5.8-py3-none-any.whl

---
+++ packages/asu/asu-0.5.8-py3-none-any.whl/asu/asu.py
@@ -0,0 +1,72 @@
+import json
+from os import getenv
+from pathlib import Path
+
+from flask import Flask, redirect, send_from_directory
+from redis import Redis
+
+
+def create_app(test_config: dict = None) -> Flask:
+    """Create the main Flask application
+
+    Args:
+        test_config (dict): A dictionry containing a configuration during tests
+
+    Returns:
+        Flask: The application
+    """
+
+    redis_host = getenv("REDIS_HOST", "localhost")
+    redis_port = getenv("REDIS_PORT", 6379)
+
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        REDIS_CONN=Redis(host=redis_host, port=redis_port),
+        TESTING=False,
+        DEBUG=False,
+        UPSTREAM_URL="https://downloads.cdn.openwrt.org",
+        BRANCHES={},
+    )
+
+    if not test_config:
+        for config_file in [
+            Path.cwd() / "config.py",
+            "/etc/asu/config.py",
+        ]:
+            if Path(config_file).exists():
+                print(f"Loading {config_file}")
+                app.config.from_pyfile(config_file)
+                break
+    else:
+        app.config.from_mapping(test_config)
+
+    for option, value in app.config.items():
+        if option.endswith("_PATH") and isinstance(value, (Path, str)):
+            app.config[option] = Path(value)
+            app.config[option].mkdir(parents=True, exist_ok=True)
+
+    (Path().cwd()).mkdir(exist_ok=True, parents=True)
+
+    @app.route("/json/")
+    @app.route("/json/<path:path>")
+    def json_path(path="index.html"):
+        return send_from_directory(app.config["JSON_PATH"], path)
+
+    @app.route("/store/")
+    @app.route("/store/<path:path>")
+    def store_path(path="index.html"):
+        return send_from_directory(app.config["STORE_PATH"], path)
+
+    from . import janitor
+
+    app.register_blueprint(janitor.bp)
+
+    from . import api
+
+    app.register_blueprint(api.bp)
+
+    (app.config["JSON_PATH"] / "branches.json").write_text(
+        json.dumps(app.config["BRANCHES"])
+    )
+
+    return app
