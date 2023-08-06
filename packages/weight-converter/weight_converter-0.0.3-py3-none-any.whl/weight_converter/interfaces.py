1dc2740f0033c82939670c7b12820a98afb56cad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:35 +0000

    Add package weight-converter/0.0.3/weight_converter-0.0.3-py3-none-any.whl

---
+++ packages/weight-converter/weight_converter-0.0.3-py3-none-any.whl/weight_converter/interfaces.py
@@ -0,0 +1,56 @@
+class IUnit:
+
+    def to_metric_tonnes(self) -> float:
+        """
+        Converts value to metric tonnes
+        :return: The converted float value to metric tonnes
+        """
+        pass
+
+    def to_kilograms(self) -> float:
+        """
+        Converts value to kilograms
+        :return: The converted float value to kilograms
+        """
+        pass
+
+    def to_grams(self) -> float:
+        """
+        Converts value to grams
+        :return: The converted float value to grams
+        """
+        pass
+
+    def to_milligrams(self) -> float:
+        """
+        Converts value to milligrams
+        :return: The converted float value to milligrams
+        """
+        pass
+
+    def to_micrograms(self) -> float:
+        """
+        Converts value to micrograms
+        :return: The converted float value to micrograms
+        """
+        pass
+
+    def to_imperial_tons(self) -> float:
+        """ Converts value to imperial or UK tons """
+        pass
+
+    def to_us_tons(self) -> float:
+        """ Converts value to to US tons """
+        pass
+
+    def to_stones(self) -> float:
+        """ Converts value to stones """
+        pass
+
+    def to_pounds(self) -> float:
+        """ Converts value to pounds """
+        pass
+
+    def to_ounces(self) -> float:
+        """ Converts value to ounces """
+        pass
