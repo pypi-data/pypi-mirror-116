a9fb94db9fc1f7f35b878ba6b87d6da6665d182a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:34 +0000

    Add package soda-sql-core/2.1.0b14/soda_sql_core-2.1.0b14-py3-none-any.whl

---
+++ packages/soda-sql-core/soda_sql_core-2.1.0b14-py3-none-any.whl/sodasql/scan/scan_error.py
@@ -0,0 +1,55 @@
+from dataclasses import dataclass
+
+from sodasql.exceptions.exceptions import SodaSqlError
+from sodasql.scan.test import Test
+
+
+@dataclass
+class ScanError:
+    message: str
+    exception: Exception = None
+
+    def __str__(self) -> str:
+        return f'[{self.get_type()}] {self.get_message()}'
+
+    def to_json(self) -> dict:
+        json = {
+            'type': self.get_type(),
+            'message': self.get_message()
+        }
+        if self.exception is not None:
+            json['exception'] = str(self.exception)
+
+            if hasattr(self.exception, "error_code"):
+                json['errorCode'] = self.exception.error_code
+        return json
+
+    def get_type(self) -> str:
+        return 'error'
+
+    def get_message(self) -> str:
+        return self.message
+
+
+@dataclass
+class TestExecutionScanError(ScanError):
+    test: Test = None
+
+    def get_type(self) -> str:
+        return 'test_execution_error'
+
+
+@dataclass
+class SodaCloudScanError(ScanError):
+    def get_type(self) -> str:
+        return 'soda_cloud_error'
+
+@dataclass
+class WarehouseAuthenticationScanError(ScanError):
+    def get_type(self) -> str:
+        return 'warehouse_authentication_error'
+
+@dataclass
+class WarehouseConnectionScanError(ScanError):
+    def get_type(self) -> str:
+        return 'warehouse_connection_error'
