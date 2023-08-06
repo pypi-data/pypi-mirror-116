8703d4d8ec64c0327b95244373475a150bc0c5bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:39 +0000

    Add package ossfs/2021.8.0/ossfs-2021.8.0.tar.gz

---
+++ packages/ossfs/ossfs-2021.8.0.tar.gz/ossfs-2021.8.0/setup.py
@@ -0,0 +1,32 @@
+"""
+Modules Distributions.
+"""
+
+from setuptools import find_packages, setup
+
+with open("test_requirements.txt") as f_r:
+    tests_requirements = [line.strip() for line in f_r.readlines()]
+
+setup(
+    name="ossfs",
+    version="2021.8.0",
+    description="fsspec filesystem for OSS",
+    author="Yanxiang Gao",
+    author_email="gao@iterive.ai",
+    download_url="https://github.com/iterative/ossfs",
+    license="Apache-2.0 License",
+    install_requires=["fsspec>=2021.7.0", "oss2>=2.6.1"],
+    extras_require={"tests": tests_requirements},
+    keywords="oss, fsspec",
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=find_packages(exclude=["tests"]),
+)
