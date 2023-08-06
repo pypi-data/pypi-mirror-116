b049c102dce40787935b58f1fce95a1875463075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:58 +0000

    Add package py-tools-Orinnass/1.3.0/py-tools_Orinnass-1.3.0.tar.gz

---
+++ packages/py-tools-Orinnass/py-tools_Orinnass-1.3.0.tar.gz/py-tools_Orinnass-1.3.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+from tools import __version__, __name__
+
+setup(
+    name=__name__,
+    version=__version__,
+    download_url="https://gitlab.com/Orinnass/python-module-tools",
+    packages=find_packages(include=('tools',))
+)
---
+++ packages/py-tools-Orinnass/py-tools_Orinnass-1.3.0.tar.gz/py-tools_Orinnass-1.3.0/tools/__init__.py
@@ -0,0 +1,5 @@
+from .singleton import Singleton
+from .trace_dump import start_trace_dump, create_snapshot
+
+__version__ = '1.3.0'
+__name__ = 'py-tools_Orinnass'
