fc99ce2630ba0c22785cbf0098ad602d3436a6b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:39 +0000

    Add package openmetadata-ingestion/0.2.1/openmetadata_ingestion-0.2.1-py3-none-any.whl

---
+++ packages/openmetadata-ingestion/openmetadata_ingestion-0.2.1-py3-none-any.whl/metadata/utils/helpers.py
@@ -0,0 +1,47 @@
+#  Licensed to the Apache Software Foundation (ASF) under one or more
+#  contributor license agreements. See the NOTICE file distributed with
+#  this work for additional information regarding copyright ownership.
+#  The ASF licenses this file to You under the Apache License, Version 2.0
+#  (the "License"); you may not use this file except in compliance with
+#  the License. You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from datetime import datetime, timedelta
+
+from metadata.generated.schema.api.services.createDatabaseService import CreateDatabaseServiceEntityRequest
+from metadata.generated.schema.entity.services.databaseService import DatabaseServiceEntity
+from metadata.ingestion.ometa.client import REST
+
+
+def get_start_and_end(duration):
+    today = datetime.utcnow()
+    start = (today + timedelta(0-duration)).replace(hour=0, minute=0, second=0, microsecond=0)
+    end = (today + timedelta(3)).replace(hour=0, minute=0, second=0, microsecond=0)
+    return start, end
+
+
+def snake_to_camel(s):
+    a = s.split('_')
+    a[0] = a[0].capitalize()
+    if len(a) > 1:
+        a[1:] = [u.title() for u in a[1:]]
+    return ''.join(a)
+
+
+def get_service_or_create(config, metadata_config) -> DatabaseServiceEntity:
+    client = REST(metadata_config)
+    service = client.get_database_service(config.service_name)
+    if service is not None:
+        return service
+    else:
+        service = {'jdbc': {'connectionUrl': config.get_connection_url(), 'driverClass': 'jdbc'},
+                   'name': config.service_name, 'description': '', 'serviceType': config.get_service_type()}
+        created_service = client.create_database_service(CreateDatabaseServiceEntityRequest(**service))
+        return created_service
