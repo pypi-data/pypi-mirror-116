434ebae283fc347d6ce6b97e7999066b0c6b4445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:42 +0000

    Add package schema-markdown/1.1.2/schema-markdown-1.1.2.tar.gz

---
+++ packages/schema-markdown/schema-markdown-1.1.2.tar.gz/schema-markdown-1.1.2/src/schema_markdown/__main__.py
@@ -0,0 +1,12 @@
+# Licensed under the MIT License
+# https://github.com/craigahobbs/schema-markdown/blob/main/LICENSE
+
+"""
+The Schema Markdown compiler and schema validation tool main entry point
+"""
+
+from .main import main
+
+
+if __name__ == '__main__': # pragma: no cover
+    main()
