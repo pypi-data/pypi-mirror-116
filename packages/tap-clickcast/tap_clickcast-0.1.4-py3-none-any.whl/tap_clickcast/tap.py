4389417cf339e471daa72c016917be839de2109f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:55 +0000

    Add package tap-clickcast/0.1.4/tap_clickcast-0.1.4-py3-none-any.whl

---
+++ packages/tap-clickcast/tap_clickcast-0.1.4-py3-none-any.whl/tap_clickcast/tap.py
@@ -0,0 +1,33 @@
+"""clickcast tap class."""
+
+from typing import List
+
+from singer_sdk import Tap, Stream
+from singer_sdk import typing as th  # JSON schema typing helpers
+
+# TODO: Import your custom stream types here:
+from tap_clickcast.streams import CampaignsStream, EmployersStream, JobsStream, JobStatsStream
+
+# TODO: Compile a list of custom stream types here
+#       OR rewrite discover_streams() below with your custom logic.
+STREAM_TYPES = [CampaignsStream, EmployersStream, JobsStream, JobStatsStream]
+
+
+class TapClickcast(Tap):
+    """clickcast tap class."""
+
+    name = "tap-clickcast"
+
+    # TODO: Update this section with the actual config values you expect:
+    config_jsonschema = th.PropertiesList(
+        th.Property("partner_token", th.StringType, required=True),
+        th.Property(
+            "api_url_base",
+            th.StringType,
+            default="https://api.clickcast.cloud/clickcast/api",
+        ),
+    ).to_dict()
+
+    def discover_streams(self) -> List[Stream]:
+        """Return a list of discovered streams."""
+        return [stream_class(tap=self) for stream_class in STREAM_TYPES]
