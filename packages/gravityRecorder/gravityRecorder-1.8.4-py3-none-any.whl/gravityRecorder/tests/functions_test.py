d8ecebc1565c36dc02d061eef249dbcbf132e6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:42 +0000

    Add package gravityRecorder/1.8.4/gravityRecorder-1.8.4-py3-none-any.whl

---
+++ packages/gravityRecorder/gravityRecorder-1.8.4-py3-none-any.whl/gravityRecorder/tests/functions_test.py
@@ -0,0 +1,27 @@
+import unittest
+from gravityRecorder import functions
+from gravityRecorder.tests import test_objects
+
+
+class FunctionsTest(unittest.TestCase):
+    def test_get_next_id(self):
+        response = functions.get_next_id(test_objects.test_sqlshell)
+        print("NEXT ID:", response)
+
+    def test_current_id(self):
+        response = functions.get_current_id(test_objects.test_sqlshell, 1274)
+        print("UNFINISHED ID:", response)
+
+    def test_get_tc_id(self):
+        response = functions.get_local_trash_cat_id(test_objects.test_sqlshell,
+                                                    1)
+        self.assertEqual(response, 1)
+
+    def test_get_tc_id_unreal_cat(self):
+        """ Несуществующая категория груза """
+        response = functions.get_local_trash_cat_id(test_objects.test_sqlshell,
+                                                    0)
+        print(response)
+        self.assertEqual(response, 1)
+if __name__ == '__main__':
+    unittest.main()
