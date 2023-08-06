e8b0fc7d260d114d100d9923c96bbf334f442a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:14 +0000

    Add package pygraphsearch/1.2.1/pygraphsearch-1.2.1.tar.gz

---
+++ packages/pygraphsearch/pygraphsearch-1.2.1.tar.gz/pygraphsearch-1.2.1/src/pygraphsearch/Node.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from typing import Generic, TYPE_CHECKING, Iterable, TypeVar
+from .Node import TNode
+from .Edge import TData
+
+if TYPE_CHECKING:
+	from .Edge import Edge
+
+
+TNode = TypeVar("TNode", bound="Node")
+
+
+class Node(Generic[TNode, TData], ABC):
+	"""An abstract base class for a node in a graph.
+
+	Args:
+		Generic (TNode): The concrete type of the nodes in the graph.
+		Generic (TData): The type of the data stored by the edges of the graph.
+	"""
+
+	@abstractmethod
+	def neighbours(self) -> Iterable[Edge[TNode, TData]]:
+		"""Get the edges to all the nodes adjacent to this one.
+
+		Returns:
+			Iterable[Edge[TNode, TData]]: The edges to all the nodes adjacent to this one.
+		"""
+		pass
