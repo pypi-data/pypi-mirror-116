8ed1cbf0e183ad8b98e0449d958cf79e37e5d458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:57 +0000

    Add package baizhan001/1.0/baizhan001-1.0.tar.gz

---
+++ packages/baizhan001/baizhan001-1.0.tar.gz/baizhan001-1.0/baizhan001/module_b1.py
@@ -0,0 +1,3 @@
+# -*- coding: UTF-8 -*-
+def fun():
+    print('有我无敌')
---
+++ packages/baizhan001/baizhan001-1.0.tar.gz/baizhan001-1.0/setup.py
@@ -0,0 +1,11 @@
+#-*- coding: UTF-8 -*-
+from  distutils.core import setup
+setup(
+    name='baizhan001',
+    version='1.0',
+    description='模块测试',
+    author='阿水',
+    author_email='as@163.com',
+    py_modules=['baizhan001.module_b1','baizhan001.module_b2']
+)
+
