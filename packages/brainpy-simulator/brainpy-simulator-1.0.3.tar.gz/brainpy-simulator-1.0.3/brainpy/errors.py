824c69d8948aaf62f376f2c395e673f7f1a7bfd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:37 +0000

    Add package brainpy-simulator/1.0.3/brainpy-simulator-1.0.3.tar.gz

---
+++ packages/brainpy-simulator/brainpy-simulator-1.0.3.tar.gz/brainpy-simulator-1.0.3/brainpy/errors.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+
+class RunningError(Exception):
+    """The error occurred in the running function."""
+    pass
+
+
+class ModelDefError(Exception):
+    """Model definition error."""
+    pass
+
+
+class ModelUseError(Exception):
+    """Model use error."""
+    pass
+
+
+class IntegratorError(Exception):
+    pass
+
+
+class DiffEqError(Exception):
+    """The differential equation definition error.
+    """
+    pass
+
+
+class CodeError(Exception):
+    """Code definition error.
+    """
+    pass
+
+
+class AnalyzerError(Exception):
+    """Differential equation analyzer error.
+    """
+
+
+class PackageMissingError(Exception):
+    """The package missing error.
+    """
+    pass
+
+
+class BackendNotInstalled(Exception):
+    def __init__(self, backend):
+        super(BackendNotInstalled, self).__init__(
+            '"{bk}" must be installed when the user wants to use {bk} backend. \n'
+            'Please install {bk} through "pip install {bk}" '
+            'or "conda install {bk}".'.format(bk=backend))
---
+++ packages/brainpy-simulator/brainpy-simulator-1.0.3.tar.gz/brainpy-simulator-1.0.3/brainpy/simulation/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from .brainobjects import *
+from .connectivity import *
+from .delays import *
+from .drivers import *
+from .every import *
+from .monitors import *
+from .utils import *
