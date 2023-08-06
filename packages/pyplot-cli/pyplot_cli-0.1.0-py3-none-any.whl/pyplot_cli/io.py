a29b1bf3b1436e9846f2d2af118f5824ada54bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:41 +0000

    Add package pyplot-cli/0.1.0/pyplot_cli-0.1.0-py3-none-any.whl

---
+++ packages/pyplot-cli/pyplot_cli-0.1.0-py3-none-any.whl/pyplot_cli/io.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+
+class DataSet:
+    _available_types = ["dat", "txt", "csv", "xls", "xlsx"]
+
+    def __init__(self, filename: str):
+        self.filename: str = filename
+        self.extension: str = filename.split(".")[-1]
+
+        if self.extension not in self._available_types:
+            raise ValueError("Invalid file extension.")
+
+        if self.extension == "xls" or self.extension == "xlsx":
+            self.dataframe = self.load_excel()
+        else:
+            self.dataframe = self.load_txt()
+
+        self.column_names = list(self.dataframe.keys())
+        self.values = self.dataframe.values.T
+
+    def load_excel(self):
+        return self.load_excel(self.filename)
+
+    def load_txt(self):
+        if self.extension == "csv":
+            sep = ","
+        elif self.extension == "dat" or self.extension == "txt":
+            sep = r"\s+"
+
+        data = pd.read_csv(self.filename, sep=sep)
+        return data
