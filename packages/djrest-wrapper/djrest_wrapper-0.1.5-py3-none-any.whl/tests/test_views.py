93132d635d8ad196b63aeb0544c54c76824e7356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:19 +0000

    Add package djrest-wrapper/0.1.5/djrest_wrapper-0.1.5-py3-none-any.whl

---
+++ packages/djrest-wrapper/djrest_wrapper-0.1.5-py3-none-any.whl/tests/test_views.py
@@ -0,0 +1,24 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from djrest_wrapper.exceptions.apis.errors import *
+from tests.models import ExampleModel
+
+
+class ExampleAPITestCase(APITestCase):
+    def setUp(self):
+        pass
+
+    def test_create_example(self):
+        url = reverse('example-list')
+        data = {
+            'text': 'some text'
+        }
+        response = self.client.post(path=url, data=data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        self.assertIsNotNone(response.json().get('examplemodel', None))
+
+        id = response.json().get('examplemodel').get('id')
+        self.assertIsNotNone(ExampleModel.objects.get(id=id))
