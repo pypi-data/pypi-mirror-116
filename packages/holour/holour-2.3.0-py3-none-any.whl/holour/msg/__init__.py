04f04a88122822e10dc76fee77bb0045e837be06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:27 +0000

    Add package holour/2.3.0/holour-2.3.0-py3-none-any.whl

---
+++ packages/holour/holour-2.3.0-py3-none-any.whl/holour/msg/__init__.py
@@ -0,0 +1,8 @@
+from holour.msg.geometry import Vector3, Pose, Poses
+from holour.msg.status import Status
+from holour.msg.force_torque import ForceTorque
+from holour.msg.script import Script, WaypointConfig
+from holour.msg.task import Task
+from holour.msg.product import Product
+from holour.msg.process import Process
+from holour.msg.assembly import AddProcess, MoveTask, CompleteTask, Destination, ConfirmNeeded, ConfirmTask
