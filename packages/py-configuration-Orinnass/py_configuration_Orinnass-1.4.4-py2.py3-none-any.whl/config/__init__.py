f6a59bd369e6e58cab41dbcb0711efd846e371f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:58 +0000

    Add package py-configuration-Orinnass/1.4.4/py_configuration_Orinnass-1.4.4-py2.py3-none-any.whl

---
+++ packages/py-configuration-Orinnass/py_configuration_Orinnass-1.4.4-py2.py3-none-any.whl/config/__init__.py
@@ -0,0 +1,9 @@
+"""
+Модуль с описанием класса Configuration
+"""
+# TODO: Написать документацию по конфигу логирования
+# from .handlers import DBHandler
+from .configuration import Configuration
+
+__version__ = '1.4.4'
+__name__ = 'py-configuration-Orinnass'
