63ec7a935cb8200db3dd3e84439ebfdbed300d00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:23 +0000

    Add package pygraphsearch/1.3.1/pygraphsearch-1.3.1.tar.gz

---
+++ packages/pygraphsearch/pygraphsearch-1.3.1.tar.gz/pygraphsearch-1.3.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="pygraphsearch",
+	version="1.3.1",
+	author="Abraham Murciano",
+	author_email="abrahammurciano@gmail.com",
+	description="A python package to search graphs.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/abrahammurciano/pygraphsearch",
+	project_urls={
+		"Bug Tracker": "https://github.com/abrahammurciano/pygraphsearch/issues",
+	},
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	package_dir={"": "src"},
+	package_data={"pygraphsearch": ["py.typed"]},
+	packages=setuptools.find_packages(where="src"),
+	python_requires=">=3.6",
+	test_suite="nose.collector",
+	tests_require=["nose"],
+)
