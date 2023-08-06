f41b71be5c46903c8dab720237c515f4fe050793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:19 +0000

    Add package besapi/1.0.1/besapi-1.0.1.tar.gz

---
+++ packages/besapi/besapi-1.0.1.tar.gz/besapi-1.0.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""
+file to configure python build and packaging for pip
+"""
+
+try:
+    from setuptools import setup
+except (ImportError, ModuleNotFoundError):
+    from distutils.core import setup
+
+setup(
+    name="besapi",
+    # version= moved to setup.cfg
+    author="Matt Hansen, James Stewart",
+    author_email="hansen.m@psu.edu, james@jgstew.com",
+    description="Library for working with the BigFix REST API",
+    license="BSD",
+    keywords="bigfix iem tem rest api",
+    url="https://github.com/CLCMacTeam/besapi",
+    long_description=(
+        "python-besapi is a Python library designed to "
+        "interact with the BES (BigFix) REST API."
+    ),
+    packages=["besapi", "bescli"],
+    package_data={"besapi": ["schemas/*.xsd"]},
+    install_requires=["requests", "lxml", "cmd2"],
+    include_package_data=True,
+    package_dir={"": "src"},
+)
