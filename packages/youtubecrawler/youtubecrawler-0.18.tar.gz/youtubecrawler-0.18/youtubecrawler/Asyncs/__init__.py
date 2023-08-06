50e4ecf7d23011c2876180be2602f4b8bf2ee5ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:08 +0000

    Add package youtubecrawler/0.18/youtubecrawler-0.18.tar.gz

---
+++ packages/youtubecrawler/youtubecrawler-0.18.tar.gz/youtubecrawler-0.18/youtubecrawler/Asyncs/__init__.py
@@ -0,0 +1,13 @@
+
+from requests_html import AsyncHTMLSession
+from bs4 import BeautifulSoup
+
+class Init:
+        async def init(self,link):
+         self.link2=link
+        
+         session = AsyncHTMLSession()
+         response = await session.get(self.link2)
+         await response.html.arender(sleep=3,timeout=30)
+         return BeautifulSoup(response.html.html, "html.parser") 
+     
