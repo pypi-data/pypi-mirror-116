64f9fb33cbf20ae8bf893d0bb7f4910d5fdf5927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:56 +0000

    Add package invenio-theme-tuw/2021.8.2/invenio_theme_tuw-2021.8.2-py2.py3-none-any.whl

---
+++ packages/invenio-theme-tuw/invenio_theme_tuw-2021.8.2-py2.py3-none-any.whl/invenio_theme_tuw/assets/semantic-ui/js/invenio_theme_tuw/mobilemenu/index.js
@@ -0,0 +1,16 @@
+// This file is part of InvenioRDM
+// Copyright (C) 2021 TU Wien.
+//
+// Invenio Theme TUW is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+import $ from "jquery";
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("nav-icon").addEventListener("click", function () {
+        $(this).toggleClass('open');
+        $("#sitewrapper").toggleClass("pushedright");
+        $("#sidebar").toggleClass("open");
+        $("header").toggleClass("pushedright");
+    })
+});
