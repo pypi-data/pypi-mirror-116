7a634033e649522e27ac209934d694b0ef465636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:55 +0000

    Add package distributions-jerome-udacity/0.1/distributions-jerome-udacity-0.1.tar.gz

---
+++ packages/distributions-jerome-udacity/distributions-jerome-udacity-0.1.tar.gz/distributions-jerome-udacity-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-jerome-udacity',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-jerome-udacity'],
+      zip_safe=False)
