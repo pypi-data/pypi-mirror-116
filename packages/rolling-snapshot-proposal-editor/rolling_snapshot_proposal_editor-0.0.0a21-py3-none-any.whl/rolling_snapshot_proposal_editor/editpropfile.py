fcf5ae0979b42ae494f694c0d8b06a024d4c719c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:37 +0000

    Add package rolling-snapshot-proposal-editor/0.0.0a21/rolling_snapshot_proposal_editor-0.0.0a21-py3-none-any.whl

---
+++ packages/rolling-snapshot-proposal-editor/rolling_snapshot_proposal_editor-0.0.0a21-py3-none-any.whl/rolling_snapshot_proposal_editor/editpropfile.py
@@ -0,0 +1,105 @@
+class EditPropFile:
+    """
+    This class handles editing prop file.
+    #####
+    Usage:
+    ???
+    #####
+    Ref:
+    ???
+    """
+    def __init__(self,propfile,verbal=True,read_propfile=True):
+        import copy
+        self.propfile = propfile
+        self.verbal = verbal
+        if read_propfile: 
+            self.prop = self._read_propfile()
+            self.newprop = copy.deepcopy(self.prop)
+            if self.verbal: print('Access by self.prop. \n')
+    def _read_propfile(self):
+        if self.verbal: print('Read propfile.\n')
+        f = open(self.propfile,'r')
+        t = f.readlines()
+        f.close()
+        return t
+    ####################
+    ####################
+    ####################
+    def profiling(self,keyword,prop,this_verbal=True):
+        ##### keyword = str e.g., 'Target_Number:'. Note that most keywords in propfile will have a comma ':' at the end.
+        ##### This function returns line index (0-indexing) and content of the line in propfile given the keyword
+        from rolling_snapshot_proposal_editor.templatehandler import TemplateHandler
+        if this_verbal: print("""Search keyword: '{0}'\n""".format(keyword))
+        output = []
+        for ii,i in enumerate(prop):
+            if keyword in i.split():
+                output.append((ii,i))
+        if this_verbal: print('Return profile ...\n')
+        return output
+    ####################
+    ####################
+    ####################
+    def add(self):
+        pass
+    ####################
+    ####################
+    ####################
+    def remove(self,section,info,this_verbal=True):
+        ##### [section,info] = ['fixed_target','target_number'], ['visit','visit_number']
+        if section=='fixed_target':
+            from rolling_snapshot_proposal_editor.remove_fixed_target import remove_fixed_target
+            prop = self.prop
+            try:
+                target_number = info['target_number']
+                do_remove_visits = info['do_remove_visits']
+                self.newprop = self.remove_fixed_target(prop,target_number,do_remove_visits,verbal=this_verbal)
+                if this_verbal: print('Access by self.newprop.\n')
+            except ValueError:
+                print("""Error: for section='fixed_target', info = {'target_number':str,'do_remove_visits':bool}\n""")  
+    ####################
+    ####################
+    ####################
+    def save(self,outpath):
+        print('Creating {0} ...\n'.format(outname))
+        f = open(outpath,'w')
+        f.writelines(self.newprop)
+        f.close()   
+####################
+####################
+####################
+    
+    
+    def find_targetname_in_visits(propfile):
+        ##### propfile = .prop file
+        ##### return a list of tuples. Each tuple = (x,y,z) where x = line index (0-indexing), y = visit number, and z = target name
+        print('Getting a list of visit numbers ...\n')
+        visit_number_list = prop_profiling(propfile,None,True,'Visit_Number:')
+        #####
+        print('Matching with target name ...\n')
+        visit_target_list = []
+        f = open(propfile,'r')
+        t0 = f.readlines()
+        f.close()
+        for i in visit_number_list:
+            ti = i[0]
+            t1 = t0[ti:]
+            for jj,j in enumerate(t1):
+                try:
+                    tk = j.split()[0]
+                except:
+                    continue
+                if j.split()[0] == 'Target_Name:':
+                    visit_target_list.append(j.split()[-1]) 
+                    break
+        #####
+        print('Preparing output ...\n')
+        output = []
+        for ii,i in enumerate(visit_number_list):
+            t1 = visit_number_list[ii][0]
+            t2 = visit_number_list[ii][1].split()[-1]
+            t3 = visit_target_list[ii]
+            output.append((t1,t2,t3))
+        #####
+        print('Finish.')
+        return output
+
---
+++ packages/rolling-snapshot-proposal-editor/rolling_snapshot_proposal_editor-0.0.0a21-py3-none-any.whl/rolling_snapshot_proposal_editor/googlesheetreader.py
@@ -0,0 +1,68 @@
+class GoogleSheetReader:
+    """
+    This class handles reading Google Spreadsheet directly using Google API.
+    #####
+    Usage:
+    t = GoogleSheetReader(scopes,sheetid,rangename,credential,verbal=True) -- Initialization. Check references for more info about these arguments.
+    t.read() -- Read the google sheet.
+    t.df -- Show the sheet.
+    #####
+    Ref:
+    1: https://developers.google.com/sheets/api/quickstart/python 
+    2: https://stackoverflow.com/questions/65184355/error-403-access-denied-from-google-authentication-web-api-despite-google-acc
+    """
+    def __init__(self,scopes,sheetid,rangename,credential,verbal=True):
+        self.scopes = scopes
+        self.sheetid = sheetid
+        self.rangename = rangename
+        self.credential = credential
+        self.verbal = verbal
+        if self.verbal: print('Use self.read() to read the sheet.\n')
+    ####################
+    ####################
+    ####################
+    # from https://developers.google.com/sheets/api/quickstart/python    
+    def read(self):
+        import os.path
+        from googleapiclient.discovery import build
+        from google_auth_oauthlib.flow import InstalledAppFlow
+        from google.auth.transport.requests import Request
+        from google.oauth2.credentials import Credentials
+        import pandas as pd
+        #####
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', self.scopes)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    self.credential,self.scopes)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=self.sheetid,
+                                    range=self.rangename).execute()
+        values = result.get('values', [])
+
+        ##### organize in pandas
+        df = pd.DataFrame(values)
+        df.columns = df.iloc[0]
+        df.drop(df.index[0],inplace=True)
+        self.df = df
+        if self.verbal: print('Access the sheet by self.df ...\n')
+    ####################
+    ####################
+    ####################
+    
