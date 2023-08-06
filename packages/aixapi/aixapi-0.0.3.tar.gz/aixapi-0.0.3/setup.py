d26feeb1dedcf59679990469ecadd708ade990c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:31 +0000

    Add package aixapi/0.0.3/aixapi-0.0.3.tar.gz

---
+++ packages/aixapi/aixapi-0.0.3.tar.gz/aixapi-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aixapi",
+    version="0.0.3",
+    author="AIx Solutions Group, Inc",
+    author_email="info@aixsolutionsgroup.com",
+    description="AI for everyone. With this API, you can access GPT-J. "
+                "GPT-J is as powerful as OpenAI's GPT-3 Curie engine.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AIx-Solutions/aix-gpt-api",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
