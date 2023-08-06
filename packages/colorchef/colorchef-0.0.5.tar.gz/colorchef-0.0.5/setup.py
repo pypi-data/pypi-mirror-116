4912559eaa4cc952be75275ca8450c9deb9063a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:59 +0000

    Add package colorchef/0.0.5/colorchef-0.0.5.tar.gz

---
+++ packages/colorchef/colorchef-0.0.5.tar.gz/colorchef-0.0.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+from os import path
+
+VERSION = '0.0.5' 
+DESCRIPTION = 'Paint your terminal with beautifull colors.'
+# LONG_DESCRIPTION = 'This package lets you print colored text on the terminal.'
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+# Setting up
+setup(
+       # the name must match the folder name 'colorchef'
+        name="colorchef", 
+        version=VERSION,
+        author="Mithilesh Pradhan",
+        author_email="knowmit@gmail.com",
+        description=DESCRIPTION,
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package.
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Information Technology",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
