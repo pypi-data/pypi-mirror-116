2950c34bb02a7311626074e4b40bede44f9a601c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:46 +0000

    Add package parqser/1.0.13/parqser-1.0.13-py3-none-any.whl

---
+++ packages/parqser/parqser-1.0.13-py3-none-any.whl/parqser/__init__.py
@@ -0,0 +1,14 @@
+"""
+parqser
+
+Finally, a good parser
+"""
+import parqser.page
+import parqser.parser
+import parqser.saver
+import parqser.scrapper
+import parqser.session
+import parqser.web_component
+
+__version__ = "1.0.13"
+__author__ = 'Ilya Shamov'
