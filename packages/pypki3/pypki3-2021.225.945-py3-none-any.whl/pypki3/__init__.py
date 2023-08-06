0ab3cc028e236dbf4c3d6f631b9f2f6cd3f841fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:54 +0000

    Add package pypki3/2021.225.945/pypki3-2021.225.945-py3-none-any.whl

---
+++ packages/pypki3/pypki3-2021.225.945-py3-none-any.whl/pypki3/__init__.py
@@ -0,0 +1,12 @@
+# vim: expandtab tabstop=4 shiftwidth=4
+
+from typing import Optional
+
+from .config import Loader
+
+loader = Loader()
+
+prepare = loader.prepare
+ssl_context = loader.ssl_context
+NamedTemporaryKeyCertPaths = loader.NamedTemporaryKeyCertPaths()
+pip = loader.pip
