3d05aa6776bec219c9f418d41ca96bd88eb7eb3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:22 +0000

    Add package data-structures-nicorodc/0.3.1/data_structures_nicorodc-0.3.1-py3-none-any.whl

---
+++ packages/data-structures-nicorodc/data_structures_nicorodc-0.3.1-py3-none-any.whl/tests/__init__.py
@@ -0,0 +1,20 @@
+"""Tests for the data-structures package.
+
+Modules
+-------
+
+    test_elements:
+        Classes
+        -------
+            TestNumber: Tests for the Number class.
+
+    test_stacks:
+        Classes
+        -------
+            TestStack: Tests for the Stack class.
+
+    test_queues:
+        Classes
+        -------
+            TestQueue: Tests for the Queue class.
+"""
