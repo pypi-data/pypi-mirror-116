9306c7d2262c58c83ca8e11fb2547940e4dd312a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:28 +0000

    Add package multiSourceWordMaps/1.0.0/multiSourceWordMaps-1.0.0.tar.gz

---
+++ packages/multiSourceWordMaps/multiSourceWordMaps-1.0.0.tar.gz/multiSourceWordMaps-1.0.0/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+from distutils.command.build_py import build_py
+import os
+import json
+        
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="multiSourceWordMaps",
+    version="1.0.0",
+    author="Ot Gabaldon Torrents",
+    author_email="gabaldonot@gmail.com",
+    description="Allows the user to list sources and compile word maps from those sources",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OtGabaldon/multiSourceWordMaps",
+    project_urls={
+        "Bug Tracker": "https://github.com/OtGabaldon/multiSourceWordMaps/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires = [
+        "pdfminer.six",
+        "wordcloud",
+        "bs4",
+        "requests",
+        "pytest",
+        "pytest-cov"
+    ],
+    entry_points= {
+        'console_scripts': [
+            'map = multiSourceWordMaps.wordMapCreator:main'
+        ]
+    }
+)
