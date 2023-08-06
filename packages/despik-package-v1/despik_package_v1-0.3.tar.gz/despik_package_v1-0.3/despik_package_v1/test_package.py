f10ce29aee7f3e1bf64474cb71744f84a265b7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:58 +0000

    Add package despik-package-v1/0.3/despik_package_v1-0.3.tar.gz

---
+++ packages/despik-package-v1/despik_package_v1-0.3.tar.gz/despik_package_v1-0.3/despik_package_v1/test_package.py
@@ -0,0 +1,17 @@
+from despik_package_v1.despik_package_v1 import despik_package
+import pytest
+
+
+@pytest.mark.parametrize("a, expected_results", [('sdfg', ['SDFG']),
+                                                    (['sdfg', 'pragf'], ['SDFG', 'PRAGF'])])
+def test_isupper(a, expected_results):
+    assert despik_package.ticker_upper(a) == expected_results
+
+
+
+
+
+
+
+
+
---
+++ packages/despik-package-v1/despik_package_v1-0.3.tar.gz/despik_package_v1-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='despik_package_v1',
+      version='0.3',
+      description='Some work with yfinance for self education',
+      packages=find_packages(),
+      author_email='artomonik@gmail.com',
+      install_requires=['yfinance', 'pandas', 'matplotlib', 'seaborn', 'sklearn']
+      )
