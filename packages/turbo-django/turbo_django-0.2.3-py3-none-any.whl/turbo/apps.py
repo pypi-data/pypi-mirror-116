49582ab16084e35bc9d71b0933d964e64efba1c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:42 +0000

    Add package turbo-django/0.2.3/turbo_django-0.2.3-py3-none-any.whl

---
+++ packages/turbo-django/turbo_django-0.2.3-py3-none-any.whl/turbo/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class TurboDjangoConfig(AppConfig):
+    """The default AppConfig for admin which does autodiscovery."""
+    name = "turbo"
+
+    def ready(self):
+        super().ready()
+        self.module.autodiscover()
