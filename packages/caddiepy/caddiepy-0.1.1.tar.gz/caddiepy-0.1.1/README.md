a08e435ccd66b4260e16c81644f94eb96093786c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:48 +0000

    Add package caddiepy/0.1.1/caddiepy-0.1.1.tar.gz

---
+++ packages/caddiepy/caddiepy-0.1.1.tar.gz/caddiepy-0.1.1/README.md
@@ -0,0 +1,8 @@
+<p align="center">
+  <img alt="CADDIE Logo" src="https://github.com/Maiykol/caddiepy/blob/main/caddie_logo.png?raw=true" />
+</p>
+
+
+# caddiepy
+
+The python package to the Cancer Driver Drug Interaction Explorer (CADDIE)
---
+++ packages/caddiepy/caddiepy-0.1.1.tar.gz/caddiepy-0.1.1/caddiepy/__init__.py
@@ -0,0 +1,13 @@
+"""
+caddiepy.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Michael Hartung'
+__credits__ = """
+Prof. Dr. Jan Baumbach, Chair of Computational Systems Biology (https://www.cosy.bio/), University of Hamburg ; 
+Dr. Markus List, Head of the Research Group Big Data in BioMedicine (https://biomedical-big-data.de/), Technical University of Munich"""
+
+from caddiepy import api
+from caddiepy.task import Task
+from caddiepy.examples import GDSC_study
