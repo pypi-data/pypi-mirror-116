d4adb1996b8537890c3c7de1d49e36fa402b7bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:56 +0000

    Add package deezloader/2021.8.18/deezloader-2021.8.18.tar.gz

---
+++ packages/deezloader/deezloader-2021.8.18.tar.gz/deezloader-2021.8.18/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+	name = "deezloader",
+	version = "2021.08.18",
+	description = "Downloads songs, albums or playlists from deezer",
+	license = "CC BY-NC-SA 4.0",
+	author = "An0nimia",
+	author_email = "An0nimia@protonmail.com",
+	url = "https://github.com/An0nimia/deezloader",
+	packages = ["deezloader", "deezloader/models"],
+
+	install_requires = [
+		"mutagen", "pycryptodome", "requests",
+		"spotipy", "tqdm"
+	],
+
+	scripts = ["deezloader/bin/deez-dw.py", "deezloader/bin/deez-web.py"]
+)
