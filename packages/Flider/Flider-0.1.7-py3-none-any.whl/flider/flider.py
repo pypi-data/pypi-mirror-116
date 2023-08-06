f0f35a61e2c924785fad822248b33706ab2ea2d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:11 +0000

    Add package Flider/0.1.7/Flider-0.1.7-py3-none-any.whl

---
+++ packages/Flider/Flider-0.1.7-py3-none-any.whl/flider/flider.py
@@ -0,0 +1,26 @@
+import os
+import pkgutil
+
+def Flider():
+    try:
+        os.mkdir('flask_app')
+        dirs = ['templates', 'config', 'models', 'controllers', 
+        'static', 'static/css', 'static/js', 'static/img']
+        for dir in dirs:
+            os.mkdir('flask_app/'+dir)
+    except Exception as e:
+        print(e)
+        print('Continuing...')
+    save_paths = ['flask_app/templates/', 'flask_app/config', 'flask_app/', 'flask_app/static/css/', 'flask_app/static/js/', '']
+    file_names = ['index.html', 'mysqlcontroller.py', '__init__.py', 'style.css', 'scripts.js', 'server.py']
+    read_names = ['index.flid', 'mysqlcontroller.flid', 'init.flid', 'style.flid', 'scripts.flid', 'server.flid']
+    for i in range(len(save_paths)):
+        try:
+            completeName = os.path.join(save_paths[i], file_names[i])
+            data = pkgutil.get_data(__name__, 'read_files/' + read_names[i])
+            write_file = open(completeName, 'w')
+            write_file.write(data)
+            write_file.close()
+        except Exception as e:
+            print(e)
+            print('Error on: ' + save_paths[i] + file_names[i])
