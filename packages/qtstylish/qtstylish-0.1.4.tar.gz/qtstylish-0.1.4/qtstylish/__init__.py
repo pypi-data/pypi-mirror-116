5aa97d4b7f41955a39034ae51c5051a62cb5d690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:33 +0000

    Add package qtstylish/0.1.4/qtstylish-0.1.4.tar.gz

---
+++ packages/qtstylish/qtstylish-0.1.4.tar.gz/qtstylish-0.1.4/qtstylish/__init__.py
@@ -0,0 +1,10 @@
+# Set version
+from pkg_resources import get_distribution
+__version__ = get_distribution('pandasgui').version
+
+# Imports
+from qtstylish.qtstylish import dark, light, ThemeSwitcher
+from qtstylish.demo_widget import DemoWidget
+from qtstylish.modern_window import ModernWindow
+
+__all__ = ["dark", "light", "ThemeSwitcher", "DemoWidget", "ModernWindow", "__version__"]
