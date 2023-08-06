0df782b65b5e33994398ee70cf3612a5806819c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:11 +0000

    Add package WebEdge/1.0.2/WebEdge-1.0.2.tar.gz

---
+++ packages/WebEdge/WebEdge-1.0.2.tar.gz/WebEdge-1.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    REQUIREMENTS = f.read().splitlines()
+with open('README.md', encoding='utf8') as f:
+    README = f.read()
+
+setup(
+    name='WebEdge',
+    version='1.0.2',
+    license='MIT License',
+    author='MLH Fellowship Team 1',
+    author_email='erbeusgriffincasper@gmail.com',
+    description='Bringing Edge to your Web Performance',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/HarshCasper/WebEdge',
+    install_requires=REQUIREMENTS,
+    packages=find_packages(exclude = ["*.tests", "*.tests.*", "tests.*", "tests"]),
+    entry_points={
+        'console_scripts': [
+            'webedge = webedge.webedge:main'
+        ]
+    }
+)
