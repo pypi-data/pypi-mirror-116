fc20c45b81700569ebfbf060315f13a7a5f7cd75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:07 +0000

    Add package TeraApp/0.0.4/TeraApp-0.0.4.tar.gz

---
+++ packages/TeraApp/TeraApp-0.0.4.tar.gz/TeraApp-0.0.4/TeraApp/AppScreen/test.py
@@ -0,0 +1,13 @@
+from TeraApp1.TeraApp.AppScreen.AppScreen import *
+
+app = TeraApp('TeraApp', 500, 500, 'blue') #500 = x,500(2) = y
+app.txt('Title', 240,2, 15) #(x = 240), (y = 2), (15 = txt size)
+input1 = app.input(210, 30, 'white')
+
+def fds():
+    print(app.input_name.text())
+
+
+app.buttonf('save', 210,100, 'green', fds)
+app.app.exec()
+
