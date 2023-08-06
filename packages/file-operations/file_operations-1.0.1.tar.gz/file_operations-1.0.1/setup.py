cc9260d2a5ef1382b8541fe0e86e9ef0357ffda4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:40 +0000

    Add package file-operations/1.0.1/file_operations-1.0.1.tar.gz

---
+++ packages/file-operations/file_operations-1.0.1.tar.gz/file_operations-1.0.1/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+import setuptools
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+__version__ = "1.0.1"
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setuptools.setup(
+    name="file_operations",
+    version=__version__,
+    description="Simple API for performing file and directory operations",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Algorithms Path",
+    author_email="support@algorithmspath.com",
+    url='http://pypi.python.org/pypi/file_operations/',
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    # packages=setuptools.find_packages(),
+    # package_data={'example' : ['kmp_utils/example.cpython-38-x86_64-linux-gnu.so']},
+    include_package_data=True,
+    install_requires=["shutil"],
+    python_requires='>=3'
+)
+
+# python3 setup.py sdist bdist_wheel
+# twine upload dist/*
