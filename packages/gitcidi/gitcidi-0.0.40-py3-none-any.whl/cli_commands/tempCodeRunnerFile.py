37b03b251ccc736042f27dbcaa134f937d90eddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:38 +0000

    Add package gitcidi/0.0.40/gitcidi-0.0.40-py3-none-any.whl

---
+++ packages/gitcidi/gitcidi-0.0.40-py3-none-any.whl/cli_commands/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+            # path=pathlib.Path().resolve()
+            # pathr=path+"/"+"cli-tool"
+            # for root, subdirs, files in os.walk(pathr):
+            #    for d in subdirs:
+            #       if d == ".git":
+            #        print("found the clone") 
