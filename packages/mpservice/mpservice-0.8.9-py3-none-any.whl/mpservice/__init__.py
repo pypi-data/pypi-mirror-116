82a9d479160ed4a057e8c64443269c00a224f5e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:07 +0000

    Add package mpservice/0.8.9/mpservice-0.8.9-py3-none-any.whl

---
+++ packages/mpservice/mpservice-0.8.9-py3-none-any.whl/mpservice/__init__.py
@@ -0,0 +1,14 @@
+__version__ = '0.8.9'
+
+# NOTE: these imports will be removed in a future release.
+# Please import them from the relevant modules directly.
+
+from .mpserver import Server, Servlet
+from .mpserver import TimeoutError, EnqueueTimeout, TotalTimeout
+from .mperror import MPError
+
+__all__ = [
+    'Servlet', 'Server',
+    'MPError',
+    'TimeoutError', 'EnqueueTimeout', 'TotalTimeout',
+]
