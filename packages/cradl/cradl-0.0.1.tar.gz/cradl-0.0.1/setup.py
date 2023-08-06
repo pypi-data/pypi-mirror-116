19eb86931c97480f923bfe981983755dcfe137e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:46 +0000

    Add package cradl/0.0.1/cradl-0.0.1.tar.gz

---
+++ packages/cradl/cradl-0.0.1.tar.gz/cradl-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cradl',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.cradl.ai',
+    author='August Kvernmo',
+    author_email='august@lucidtech.ai',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
