2cbfcde299bbc8aa4df010743b9826bdb16c7a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:31 +0000

    Add package Flask-Parcel/0.0.3/Flask-Parcel-0.0.3.tar.gz

---
+++ packages/Flask-Parcel/Flask-Parcel-0.0.3.tar.gz/Flask-Parcel-0.0.3/setup.py
@@ -0,0 +1,35 @@
+"""
+Flask-Parcel
+-------------
+
+ParcelJS Extension for Flask
+"""
+import setuptools
+
+with open("README.md") as fh:
+    long_description = fh.read().strip()
+
+setuptools.setup(
+    name='Flask-Parcel',
+    version='0.0.3',
+    url='https://github.com/ysenarath/flask-parcel',
+    license='MIT',
+    author='Yasas Senarath',
+    author_email='ysenarath.93@gmail.com',
+    description='Run parcel builds alongside flask.',
+    long_description=long_description,
+    py_modules=['flask_parcel'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=['Flask'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
