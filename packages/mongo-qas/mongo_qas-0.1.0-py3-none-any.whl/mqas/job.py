c638e980f18e608cde00e49bac8de7b3c6b4e547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:44 +0000

    Add package mongo-qas/0.1.0/mongo_qas-0.1.0-py3-none-any.whl

---
+++ packages/mongo-qas/mongo_qas-0.1.0-py3-none-any.whl/mqas/job.py
@@ -0,0 +1,113 @@
+from pymongo.collection import Collection
+from typing import Type, Callable
+from datetime import datetime
+from .misc import toOid
+import importlib
+
+class Job:
+  def __init__(self, id, data, collection: Type[Collection], **kwargs):
+    self.id = toOid(id)
+    self.coll = collection
+    self.payload = data
+    self.kwargs = kwargs
+    self.result = None
+    self.logger = None
+    self.verbosity = "error"
+
+    if "logger" in kwargs:
+      self.logger = kwargs["logger"]
+
+    if "verbosity" in kwargs:
+      self.verbosity = kwargs["verbosity"]
+
+  def data(self):
+    return self.payload
+
+  def set_result(self, result):
+    self.result = result
+    self.coll.update_one({"_id": self.id}, {"$set": {"result": self.result}})
+    return True
+
+  def complete(self, result=None):
+    if result is None:
+      result = self.result
+    
+    self.coll.update_one({"_id": self.id}, {"$set": {"progress": 100, "inProgress": False, "done": True, "result": result, "completedAt": datetime.utcnow()}})
+    
+    if "on_success" in self.payload:
+      payload = self.payload
+      try:
+        if not payload is None:
+          callback = payload.get("on_success")
+          if not callback is None:
+            if str(callback).__contains__("."):
+              mod_name, func_name = callback.rsplit(".", 1)
+              mod = importlib.import_module(mod_name)
+              func = getattr(mod, func_name)
+              args = payload.get("args", [])
+              kwargs = payload.get("kwargs", {})
+              func(result, *args, **kwargs)
+            elif callback in globals():
+              func = globals()[callback]
+              args = payload.get("args", [])
+              kwargs = payload.get("kwargs", {})
+              func(result, *args, **kwargs)
+
+      except Exception as ex:
+        self.logger("error", error_message=f"Error: {ex}")
+
+    self.logger("completed", result=result)
+    return True
+
+  def error(self, msg=None):   
+    self.coll.update_one({"_id": self.id}, {"$inc": {"attempts": 1}, "$set": {"inProgress": False, "error": True, "lastErrorAt": datetime.utcnow(), "errorMessage": msg}})
+    
+    if "on_failure" in self.payload:
+      payload = self.payload
+      try:
+        if not payload is None:
+          callback = payload.get("on_failure")
+          if not callback is None:
+            if str(callback).__contains__("."):
+              mod_name, func_name = callback.rsplit(".", 1)
+              mod = importlib.import_module(mod_name)
+              func = getattr(mod, func_name)
+              args = payload.get("args", [])
+              kwargs = payload.get("kwargs", {})
+              func(msg, *args, **kwargs)
+            elif callback in globals():
+              func = globals()[callback]
+              args = payload.get("args", [])
+              kwargs = payload.get("kwargs", {})
+              func(msg, *args, **kwargs)
+
+      except Exception as ex:
+        self.logger("error", error_message=f"Error: {ex}")
+
+    self.logger("error", error_message=msg)
+    return True
+
+  def log(self, type, *args, **kwargs):
+    if self.logger is None:
+      if self.verbosity.__contains__(type):
+        payload = self.payload
+        _args = payload.get("args", [])
+        _kwargs = payload.get("kwargs", {})
+        _args = _args + args
+        _kwargs.update(kwargs)
+        self.logger(type, *_args, **_kwargs)
+
+  def progress(self, percent, msg=None):
+    self.coll.update_one({"_id": self.id}, {"$set": {"progress": percent, "progressMessage": msg, "lastProgressAt": datetime.utcnow()}})
+    self.logger("progress", percent=percent, progress_message=msg)
+    return True
+
+  def setLogger(self, logger):
+    self.logger = logger
+
+  def setVerbosity(self, verbosity):
+    self.verbosity = verbosity
+
+  def release(self):
+    self.coll.update_one({"_id": self.id}, {"$set": {"inProgress": False, "error": False, "done": False, "releasedAt": datetime.utcnow(), "attempts": 0}})
+    return True
---
+++ packages/mongo-qas/mongo_qas-0.1.0-py3-none-any.whl/mqas/worker.py
@@ -0,0 +1,133 @@
+from .queue import Queue
+from .job import Job
+from typing import Type, Union, Tuple, Callable
+from time import sleep
+from threading import Thread
+import importlib
+
+class Worker:
+
+  def __init__(self, queues: Union[Tuple[Queue,...], Type[Queue]], channel: Union[Tuple[str,...], str]=None, heart_beat: int = 1, verbosity: str = "error", logger: Union[str, Callable] = None ) -> None:
+    
+    if isinstance(queues, tuple) or isinstance(queues, list):
+      self.queues = queues
+    elif isinstance(queues, Queue):
+      self.queues = tuple([queues])
+
+    if isinstance(channel, tuple) or isinstance(channel, list):
+      self._channels = channel
+    elif isinstance(channel, str):
+      self._channels = [channel]
+    else:
+      self._channels = None
+
+    self._working = False
+    self._heart_beat = heart_beat
+    self._running = False
+
+    self._verbosity = verbosity
+
+    if callable(logger):
+      self._logger = logger
+    elif isinstance(logger, str):
+      callback = str(logger)
+      if str(callback).__contains__("."):
+        mod_name, func_name = callback.rsplit(".", 1)
+        mod = importlib.import_module(mod_name)
+        func = getattr(mod, func_name)
+        self._logger = func
+      elif callback in globals():
+        func = globals()[callback]
+        self._logger = func
+    else:
+      self._logger = None
+
+  def setLogger(self, logger: Union[str, Callable]):
+    if callable(logger):
+      self._logger = logger
+    elif isinstance(logger, str):
+      callback = str(logger)
+      if str(callback).__contains__("."):
+        mod_name, func_name = callback.rsplit(".", 1)
+        mod = importlib.import_module(mod_name)
+        func = getattr(mod, func_name)
+        self._logger = func
+      elif callback in globals():
+        func = globals()[callback]
+        self._logger = func
+    else:
+      self._logger = None
+
+  def setVerbosity(self, verbosity: str):
+    self.verbosity = verbosity
+
+  def start(self):
+    self._running = True
+    self._run()
+
+  def stop(self):
+    self._running = False
+
+  def _work(self):
+    self._working = True
+    job: Type[Job] = None
+
+    for queue in self.queues:
+      if self._channels is None:
+        job = queue.dequeue()
+        if not job is None:
+          break
+      else:
+        for channel in self._channels:
+          job = queue.dequeue(channel)
+          if not job is None:
+            break
+        if not job is None:
+          break
+
+    if not job is None:
+      self.thread = Thread(target=self._run_job, kwargs={'job': job})
+      self.thread.start()
+      self.thread.join()
+      self._working = False
+    else:
+      self._working = False
+
+  def _run_job(self, job: Type[Job]):
+    payload = job.payload
+    job.setVerbosity(self._verbosity)
+    job.setLogger(self._logger)
+    try:
+      if not payload is None:
+        callback = payload.get("function_name")
+        if not callback is None:
+          if str(callback).__contains__("."):
+            mod_name, func_name = callback.rsplit(".", 1)
+            mod = importlib.import_module(mod_name)
+            func = getattr(mod, func_name)
+            args = payload.get("args", [])
+            kwargs = payload.get("kwargs", {})
+            result = func(*args, **kwargs)
+            job.complete(result)
+          elif callback in globals():
+            func = globals()[callback]
+            args = payload.get("args", [])
+            kwargs = payload.get("kwargs", {})
+            result = func(*args, **kwargs)
+            job.complete(result)
+          else:
+            job.error(f"Function {callback} not found!")
+
+    except Exception as ex:
+      job.error(f"Error: {ex}")
+      print(f"Error: {ex}")
+
+  def _run(self):
+    while True:
+      if not self._running:
+        break
+
+      if not self._working:
+        self._work()
+
+      sleep(self._heart_beat)
