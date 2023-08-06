421841042b8e184dd759a02f562cf2a0a4c9a2d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:23 +0000

    Add package f1z1-async-http/0.2.0/f1z1_async_http-0.2.0-py3-none-any.whl

---
+++ packages/f1z1-async-http/f1z1_async_http-0.2.0-py3-none-any.whl/f1z1_async_http/src/components/event_hook/__init__.py
@@ -0,0 +1,19 @@
+# @Time     : 2021/7/18
+# @Project  : f1z1-g
+# @IDE      : PyCharm
+# @Author   : Angel
+# @Email    : 376355670@qq.com
+from .base import IAsyncEventHook, IAsyncEventHooks
+from .hooks import (
+    AsyncEventHookList,
+    AsyncEventHooksDecorate,
+    AsyncFunctionHooks,
+    AsyncEventHooks,
+    AsyncFunction
+)
+from .manager import (
+    AbstractEventHooksManager,
+    FunctionHooksManager,
+    EventHooksManager,
+    HookOrAsyncFunc
+)
