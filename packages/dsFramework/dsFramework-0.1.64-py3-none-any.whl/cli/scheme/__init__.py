a1c0113a3d74fbc847131c4208ff85e72f976b7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:07 +0000

    Add package dsFramework/0.1.64/dsFramework-0.1.64-py3-none-any.whl

---
+++ packages/dsFramework/dsFramework-0.1.64-py3-none-any.whl/cli/scheme/__init__.py
@@ -0,0 +1,3 @@
+from cli.scheme.inputs_template import generatedClass
+from cli.scheme.outputs_template import generatedClass
+
---
+++ packages/dsFramework/dsFramework-0.1.64-py3-none-any.whl/cli/scheme/__init___template.py
@@ -0,0 +1,2 @@
+from server.test_scheme.test_input import TestInputRequest
+from server.test_scheme.test_output import TestOutputResponse
