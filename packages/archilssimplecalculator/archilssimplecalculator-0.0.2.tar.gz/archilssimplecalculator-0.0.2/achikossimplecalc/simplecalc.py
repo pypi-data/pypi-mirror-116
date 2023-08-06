6ba5b30378f6324ec152ed54cda73a2bfe747cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:43 +0000

    Add package archilssimplecalculator/0.0.2/archilssimplecalculator-0.0.2.tar.gz

---
+++ packages/archilssimplecalculator/archilssimplecalculator-0.0.2.tar.gz/archilssimplecalculator-0.0.2/achikossimplecalc/simplecalc.py
@@ -0,0 +1,13 @@
+class calc:
+
+    def add(self, a, b):
+        return a + b
+
+    def substract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def devise(self, a, b):
+        return a / b
