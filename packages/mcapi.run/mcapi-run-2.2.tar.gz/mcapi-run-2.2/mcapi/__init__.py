73d3a2c2475f272f5ce72d4e27b7a3ffc8ddccc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:05 +0000

    Add package mcapi.run/2.2/mcapi-run-2.2.tar.gz

---
+++ packages/mcapi.run/mcapi-run-2.2.tar.gz/mcapi-run-2.2/mcapi/__init__.py
@@ -0,0 +1,179 @@
+import requests,os,sys,json
+from colorama import *
+from sys import platform
+from time import *
+import logging
+
+init(autoreset=True)
+
+
+def cool_text(text):
+    for char in str(f"{text}"):
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        sleep(0.05)
+    
+
+def run():
+    if platform == "linux" or platform == "linux2":
+        os.system("clear")
+    elif platform == "darwin":
+        os.system("clear")
+    elif platform == "win32":
+        os.system("cls")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print(
+f"""
+{Fore.WHITE}
+           {Fore.WHITE}
+          /^\\
+          |-|
+          |{Fore.RED}m{Fore.WHITE}|
+          |{Fore.RED}c{Fore.WHITE}|
+          |{Fore.RED}a{Fore.WHITE}|
+          |{Fore.RED}p{Fore.WHITE}|
+          |{Fore.RED}i{Fore.WHITE}|
+         /|{Fore.RED}!{Fore.WHITE}|\\
+        / | | \\
+       |  | |  |
+       `-\\"\\"\\"-`
+     
+""")
+    sleep(1)
+    delay23 = 320
+    for i in range(50):
+        print("")
+        sleep(delay23/2000)
+        delay23 = delay23 * 0.9
+
+
+
+
+
+
+def init(show=None):
+    if(show == None or show == True):
+        if platform == "linux" or platform == "linux2":
+            os.system("clear")
+        elif platform == "darwin":
+            os.system("clear")
+        elif platform == "win32":
+            os.system("cls")
+        while True:
+            try:
+                check = requests.get("http://api.mcapi.run/", timeout=1).status_code
+                if(check == 200):
+                    run()
+                    print("tip : open cmd and run 'mcapi-help.py' for a full help script!")
+                    sleep(3)
+                    break
+                else:
+                    print(f"{Fore.RED}cant init the api")
+                    break
+            except requests.Timeout:
+                print(f"{Fore.RED}cant init the api")
+                break
+    if(show == False):
+        if platform == "linux" or platform == "linux2":
+            os.system("clear")
+        elif platform == "darwin":
+            os.system("clear")
+        elif platform == "win32":
+            os.system("cls")
+        while True:
+            try:
+                check = requests.get("http://api.mcapi.run/", timeout=1).status_code
+                if(check == 200):
+                    break
+                else:
+                    print(f"{Fore.RED}cant init the api")
+                    break
+            except requests.Timeout:
+                print(f"{Fore.RED}cant init the api")
+                break
+
+
+
+class options(object):
+    def player_info(self, name, search=None):
+        name = str(name)
+        if(name == None):
+            print(f"{Fore.RED}please specify a name")
+        else:
+            if(search == None):
+                data = requests.get(f"http://api.mcapi.run/player/{name}", timeout=5).json()
+                print(data)
+                logging.info(f"{data}")
+            else:
+                data = requests.get(f"http://api.mcapi.run/player/{name}", timeout=5).json()
+                print(data[f'{search}'])
+                logging.info(f"{data}")
+    def hypixel_stats(self, name, search=None):
+        name = str(name)
+        if(name == None):
+            print(f"{Fore.RED}please specify a name")
+        else:
+            if(search == None):
+                data = requests.get(f"http://api.mcapi.run/hypixel/{name}", timeout=5).json()
+                print(data)
+                logging.info(f"{data}")
+            else:
+                data = requests.get(f"http://api.mcapi.run/hypixel/{name}", timeout=5).json()
+                print(data[f'{search}'])
+                logging.info(f"{data}")
+        #data = requests.get(f"http://api.mcapi.run/hypixel/{name}", timeout=5).json()
+        #print(data)
+    def name_droptime(self, name):
+        name = str(name)
+        if(name == None):
+            print(f"{Fore.RED}please specify a name")
+        else:
+            data = requests.get(f"http://api.mcapi.run/droptime/{name}", timeout=5).json()
+            print(data)
+            logging.info(f"{data}")
+
+
+def get():
+    return options()
+
+
+def clear_screen():
+    if platform == "linux" or platform == "linux2":
+        os.system("clear")
+    elif platform == "darwin":
+        os.system("clear")
+    elif platform == "win32":
+        os.system("cls")
+
+        
---
+++ packages/mcapi.run/mcapi-run-2.2.tar.gz/mcapi-run-2.2/test.py
@@ -0,0 +1,5 @@
+import mcapi
+
+mcapi.output_file('C:/Users/silke/Music/change bot/discord sniper bot/api/mcapi/test/mcapi-main/mcapi/log.txt')
+
+mcapi.get().player_info("hypixel")
