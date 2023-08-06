78ea580f408a0b255c2c9c5c1186ffe64293bca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:05 +0000

    Add package chuy/0.3.0/chuy-0.3.0.tar.gz

---
+++ packages/chuy/chuy-0.3.0.tar.gz/chuy-0.3.0/chuy/__init__.py
@@ -0,0 +1,12 @@
+"""
+This file define chuy as a module.
+"""
+
+from .core import main
+
+# Package Data
+__version__ = "0.1.1"
+__authors__ = ["Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>"]
+__author_email__ = "eliaz.bobadilladev@gmail.com"
+__url__ = "https://github.com/UltiRequiem/chuy"
+__package_name__ = "chuy"
