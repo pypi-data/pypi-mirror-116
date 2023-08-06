f16f51d48559218c6da3c5558c1c0ad2c9ded467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:59 +0000

    Add package mlplatform-lib/8.4.0.0.0.0.4/mlplatform_lib-8.4.0.0.0.0.4.tar.gz

---
+++ packages/mlplatform-lib/mlplatform_lib-8.4.0.0.0.0.4.tar.gz/mlplatform_lib-8.4.0.0.0.0.4/mlplatform_lib/dataclass/model/model_dto.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass, field
+from dataclasses_json import dataclass_json, LetterCase
+from mlplatform_lib.dataclass.experiment.type import ExperimentType
+from mlplatform_lib.dataclass.model.type import ModelStatus
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class ModelDto:
+    id: int = field(init=False, default = 0)
+    train_id: int = field(init=False)
+    name: int
+    experiment_type: ExperimentType = field(init=False)
+    description: str
+    model_path: str
+    status: ModelStatus = field(init=False)
