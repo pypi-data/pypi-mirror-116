ec9796e403ef223256cc8b4936f2d26353669051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:50 +0000

    Add package authsrv/0.0.4/authsrv-0.0.4.tar.gz

---
+++ packages/authsrv/authsrv-0.0.4.tar.gz/authsrv-0.0.4/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='authsrv',
+    version='0.0.4',
+    description='wrapper for authsrv utilities',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    author='Nathan Neulinger',
+    author_email='nneul@neulinger.org',
+    keywords=['authsrv'],
+    url='https://github.com/unixtools/authsrv-python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
