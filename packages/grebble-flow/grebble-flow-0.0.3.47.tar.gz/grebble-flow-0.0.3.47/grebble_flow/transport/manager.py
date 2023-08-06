61a7dd1163bd2fc8a86cff3400969d506174209f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:36 +0000

    Add package grebble-flow/0.0.3.47/grebble-flow-0.0.3.47.tar.gz

---
+++ packages/grebble-flow/grebble-flow-0.0.3.47.tar.gz/grebble-flow-0.0.3.47/grebble_flow/transport/manager.py
@@ -0,0 +1,15 @@
+import logging
+from concurrent import futures
+
+import grpc
+from grebble_flow.transport.generated.sdk.v1 import processor_pb2_grpc
+from grebble_flow.transport.service.processor import ProcessorService
+
+
+def start_server(port):
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    processor_pb2_grpc.add_ProcessorServiceServicer_to_server(ProcessorService(), server)
+    server.add_insecure_port(f"[::]:{port}")
+    server.start()
+    logging.info("Server started")
+    server.wait_for_termination()
