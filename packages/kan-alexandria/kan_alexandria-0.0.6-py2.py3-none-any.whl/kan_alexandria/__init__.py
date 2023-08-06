d608429e3cac111e6e4075d7a391dd8dca64a209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:50 +0000

    Add package kan-alexandria/0.0.6/kan_alexandria-0.0.6-py2.py3-none-any.whl

---
+++ packages/kan-alexandria/kan_alexandria-0.0.6-py2.py3-none-any.whl/kan_alexandria/__init__.py
@@ -0,0 +1,14 @@
+"""
+Kan_Alexandria is book search utility so you spend less time searching and more time
+reading.
+Big thanks to Sang Han team for their work on the project.You can find their work at https://github.com/jjangsangy/kan
+This is a fork from their original work meant to better integrate with Python projects that need access to Google Books API
+"""
+
+from .__main__ import *
+from .__version__ import __version__, __release__
+
+__title__ = 'Kan_Alexandria'
+__author__ = 'João Rodrigues'
+__license__ = 'Apache Software License Version 2.0'
+__copyright__ = 'João Rodrigues 2021'
