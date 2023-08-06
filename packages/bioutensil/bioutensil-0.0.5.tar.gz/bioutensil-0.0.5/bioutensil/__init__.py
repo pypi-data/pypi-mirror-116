3d98f10e8059b44e14e2779247e617263007783c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:44 +0000

    Add package bioutensil/0.0.5/bioutensil-0.0.5.tar.gz

---
+++ packages/bioutensil/bioutensil-0.0.5.tar.gz/bioutensil-0.0.5/bioutensil/__init__.py
@@ -0,0 +1,24 @@
+from .version import __version__
+from . import bam
+from . import bash
+from . import basic
+from . import clustering
+from . import cnv
+from . import complex_haplotype
+from . import constants
+from . import converter
+from . import csv_aux
+from . import fasta
+from . import gene
+from . import genome
+from . import genomic_region
+from . import module
+from . import myio
+from . import myprint
+from . import psl_tran
+from . import sequence
+from . import shell
+from . import structure_variant
+from . import vcf_aux
+
+name = 'bioutensil'
