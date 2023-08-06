eddae9a13465616f548771c07d2b873a5cb76474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:22 +0000

    Add package pyexeqjy/0.1.6/pyexeqjy-0.1.6.tar.gz

---
+++ packages/pyexeqjy/pyexeqjy-0.1.6.tar.gz/pyexeqjy-0.1.6/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyexeqjy']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['pyexeqjy = pyexeqjy:entry.main']}
+
+setup_kwargs = {
+    'name': 'pyexeqjy',
+    'version': '0.1.6',
+    'description': 'This is a cli for developer of remote vehicle side',
+    'long_description': None,
+    'author': 'qiujingyu',
+    'author_email': 'qiujingyu@momenta.ai',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
