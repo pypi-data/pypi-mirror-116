e58eae171241c57bbbbe404b27a341c7b05db472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:07 +0000

    Add package MiniTensorflow/0.1.7/MiniTensorflow-0.1.7-py3-none-any.whl

---
+++ packages/MiniTensorflow/MiniTensorflow-0.1.7-py3-none-any.whl/mini_tensorflow/__init__.py
@@ -0,0 +1,6 @@
+import mini_tensorflow
+from mini_tensorflow.MiniTensorflow import Layer
+from mini_tensorflow.MiniTensorflow import Network
+__version__ = '0.1.7'
+__author__ = 'Aayushmaan Jain'
+__email__ = 'aayushmaan1306@gmail.com'
