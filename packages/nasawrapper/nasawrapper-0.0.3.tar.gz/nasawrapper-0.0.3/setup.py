7d88b9036c831110ca96d3e032435d34116230f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:42 +0000

    Add package nasawrapper/0.0.3/nasawrapper-0.0.3.tar.gz

---
+++ packages/nasawrapper/nasawrapper-0.0.3.tar.gz/nasawrapper-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as rm:
+    readme = rm.read()
+
+setup(
+    name="nasawrapper",
+    author="End313234",
+    url="https://github.com/End313234/nasawrapper-python",
+    project_urls={
+        "Bug Hunter": "https://github.com/End313234/nasawrapper-python/issues",
+        "Documentation": "https://github.com/End313234/nasawrapper-python#documentation"
+    },
+    version="0.0.3",
+    license="MIT",
+    description="A wrapper for NASA APIs",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6.0",
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Natural Language :: English",
+        "Intended Audience :: Developers",
+        "Topic :: Internet"
+    ],
+    packages=find_packages()
+)
