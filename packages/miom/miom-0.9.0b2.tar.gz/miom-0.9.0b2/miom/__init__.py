4a335edaec09cae70606fd6349946ac60d530513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:49 +0000

    Add package miom/0.9.0b2/miom-0.9.0b2.tar.gz

---
+++ packages/miom/miom-0.9.0b2.tar.gz/miom-0.9.0b2/miom/__init__.py
@@ -0,0 +1,9 @@
+from miom.miom import ( 
+    load,
+    Solvers,
+    Comparator,
+    ExtractionMode
+)
+
+__all__ = ["load", "Solvers", "Comparator", "ExtractionMode"]
+__version__ = "0.9.0-beta.2"
