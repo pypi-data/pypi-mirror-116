495be742f791e5838a5912445945d3b3bac7f67a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:48 +0000

    Add package EasyMyWeb/0.3/EasyMyWeb-0.3.tar.gz

---
+++ packages/EasyMyWeb/EasyMyWeb-0.3.tar.gz/EasyMyWeb-0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="EasyMyWeb",
+    version="0.3",
+    author="Sheng Fan",
+    author_email="1175882937@qq.com",
+    description="deploy your website quickly",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.ft2.club/fred913/easymyweb",
+    packages=setuptools.find_packages(),
+    install_requires=['flask', 'uvicorn', 'requests'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
