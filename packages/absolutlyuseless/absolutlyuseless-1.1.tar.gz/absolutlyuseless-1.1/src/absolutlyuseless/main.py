a0d472dcd287f9d50a2a5181138635ca9b1ac44c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:21 +0000

    Add package absolutlyuseless/1.1/absolutlyuseless-1.1.tar.gz

---
+++ packages/absolutlyuseless/absolutlyuseless-1.1.tar.gz/absolutlyuseless-1.1/src/absolutlyuseless/main.py
@@ -0,0 +1,6 @@
+import pyttsx3
+engine = pyttsx3.init()
+def lol():
+    engine.say("his package is absolutly useless. Donate to make an actual package at https://paypal.me/thinkerdesign")
+    print("This package is absolutly useless. Donate to make an actual package at https://paypal.me/thinkerdesign")
+lol()
