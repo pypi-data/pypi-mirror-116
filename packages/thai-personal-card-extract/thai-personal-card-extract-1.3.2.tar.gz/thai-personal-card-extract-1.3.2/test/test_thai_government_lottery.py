97a8868625df5c4651b21d64d113202b82e503f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:49 +0000

    Add package thai-personal-card-extract/1.3.2/thai-personal-card-extract-1.3.2.tar.gz

---
+++ packages/thai-personal-card-extract/thai-personal-card-extract-1.3.2.tar.gz/thai-personal-card-extract-1.3.2/test/test_thai_government_lottery.py
@@ -0,0 +1,8 @@
+import src.ThaiPersonalCardExtract as card
+reader = card.ThaiGovernmentLottery(
+    save_extract_result=True,
+    path_to_save="D:/dev/ThaiPersonalCardExtract/examples/extract/thai_government_lottery")
+    # for windows need to pass tesseract_cmd parameter to setup your tesseract command path.
+
+result = reader.extractInfo("../examples/card14.jpg")
+print(result)
