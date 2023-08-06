24436d26fa13b48959458d39cbd1f2680be1ac33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:00 +0000

    Add package music-assistant/0.2.7/music_assistant-0.2.7-py3-none-any.whl

---
+++ packages/music-assistant/music_assistant-0.2.7-py3-none-any.whl/music_assistant/web/static/precache-manifest.0504a7fc198e9909434f37e0845c324a.js
@@ -0,0 +1,342 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30622df32130bafc5b1a",
+    "url": "css/albumdetails.3ce7b418.css"
+  },
+  {
+    "revision": "d95b4ba79e1daf6688e1",
+    "url": "css/albumdetails~artistdetails~config~library-albums~library-artists~library-playlists~library-radios~li~3e652d87.3191dc9a.css"
+  },
+  {
+    "revision": "4e58462a8fc33d7bc472",
+    "url": "css/albumdetails~artistdetails~config~library-albums~library-artists~library-playlists~library-radios~li~7ba91e65.c4d2f106.css"
+  },
+  {
+    "revision": "d8387878613393a50b19",
+    "url": "css/albumdetails~artistdetails~library-albums~library-artists~library-playlists~library-radios~library-t~e7a02bed.9041c058.css"
+  },
+  {
+    "revision": "fac1a97cf9f51508d29a",
+    "url": "css/albumdetails~artistdetails~library-albums~library-artists~library-playlists~library-radios~library-t~f6c0cde4.f8b19a30.css"
+  },
+  {
+    "revision": "8f8071b032457e240c9a",
+    "url": "css/app.f21a719d.css"
+  },
+  {
+    "revision": "43b7d24965d6529f262e",
+    "url": "css/artistdetails.7b987af9.css"
+  },
+  {
+    "revision": "738729a33752e0127c90",
+    "url": "css/chunk-vendors.8aa5e75e.css"
+  },
+  {
+    "revision": "9537a62a9ce9d286a94e",
+    "url": "css/config.820246e5.css"
+  },
+  {
+    "revision": "7e87e9d56f02d2e6ccf9",
+    "url": "css/config~main.db52994d.css"
+  },
+  {
+    "revision": "525adf44414c8230136a",
+    "url": "css/library-albums.60e3d791.css"
+  },
+  {
+    "revision": "6ee5514d15ebb29a572e",
+    "url": "css/library-artists.60e3d791.css"
+  },
+  {
+    "revision": "0fa1a6e26cedf670e9c1",
+    "url": "css/library-playlists.60e3d791.css"
+  },
+  {
+    "revision": "da50e161b8198692a751",
+    "url": "css/library-radios.60e3d791.css"
+  },
+  {
+    "revision": "db99eae9375219e860f6",
+    "url": "css/library-tracks.60e3d791.css"
+  },
+  {
+    "revision": "692f78d53a08908127c0",
+    "url": "css/login.63b8eeaa.css"
+  },
+  {
+    "revision": "c8d9de5347e1a861e926",
+    "url": "css/main.48b95a98.css"
+  },
+  {
+    "revision": "8f2b37aa8d3cec7dc3f1",
+    "url": "css/playerqueue.ef4962ef.css"
+  },
+  {
+    "revision": "56fc525d9d1a0624bc5b",
+    "url": "css/playlistdetails.c59b7b78.css"
+  },
+  {
+    "revision": "ba22514c441ddc16933d",
+    "url": "css/search.7df20dad.css"
+  },
+  {
+    "revision": "9381a1f2358c80b7a019",
+    "url": "css/tasks.18bc4a4a.css"
+  },
+  {
+    "revision": "c72b64b65f5167150ad0",
+    "url": "css/trackdetails.67e70719.css"
+  },
+  {
+    "revision": "2a61b00fcc9b1afbf99be145777b1b72",
+    "url": "fonts/MaterialIcons-Regular.2a61b00f.eot"
+  },
+  {
+    "revision": "51cf1d641ae503ece727e7d84b182524",
+    "url": "fonts/MaterialIcons-Regular.51cf1d64.ttf"
+  },
+  {
+    "revision": "703cf8f274fbb265d49c6262825780e1",
+    "url": "fonts/MaterialIcons-Regular.703cf8f2.woff2"
+  },
+  {
+    "revision": "84a37de85c17f186652a179b1145392f",
+    "url": "fonts/MaterialIcons-Regular.84a37de8.woff"
+  },
+  {
+    "revision": "313a65630d341645c13e4f2a0364381d",
+    "url": "fonts/Roboto-Black.313a6563.woff"
+  },
+  {
+    "revision": "59eb3601394dd87f30f82433fb39dd94",
+    "url": "fonts/Roboto-Black.59eb3601.woff2"
+  },
+  {
+    "revision": "cc2fadc3928f2f223418887111947b40",
+    "url": "fonts/Roboto-BlackItalic.cc2fadc3.woff"
+  },
+  {
+    "revision": "f75569f8a5fab0893fa712d8c0d9c3fe",
+    "url": "fonts/Roboto-BlackItalic.f75569f8.woff2"
+  },
+  {
+    "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
+    "url": "fonts/Roboto-Bold.50d75e48.woff"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "fonts/Roboto-Bold.b52fac2b.woff2"
+  },
+  {
+    "revision": "4fe0f73cc919ba2b7a3c36e4540d725c",
+    "url": "fonts/Roboto-BoldItalic.4fe0f73c.woff"
+  },
+  {
+    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
+    "url": "fonts/Roboto-BoldItalic.94008e69.woff2"
+  },
+  {
+    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
+    "url": "fonts/Roboto-Light.c73eb1ce.woff"
+  },
+  {
+    "revision": "d26871e8149b5759f814fd3c7a4f784b",
+    "url": "fonts/Roboto-Light.d26871e8.woff2"
+  },
+  {
+    "revision": "13efe6cbc10b97144a28310ebdeda594",
+    "url": "fonts/Roboto-LightItalic.13efe6cb.woff"
+  },
+  {
+    "revision": "e8eaae902c3a4dacb9a5062667e10576",
+    "url": "fonts/Roboto-LightItalic.e8eaae90.woff2"
+  },
+  {
+    "revision": "1d6594826615607f6dc860bb49258acb",
+    "url": "fonts/Roboto-Medium.1d659482.woff"
+  },
+  {
+    "revision": "90d1676003d9c28c04994c18bfd8b558",
+    "url": "fonts/Roboto-Medium.90d16760.woff2"
+  },
+  {
+    "revision": "13ec0eb5bdb821ff4930237d7c9f943f",
+    "url": "fonts/Roboto-MediumItalic.13ec0eb5.woff2"
+  },
+  {
+    "revision": "83e114c316fcc3f23f524ec3e1c65984",
+    "url": "fonts/Roboto-MediumItalic.83e114c3.woff"
+  },
+  {
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "fonts/Roboto-Regular.35b07eb2.woff"
+  },
+  {
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "fonts/Roboto-Regular.73f0a88b.woff2"
+  },
+  {
+    "revision": "4357beb823a5f8d65c260f045d9e019a",
+    "url": "fonts/Roboto-RegularItalic.4357beb8.woff2"
+  },
+  {
+    "revision": "f5902d5ef961717ed263902fc429e6ae",
+    "url": "fonts/Roboto-RegularItalic.f5902d5e.woff"
+  },
+  {
+    "revision": "ad538a69b0e8615ed0419c4529344ffc",
+    "url": "fonts/Roboto-Thin.ad538a69.woff2"
+  },
+  {
+    "revision": "d3b47375afd904983d9be8d6e239a949",
+    "url": "fonts/Roboto-Thin.d3b47375.woff"
+  },
+  {
+    "revision": "5b4a33e176ff736a74f0ca2dd9e6b396",
+    "url": "fonts/Roboto-ThinItalic.5b4a33e1.woff2"
+  },
+  {
+    "revision": "8a96edbbcd9a6991d79371aed0b0288e",
+    "url": "fonts/Roboto-ThinItalic.8a96edbb.woff"
+  },
+  {
+    "revision": "2b72faddcfa7a9933fb2dec1f4c3342b",
+    "url": "img/background.jpg"
+  },
+  {
+    "revision": "4cbed97a68515b8a2063b2e2f663e158",
+    "url": "img/background2.jpg"
+  },
+  {
+    "revision": "7305b29c7526c212938516a0717e5ccd",
+    "url": "img/default_artist.7305b29c.png"
+  },
+  {
+    "revision": "eabcf7ae3898600793d98017531fb3e2",
+    "url": "img/hires.eabcf7ae.png"
+  },
+  {
+    "revision": "29fe3282407f51338f27ae7bc33d8513",
+    "url": "img/homeassistant.29fe3282.png"
+  },
+  {
+    "revision": "4c4e488018bcf5af49d08a8a1434dd9b",
+    "url": "img/http_streamer.4c4e4880.png"
+  },
+  {
+    "revision": "4db55f47d3079cd17637da3220b953e2",
+    "url": "img/info_gradient.4db55f47.jpg"
+  },
+  {
+    "revision": "c079bd979828319b9104eae3c39cc373",
+    "url": "img/logo.c079bd97.png"
+  },
+  {
+    "revision": "798ba28fac7cd93ad48b7bf28cce3f52",
+    "url": "img/web.798ba28f.png"
+  },
+  {
+    "revision": "b8ef7a44f719ad78ac44bf5fda659869",
+    "url": "index.html"
+  },
+  {
+    "revision": "30622df32130bafc5b1a",
+    "url": "js/albumdetails.f67d8935.js"
+  },
+  {
+    "revision": "d95b4ba79e1daf6688e1",
+    "url": "js/albumdetails~artistdetails~config~library-albums~library-artists~library-playlists~library-radios~li~3e652d87.ad56fb6f.js"
+  },
+  {
+    "revision": "4e58462a8fc33d7bc472",
+    "url": "js/albumdetails~artistdetails~config~library-albums~library-artists~library-playlists~library-radios~li~7ba91e65.f6425b5d.js"
+  },
+  {
+    "revision": "d8387878613393a50b19",
+    "url": "js/albumdetails~artistdetails~library-albums~library-artists~library-playlists~library-radios~library-t~e7a02bed.fe22302f.js"
+  },
+  {
+    "revision": "fac1a97cf9f51508d29a",
+    "url": "js/albumdetails~artistdetails~library-albums~library-artists~library-playlists~library-radios~library-t~f6c0cde4.4a174cb7.js"
+  },
+  {
+    "revision": "8f8071b032457e240c9a",
+    "url": "js/app.a13710b8.js"
+  },
+  {
+    "revision": "43b7d24965d6529f262e",
+    "url": "js/artistdetails.1a6a4682.js"
+  },
+  {
+    "revision": "738729a33752e0127c90",
+    "url": "js/chunk-vendors.4a0c8c84.js"
+  },
+  {
+    "revision": "9537a62a9ce9d286a94e",
+    "url": "js/config.a218283a.js"
+  },
+  {
+    "revision": "7e87e9d56f02d2e6ccf9",
+    "url": "js/config~main.b0ccfec2.js"
+  },
+  {
+    "revision": "7d2b0ab7ab0bd923a0c7",
+    "url": "js/home.03318710.js"
+  },
+  {
+    "revision": "525adf44414c8230136a",
+    "url": "js/library-albums.6317e7c0.js"
+  },
+  {
+    "revision": "6ee5514d15ebb29a572e",
+    "url": "js/library-artists.ca42b931.js"
+  },
+  {
+    "revision": "0fa1a6e26cedf670e9c1",
+    "url": "js/library-playlists.8749108e.js"
+  },
+  {
+    "revision": "da50e161b8198692a751",
+    "url": "js/library-radios.04b87c06.js"
+  },
+  {
+    "revision": "db99eae9375219e860f6",
+    "url": "js/library-tracks.573831ff.js"
+  },
+  {
+    "revision": "692f78d53a08908127c0",
+    "url": "js/login.8efee99d.js"
+  },
+  {
+    "revision": "c8d9de5347e1a861e926",
+    "url": "js/main.1b9abb9a.js"
+  },
+  {
+    "revision": "8f2b37aa8d3cec7dc3f1",
+    "url": "js/playerqueue.b87cff0c.js"
+  },
+  {
+    "revision": "56fc525d9d1a0624bc5b",
+    "url": "js/playlistdetails.a010160f.js"
+  },
+  {
+    "revision": "ba22514c441ddc16933d",
+    "url": "js/search.3a7b1f90.js"
+  },
+  {
+    "revision": "9381a1f2358c80b7a019",
+    "url": "js/tasks.13e73163.js"
+  },
+  {
+    "revision": "c72b64b65f5167150ad0",
+    "url": "js/trackdetails.3948fbb1.js"
+  },
+  {
+    "revision": "c93d318ec22138832730289be28caa77",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
