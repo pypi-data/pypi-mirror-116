4e43a51510051484c170880ee8bb7f0fc80d1cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:13 +0000

    Add package absolutlyuseless/1.0/absolutlyuseless-1.0.tar.gz

---
+++ packages/absolutlyuseless/absolutlyuseless-1.0.tar.gz/absolutlyuseless-1.0/README.md
@@ -0,0 +1,3 @@
+# Absolutly useless
+An absolutly useless package which your lonely or curious self decided the install
+[Github-flavored Markdown](https://srv.dhruvsingh.com/python-proj/absolutlyuseless)
---
+++ packages/absolutlyuseless/absolutlyuseless-1.0.tar.gz/absolutlyuseless-1.0/src/absolutlyuseless/main.py
@@ -0,0 +1,4 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("his package is absolutly useless. Donate to make an actual package at https://paypal.me/thinkerdesign")
+print("This package is absolutly useless. Donate to make an actual package at https://paypal.me/thinkerdesign")
