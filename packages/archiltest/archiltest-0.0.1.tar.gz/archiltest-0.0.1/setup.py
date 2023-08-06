dc8cadb499cd4cb22d7479b8118b5bc1fa5ea52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:02 +0000

    Add package archiltest/0.0.1/archiltest-0.0.1.tar.gz

---
+++ packages/archiltest/archiltest-0.0.1.tar.gz/archiltest-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='archiltest',
+version='0.0.1',
+    description='Simple Addition',
+    long_description=open('README.txt').read(),
+    include_package_data=True,
+    url='',
+    author='Archil Chachanidze',
+    author_email='archikochachanidze@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='calculator',
+    install_requires=['']
+)
