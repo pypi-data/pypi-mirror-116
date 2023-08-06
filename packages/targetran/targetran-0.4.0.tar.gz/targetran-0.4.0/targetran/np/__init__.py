bbbd1c76bad072afad03b066d704df942af29548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:01 +0000

    Add package targetran/0.4.0/targetran-0.4.0.tar.gz

---
+++ packages/targetran/targetran-0.4.0.tar.gz/targetran-0.4.0/targetran/np/__init__.py
@@ -0,0 +1,16 @@
+from ._np import (
+    flip_left_right,
+    flip_up_down,
+    rotate,
+    crop,
+    translate,
+    shear,
+    Resize,
+    RandomTransform,
+    RandomFlipLeftRight,
+    RandomFlipUpDown,
+    RandomRotate,
+    RandomCrop,
+    RandomTranslate,
+    RandomShear
+)
