aaebae668fbd99de3f51f16dbacc9375e0410ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:47 +0000

    Add package mpl-plotter/4.0.0/mpl_plotter-4.0.0.tar.gz

---
+++ packages/mpl-plotter/mpl_plotter-4.0.0.tar.gz/mpl_plotter-4.0.0/mpl_plotter/color/schemes.py
@@ -0,0 +1,19 @@
+def one():
+    custom = ["darkred",
+              "#1f8fff",
+              "#FF8F1F",
+              "#00C298",
+              "#FFBD00",
+              "#00FFC4",
+              "#FF003B"]
+    tableau = ['tab:blue',
+               'tab:orange',
+               'tab:green',
+               'tab:red',
+               'tab:purple',
+               'tab:brown',
+               'tab:pink',
+               'tab:gray',
+               'tab:olive',
+               'tab:cyan']
+    return custom + tableau
