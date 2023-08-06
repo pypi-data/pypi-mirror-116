0c0276ebe5c6df7be91172b8210c84fa35101a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:26 +0000

    Add package requests-base/0.1.0/requests-base-0.1.0.tar.gz

---
+++ packages/requests-base/requests-base-0.1.0.tar.gz/requests-base-0.1.0/requests_base/__init__.py
@@ -0,0 +1,4 @@
+"""
+A Requests Session with a base URL.
+"""
+from .requests_base import BaseUrlSession  # noqa
---
+++ packages/requests-base/requests-base-0.1.0.tar.gz/requests-base-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['requests_base']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.26.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'requests-base',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '# Requests Base\n\n[![GitHub Super-Linter](https://github.com/michaeltinsley/requests-base/workflows/Lint%20Code%20Base/badge.svg)](https://github.com/marketplace/actions/super-linter)\n[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)\n',
+    'author': 'Michael Tinsley',
+    'author_email': 'michaeltinsley@outlook.com',
+    'maintainer': 'Michael Tinsley',
+    'maintainer_email': 'michaeltinsley@outlook.com',
+    'url': 'https://github.com/michaeltinsley/requests-base/',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
