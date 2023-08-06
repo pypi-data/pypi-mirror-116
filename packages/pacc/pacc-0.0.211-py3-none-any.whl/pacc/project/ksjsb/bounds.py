8e6bc5a6e9c6f0afb411ed72140079443df06379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:37 +0000

    Add package pacc/0.0.211/pacc-0.0.211-py3-none-any.whl

---
+++ packages/pacc/pacc-0.0.211-py3-none-any.whl/pacc/project/ksjsb/bounds.py
@@ -0,0 +1,5 @@
+attendance = '[195,1299][885,1440]'  # 财富页面签到
+cashCoupons = '[648,291][-1,417]'  # 可用抵用金（张）
+goldCoins = '[33,294][-1,414]'  # 金币收益
+closeInviteFriendsToMakeMoney = '[483,1563][597,1674]'  # 关闭邀请好友赚更多
+closeCongratulations = '[78,405][174,501]'  # 关闭恭喜获得好友看视频奖励
