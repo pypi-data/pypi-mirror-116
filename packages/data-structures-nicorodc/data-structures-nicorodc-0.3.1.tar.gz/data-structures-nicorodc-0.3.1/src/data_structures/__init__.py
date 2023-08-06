904da6be668485fdda7f65e07fa73607f1358c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:22 +0000

    Add package data-structures-nicorodc/0.3.1/data-structures-nicorodc-0.3.1.tar.gz

---
+++ packages/data-structures-nicorodc/data-structures-nicorodc-0.3.1.tar.gz/data-structures-nicorodc-0.3.1/src/data_structures/__init__.py
@@ -0,0 +1,29 @@
+"""Some useful data structures for problem solving.
+
+Modules
+-------
+
+elements:
+    Basic elements to use in data structures.
+
+    Classes
+    -------
+    Number:
+        Immutable real numbers.
+
+stacks:
+    Stack-like data structures.
+
+    Classes
+    -------
+    Stack:
+        Basic stack data structure.
+
+queues:
+    Queue-like data structures.
+
+    Classes
+    -------
+    Queue:
+        Basic queue data structure.
+"""
