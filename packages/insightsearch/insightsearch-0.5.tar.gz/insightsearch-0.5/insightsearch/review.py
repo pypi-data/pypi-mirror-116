360d33ec916c4f7870297c71b19080fe44d0425c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:05 +0000

    Add package insightsearch/0.5/insightsearch-0.5.tar.gz

---
+++ packages/insightsearch/insightsearch-0.5.tar.gz/insightsearch-0.5/insightsearch/review.py
@@ -0,0 +1,20 @@
+from insightsearch.data_clean import *
+from insightsearch.dataframe_checking import *
+from pathlib import Path
+
+class Analysis(DataframeClean):
+    def __init__(
+        self, df: pd.DataFrame = None, column_name: str = None, date_column: str = None, bert: bool = False
+    ):
+        self.df = df
+        self.column_name = column_name
+        self.date_column = date_column
+        self.bert=bert
+        DFvalid.__init__(self, self.df, self.column_name, self.date_column,self.bert)
+        self._clean()
+    def review_analyze(self):
+        self.showing_aspect()
+        self.showing_sentiment()
+
+
+
