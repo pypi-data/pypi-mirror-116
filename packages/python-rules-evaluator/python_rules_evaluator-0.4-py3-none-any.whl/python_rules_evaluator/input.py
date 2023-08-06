150498741cd77a5a9dc8988d7df2788f87a81f3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:20 +0000

    Add package python-rules-evaluator/0.4/python_rules_evaluator-0.4-py3-none-any.whl

---
+++ packages/python-rules-evaluator/python_rules_evaluator-0.4-py3-none-any.whl/python_rules_evaluator/input.py
@@ -0,0 +1,311 @@
+DOC = {
+    "items": [
+      {
+          "_id": "P02746-1_15_43600074_T_C",
+          "_cls": "BaseVariant.SnvVariant",
+          "analysed": False,
+          "base_quality": 33.51,
+          "causative": False,
+          "change": {
+              "ref": {
+                  "base": "T",
+                  "allelic_depth": 809
+              },
+              "alt": {
+                  "base": "C",
+                  "allelic_depth": 727
+              }
+          },
+          "check_de_novo": False,
+          "chromosome": "15",
+          "created_ts": "2021-08-13T11:11:01.754Z",
+          "custom_report_table": "AUTO",
+          "de_novo": None,
+          "de_novo_information": {
+              "mother": {
+                  "checked": False,
+                  "parenthood_confirmed": False,
+                  "variant_absent": False
+              },
+              "father": {
+                  "checked": False,
+                  "parenthood_confirmed": False,
+                  "variant_absent": False
+              }
+          },
+          "frequency": 47.33072916666667,
+          "genes": [
+              {
+                  "gene": "STRC",
+                  "strand": "-",
+                  "pathways": [
+                      "."
+                  ],
+                  "hsir": False,
+                  "tier": 1,
+                  "phenotypes": {
+                      "custom": [
+                          {
+                              "phenotype": "Hipoacusia no sindrómica/DFNB16",
+                              "reference": ".",
+                              "inheritance_mode": "AR",
+                              "disease": ".",
+                              "candidate": False,
+                              "notes": ".",
+                              "separated_analysis": False,
+                              "phenotypic_consistency": "NOT_EVALUATED",
+                              "override_to_vus": False,
+                              "analyse": True,
+                              "calculated_analyse": True
+                          },
+                          {
+                              "phenotype": "Síndrome de sordera e infertilidad (deleción STRC+CATSPER)",
+                              "reference": ".",
+                              "inheritance_mode": "DR",
+                              "disease": ".",
+                              "candidate": True,
+                              "notes": ".",
+                              "separated_analysis": False,
+                              "phenotypic_consistency": "NOT_EVALUATED",
+                              "override_to_vus": False,
+                              "analyse": False,
+                              "calculated_analyse": False
+                          }
+                      ],
+                      "omim": [
+                          {
+                              "phenotype": "Spermatogenic failure 7",
+                              "reference": "612997,Deafness",
+                              "inheritance_mode": ".",
+                              "disease": ".",
+                              "candidate": False,
+                              "notes": ".",
+                              "separated_analysis": False,
+                              "phenotypic_consistency": "NOT_EVALUATED",
+                              "override_to_vus": False,
+                              "analyse": False,
+                              "calculated_analyse": False
+                          },
+                          {
+                              "phenotype": " autosomal recessive 16",
+                              "reference": "603720,Deafness, sensorineural",
+                              "inheritance_mode": ".",
+                              "disease": ".",
+                              "candidate": False,
+                              "notes": ".",
+                              "separated_analysis": False,
+                              "phenotypic_consistency": "NOT_EVALUATED",
+                              "override_to_vus": False,
+                              "analyse": False,
+                              "calculated_analyse": False
+                          },
+                          {
+                              "phenotype": " and male infertility",
+                              "reference": "611102",
+                              "inheritance_mode": ".",
+                              "disease": ".",
+                              "candidate": False,
+                              "notes": ".",
+                              "separated_analysis": False,
+                              "phenotypic_consistency": "NOT_EVALUATED",
+                              "override_to_vus": False,
+                              "analyse": False,
+                              "calculated_analyse": False
+                          },
+                          {
+                              "phenotype": "Deafness-infertility syndrome",
+                              "reference": "94064",
+                              "inheritance_mode": ".",
+                              "disease": ".",
+                              "candidate": False,
+                              "notes": ".",
+                              "separated_analysis": False,
+                              "phenotypic_consistency": "NOT_EVALUATED",
+                              "override_to_vus": False,
+                              "analyse": False,
+                              "calculated_analyse": False
+                          }
+                      ],
+                      "orpha": []
+                  },
+                  "annotations": [
+                      {
+                          "_cls": "SnvAnnotation",
+                          "id": "6085d4c8-d04f-4907-a712-1111893efd8b",
+                          "rank": 1,
+                          "num_P": 29,
+                          "num_LP": 11,
+                          "impact": "missense_variant",
+                          "variant_sub_type": "SNV",
+                          "selected": True,
+                          "ensembl_id": "ENST00000450892.7",
+                          "ref_seq_id": "NM_153700.2",
+                          "dna_change": "c.5125A>G",
+                          "protein_change": "p.Thr1709Ala",
+                          "codon": 1709,
+                          "revel_score": 0.637,
+                          "rf_score": None,
+                          "ada_score": None,
+                          "affected_exon": "28/29",
+                          "max_ent_scan": {
+                              "ref_score": None,
+                              "alt_score": None,
+                              "diff_score": None
+                          },
+                          "protein_domains": [
+                              {
+                                  "source": "PANTHER",
+                                  "id": "PTHR23412"
+                              }
+                          ],
+                          "protein_predictions": [
+                              "."
+                          ],
+                          "clinical_significances": [
+                              "LIKELY_PATHOGENIC"
+                          ],
+                          "variant_ids": [
+                              "."
+                          ],
+                          "maf": {
+                              "values": [
+                                  {
+                                      "population": "ALL",
+                                      "frequency": 0.000178496,
+                                      "size": 151264
+                                  },
+                                  {
+                                      "population": "AMI",
+                                      "frequency": 0,
+                                      "size": 912
+                                  },
+                                  {
+                                      "population": "OTH",
+                                      "frequency": 0,
+                                      "size": 2078
+                                  },
+                                  {
+                                      "population": "AFR",
+                                      "frequency": 0.000170524,
+                                      "size": 41050
+                                  },
+                                  {
+                                      "population": "SAS",
+                                      "frequency": 0.00126156,
+                                      "size": 4756
+                                  },
+                                  {
+                                      "population": "ASJ",
+                                      "frequency": 0,
+                                      "size": 3462
+                                  },
+                                  {
+                                      "population": "FIN",
+                                      "frequency": 0,
+                                      "size": 10548
+                                  },
+                                  {
+                                      "population": "AMR",
+                                      "frequency": 0.00013151,
+                                      "size": 15208
+                                  },
+                                  {
+                                      "population": "NFE",
+                                      "frequency": 0.0000589883,
+                                      "size": 67810
+                                  },
+                                  {
+                                      "population": "EAS",
+                                      "frequency": 0.00136612,
+                                      "size": 5124
+                                  },
+                                  {
+                                      "population": "MID",
+                                      "frequency": 0.00316456,
+                                      "size": 316
+                                  },
+                                  {
+                                      "population": "NON_V2",
+                                      "frequency": 0.000149238,
+                                      "size": 113912
+                                  },
+                                  {
+                                      "population": "NON_NEURO",
+                                      "frequency": 0.000171639,
+                                      "size": 134002
+                                  },
+                                  {
+                                      "population": "NON_TOPMED",
+                                      "frequency": 0.000287234,
+                                      "size": 80074
+                                  },
+                                  {
+                                      "population": "NON_CANCER",
+                                      "frequency": 0.000183656,
+                                      "size": 147014
+                                  },
+                                  {
+                                      "population": "CONTROLS",
+                                      "frequency": 0.000398749,
+                                      "size": 32602
+                                  }
+                              ],
+                              "max": 0.000178496,
+                              "max_subpop": 0.00136612
+                          },
+                          "splicing_type": ".",
+                          "repetitive_region_info": ".",
+                          "hotspot_region_info": "hotspot_region",
+                          "autopvs1_strength": "."
+                      }
+                  ],
+                  "mitochondrial": False
+              }
+          ],
+          "genotype": "0/1",
+          "hgmd": {
+              "variant_class": "Absent",
+              "phenotype": "",
+              "date": "2021-08-13T00:00:00.000Z"
+          },
+          "last_modify_user": "importer",
+          "mapping_quality": 60,
+          "modified_ts": "2021-08-13T11:13:46.810Z",
+          "patient_info": {
+              "patient_id": "IM20-0225",
+              "gender": "M",
+              "segregation": None
+          },
+          "position": 43600074,
+          "probe_distance": 0,
+          "read_depth": 1536,
+          "report_table": "NONE",
+          "test_id": "P02746-1",
+          "validation_required": False,
+          "variant_type": "SNV",
+          "zygosity": "HETEROZYGOUS",
+          "analysed_ts": None,
+          "in_cis_variant_references": [],
+          "in_cis_variants": None,
+          "in_trans_variant_references": [],
+          "in_trans_variants": None,
+          "pdf_fraction_primary": None,
+          "potential_pseudogene": None,
+          "validation_information": None,
+          "variant_phenotype": None,
+          "flags": {
+              "RecoveredVariant": False,
+              "Type1": True,
+              "Type2": False,
+              "Type3": False,
+              "Type4": False,
+              "Type5": False,
+              "MAFSubpopMaxExceeded": False
+          }
+      }
+    ],
+    "count": 1,
+    "ids": [
+        "P02746-1_15_43600074_T_C"
+    ]
+}
