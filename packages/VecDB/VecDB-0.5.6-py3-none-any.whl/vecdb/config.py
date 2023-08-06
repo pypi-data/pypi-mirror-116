4fa1b5f789131cee92368794b08a6a5b1e7b80b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:11 +0000

    Add package VecDB/0.5.6/VecDB-0.5.6-py3-none-any.whl

---
+++ packages/VecDB/VecDB-0.5.6-py3-none-any.whl/vecdb/config.py
@@ -0,0 +1,24 @@
+import logging
+
+class BaseConfig:
+    def __getitem__(self, item):
+        return getattr(self, item)
+    
+    def __setitem__(self, item, value):
+        setattr(self, item, value)
+
+class TransportConfig(BaseConfig):
+    number_of_retries: int = 3
+    seconds_between_retries: int = 2
+
+    #Set Logging Rules
+    log: bool = False
+    log_to_file: bool = True
+    log_to_console: bool = True
+    logging_level: int = logging.INFO
+
+class Config(TransportConfig):
+    """All the configs - which are to be inhertied
+    """
+
+CONFIG = Config()
