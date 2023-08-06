2cd323ddd6318a7982f58d3b8b1c144e3de0f642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:00 +0000

    Add package drf-chunked-upload/0.5.1/drf-chunked-upload-0.5.1.tar.gz

---
+++ packages/drf-chunked-upload/drf-chunked-upload-0.5.1.tar.gz/drf-chunked-upload-0.5.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(ROOT, 'README.md'), encoding='utf-8') as f:
+    readme = f.read()
+
+version = os.environ.get('DCU_VERSION', '0.0.0')
+
+
+setup(
+    name='drf-chunked-upload',
+    packages=find_packages(exclude=['tests*']),
+    version=version,
+    description=('Upload large files to Django REST Framework in multiple chunks,' +
+                 ' with the ability to resume if the upload is interrupted.'),
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Jarrett Keifer',
+    author_email='jkeifer0@gmail.com',
+    url='https://github.com/jkeifer/drf-chunked-upload',
+    install_requires=[
+        'Django>=2.2',
+        'djangorestframework>=3.11',
+    ],
+    license='MIT-Zero',
+)
