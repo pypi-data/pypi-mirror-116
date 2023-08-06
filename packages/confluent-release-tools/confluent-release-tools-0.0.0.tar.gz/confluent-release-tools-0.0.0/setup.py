50652220c372a6e7b366cdff8166dfc1b0b78413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:06 +0000

    Add package confluent-release-tools/0.0.0/confluent-release-tools-0.0.0.tar.gz

---
+++ packages/confluent-release-tools/confluent-release-tools-0.0.0.tar.gz/confluent-release-tools-0.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="confluent-release-tools",
+      version="0.0.0",
+      author="John Doe",
+      author_email="jd@email.com",
+      packages=["package_name"]
+)
