9d99ff6b04125a1cc8e1810247646d673253d63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:03 +0000

    Add package ailever/0.2.617/ailever-0.2.617-py3-none-any.whl

---
+++ packages/ailever/ailever-0.2.617-py3-none-any.whl/ailever/investment/__init__.py
@@ -0,0 +1,31 @@
+from ._fmlops_policy import local_initialization_policy
+
+local_environment = dict()
+local_environment['root'] = '.fmlops'
+local_environment['feature_store'] = 'feature_store'
+local_environment['source_repository'] = 'source_repository'
+local_environment['model_registry'] = 'model_registry'
+local_environment['monitoring_source'] = 'monitoring_source'
+local_environment['analysis_report_repository'] = 'analysis_report_repository'
+local_environment['investment_outcome_repository'] = 'investment_outcome_repository'
+local_environment['metadata_store'] = 'metadata_store'
+
+local_environment['forecasting_model_registry'] = 'forecasting_model_registry'
+local_environment['strategy_model_registry'] = 'strategy_model_registry'
+local_environment['data_management'] = 'model_specification'
+local_environment['model_management'] = 'model_specification'
+local_environment['model_specification'] = 'model_specification'
+__fmlops_bs__ = local_initialization_policy(local_environment)
+
+from .INVESTMENTInterfaces import initialize
+from .INVESTMENTInterfaces import parallelize
+from .INVESTMENTInterfaces import portfolio_optimize
+from .INVESTMENTInterfaces import Forecaster
+from .INVESTMENTInterfaces import sectors
+from .INVESTMENTInterfaces import Loader
+from .INVESTMENTInterfaces import Preprocessor
+from .INVESTMENTInterfaces import screener
+
+
+
+
