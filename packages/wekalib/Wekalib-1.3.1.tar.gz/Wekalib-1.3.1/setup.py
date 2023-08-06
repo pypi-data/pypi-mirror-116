a2b227264be6ac81e1287cab4b2ad1b56d7ad2e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:11 +0000

    Add package wekalib/1.3.1/Wekalib-1.3.1.tar.gz

---
+++ packages/wekalib/Wekalib-1.3.1.tar.gz/Wekalib-1.3.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Wekalib',
+    version='1.3.1',
+
+    packages=['wekalib'],
+    url='https://github.com/weka/wekalib',
+    license='GNU',
+    author='Vincent Fleming',
+    author_email='vince@weka.io',
+    description='Weka API library'
+)
