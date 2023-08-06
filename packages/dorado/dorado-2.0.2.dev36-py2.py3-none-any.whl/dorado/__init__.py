8a86c3338d3de35268e7481c836f1ae6e9d72c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:14 +0000

    Add package dorado/2.0.2.dev36/dorado-2.0.2.dev36-py2.py3-none-any.whl

---
+++ packages/dorado/dorado-2.0.2.dev36-py2.py3-none-any.whl/dorado/__init__.py
@@ -0,0 +1,26 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+# Packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *   # noqa
+
+from .target import *
+from .ceres import *
+from .stack import *
+from .filer import *
+# from .fournax import *
+from .timeseries import *
+# ----------------------------------------------------------------------------
+
+# __all__ = []
+# from .config import *
+# from .example_mod import *   # noqa
+# Then you can be explicit to control what ends up in the namespace,
+# __all__ += ['do_primes']   # noqa
+# or you can keep everything from the subpackage with the following instead
+# __all__ += example_mod.__all__
+
+
+# .. warning::
+# :mod:`word`
