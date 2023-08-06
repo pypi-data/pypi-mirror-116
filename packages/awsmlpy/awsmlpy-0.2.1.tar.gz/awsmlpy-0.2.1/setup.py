ecd8a1c86cbabd7e47a9aa60e56b7c01035f77a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:09 +0000

    Add package awsmlpy/0.2.1/awsmlpy-0.2.1.tar.gz

---
+++ packages/awsmlpy/awsmlpy-0.2.1.tar.gz/awsmlpy-0.2.1/setup.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+setup(
+    name='awsmlpy',
+    version='0.2.1',
+    description='Just wanna make sagemaker easy to use',
+    licenes='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Environment :: Web Environment",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    install_requires=[
+            'sagemaker==2.45.0',
+    ],
+    zip_safe=True,
+)
+
