1da5a6bdb1b85acd824e2413022b9b21b6885142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:52 +0000

    Add package deeptables/0.2.3/deeptables-0.2.3-py3-none-any.whl

---
+++ packages/deeptables/deeptables-0.2.3-py3-none-any.whl/deeptables/utils/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+__author__ = 'yangjian'
+"""
+
+"""
+from hypernets.utils import fs, hash_data, infer_task_type, load_data, isnotebook
+from hypernets.tabular.metrics import calc_score
