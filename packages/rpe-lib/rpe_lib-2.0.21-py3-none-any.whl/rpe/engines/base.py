8de2a6397d9cb9eaa0fd7be6eda1037d3eb73505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:32 +0000

    Add package rpe-lib/2.0.21/rpe_lib-2.0.21-py3-none-any.whl

---
+++ packages/rpe-lib/rpe_lib-2.0.21-py3-none-any.whl/rpe/engines/base.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+
+# Base class for policy engines
+class Engine(ABC):
+
+    @abstractmethod
+    def policies(self):
+        pass
+
+    @abstractmethod
+    def evaluate(self, resource):
+        pass
+
+    @abstractmethod
+    def remediate(self, resource, policy_id):
+        pass
