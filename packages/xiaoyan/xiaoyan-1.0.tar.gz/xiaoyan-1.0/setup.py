f4b87c4ef7e3bcc675c1cd39c9df19c10763412f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:50 +0000

    Add package xiaoyan/1.0/xiaoyan-1.0.tar.gz

---
+++ packages/xiaoyan/xiaoyan-1.0.tar.gz/xiaoyan-1.0/setup.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+from distutils.core import setup
+setup(
+    name='xiaoyan',
+    version='1.0',
+    description='这是第一个对外发布的模块，测试哦',
+    author='01-xiao.yan_01',
+    author_email='1599329179@qq.com',
+    py_modules=['xiaoyan.demo01','xiaoyan.demo02']
+)
