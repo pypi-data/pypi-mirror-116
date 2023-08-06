abc57e928f6a57bff1f86452994f9b67392ec3dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:16 +0000

    Add package equilibrator-cheminfo/0.7.0/equilibrator_cheminfo-0.7.0-py3-none-any.whl

---
+++ packages/equilibrator-cheminfo/equilibrator_cheminfo-0.7.0-py3-none-any.whl/equilibrator_cheminfo/application/service/__init__.py
@@ -0,0 +1,5 @@
+from .create_molecular_entities_from_table_command import (
+    CheminformaticsBackend,
+    CreateMolecularEntitiesFromTableCommand,
+)
+from .structures_table_service import StructuresTableService
