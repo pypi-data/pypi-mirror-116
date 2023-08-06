23d19509cc540fa70c9359105d1ad168a427d9bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:38 +0000

    Add package cbdevtools/0.0.1b0/cbdevtools-0.0.1b0.tar.gz

---
+++ packages/cbdevtools/cbdevtools-0.0.1b0.tar.gz/cbdevtools-0.0.1b0/cbdevtools/addsrc.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+###
+# ????
+###
+
+
+from pathlib import Path
+import sys
+
+
+###
+# prototype::
+#     file    = ; // See Python typing...
+#               ???
+#     project = ; // See Python typing...
+#               ???
+###
+
+def addsrc(
+    file   : str,
+    project: str,
+) -> None:
+    project_dir = Path(file).parent
+
+    if not project in str(project_dir):
+        raise Exception(
+            "call the script from a working directory containing the project."
+        )
+
+    while(not project_dir.name.startswith(project)):
+        project_dir = project_dir.parent
+
+    sys.path.append(str(project_dir))
---
+++ packages/cbdevtools/cbdevtools-0.0.1b0.tar.gz/cbdevtools-0.0.1b0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cbdevtools']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'cbdevtools',
+    'version': '0.0.1b0',
+    'description': '???',
+    'long_description': 'The `Python` module `cbdevtools`\n================================\n\n\n> **I beg your pardon for my english...**\n>\n> English is not my native language, so be nice if you notice misunderstandings, misspellings, or grammatical errors in my documents and codes.\n\n\nAbout `cbdevtools`\n-----------------\n\nThis project is a "Common Box of Dev Tools". It proposes small scripts taht can be helpful, at least to the author of this package. :-) \n\n\n????\n----\n\n\n\n<!-- :tutorial-START: -->\n<!-- :tutorial-END: -->\n\n\n<!-- :version-START: -->\n<!-- :version-END: -->\n',
+    'author': 'Christophe BAL',
+    'author_email': None,
+    'maintainer': 'Christophe BAL',
+    'maintainer_email': None,
+    'url': 'https://github.com/projetmbc/tools-for-dev/tree/master/cbdevtools',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
