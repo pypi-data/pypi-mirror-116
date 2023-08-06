962249ea9852b179659f3513a69909c3386acc71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:17 +0000

    Add package sharestats-item-editor/0.2.4/sharestats_item_editor-0.2.4-py3-none-any.whl

---
+++ packages/sharestats-item-editor/sharestats_item_editor-0.2.4-py3-none-any.whl/sharestats_item_editor/rexam_item_editor/__main__.py
@@ -0,0 +1,18 @@
+from . import __version__, APPNAME
+from .cli import cli
+from .gui.mainwin import MainWin
+
+def run():
+
+    opt = cli("{} {}".format(APPNAME, __version__))
+    if opt["monolingual"]:
+        languages = None
+    else:
+        languages = ("Dutch", "English")
+
+    MainWin(reset_settings=opt["reset"],
+            two_languages=languages).run()
+
+
+if __name__ == "__main__":
+    run()
