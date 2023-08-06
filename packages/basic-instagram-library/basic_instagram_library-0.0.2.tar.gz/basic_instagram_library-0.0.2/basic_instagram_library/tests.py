5c518945e89deb4c4c29a9703a2be67355aa4f8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:43 +0000

    Add package basic-instagram-library/0.0.2/basic_instagram_library-0.0.2.tar.gz

---
+++ packages/basic-instagram-library/basic_instagram_library-0.0.2.tar.gz/basic_instagram_library-0.0.2/basic_instagram_library/tests.py
@@ -0,0 +1,35 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import unittest
+
+from . import find_words
+
+
+class FindTags(unittest.TestCase):
+    def assertEmpty(self, obj):
+        self.assertFalse(obj)
+
+    def assertNotEmpty(self, obj):
+        self.assertTrue(obj)
+
+    def test_find_some_words(self):
+        words = find_words("hkarau")
+        self.assertNotEmpty(words)
+        self.assertTrue(len(words) > 4)
+
+    def test_find_no_tags(self):
+        self.assertRaises(Exception, find_words, "hkarau_dne_22")
