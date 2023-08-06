36904710832f5abbb1d2e33f83596d926ef56a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:27 +0000

    Add package jjyyzz-distributions/1.0/jjyyzz_distributions-1.0.tar.gz

---
+++ packages/jjyyzz-distributions/jjyyzz_distributions-1.0.tar.gz/jjyyzz_distributions-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='jjyyzz_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['jjjyyzz_distributions'],
+      author = 'Jiuyue Zhou',
+      zip_safe=False)
