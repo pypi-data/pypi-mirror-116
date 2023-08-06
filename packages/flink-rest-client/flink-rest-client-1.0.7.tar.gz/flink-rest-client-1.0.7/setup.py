13e7125ae04317bb1a00ac4b653b7f6bcc39520f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:28 +0000

    Add package flink-rest-client/1.0.7/flink-rest-client-1.0.7.tar.gz

---
+++ packages/flink-rest-client/flink-rest-client-1.0.7.tar.gz/flink-rest-client-1.0.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='flink-rest-client',
+    version='1.0.7',
+    description='Easy to use Flink REST API client implementation',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/frego-dev/flink-rest-client',
+    author='Attila Nagy',
+    author_email='anagy@frego.dev',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['requests', 'importlib_resources'],
+    entry_points={
+          'console_scripts': [],
+    }
+)
