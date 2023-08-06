42dd6025d6da4f5bef9a04bc1523f3d70fe939e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:51 +0000

    Add package data-structures-nicorodc/0.2.3/data-structures-nicorodc-0.2.3.tar.gz

---
+++ packages/data-structures-nicorodc/data-structures-nicorodc-0.2.3.tar.gz/data-structures-nicorodc-0.2.3/src/tests/__init__.py
@@ -0,0 +1,15 @@
+"""Tests for the data-structures package.
+
+Modules
+-------
+
+    test_elements:
+        Classes
+        -------
+            TestNumber: Tests for the Number class.
+
+    test_stacks:
+        Classes
+        -------
+            TestStack: Tests for the Stack class.
+"""
