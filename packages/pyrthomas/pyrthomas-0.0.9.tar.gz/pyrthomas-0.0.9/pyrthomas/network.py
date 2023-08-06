c66b38947c303876163be8207079e1c7f620116a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:04 +0000

    Add package pyrthomas/0.0.9/pyrthomas-0.0.9.tar.gz

---
+++ packages/pyrthomas/pyrthomas-0.0.9.tar.gz/pyrthomas-0.0.9/pyrthomas/network.py
@@ -0,0 +1,43 @@
+import networkx as nx
+
+from pyrthomas.cytoscape import read_sif, write_sif
+
+
+class NetworkService:
+    network: nx.DiGraph
+
+    def __init__(self) -> None:
+        self.network = nx.DiGraph()
+
+    def add_node(self, node):
+        self.network.add_node(node.id, min=node.min, max=node.max)
+
+    def add_edge(self, edge):
+        self.network.add_edge(edge.source, edge.target, weight=edge.weight)
+
+    def get_nodes(self):
+        return list(self.network.nodes)
+
+    def clear(self):
+        self.network.clear()
+
+    def get_edges(self):
+        return list(self.network.edges)
+
+    def import_graphml(self, file):
+        self.network = nx.read_graphml(file)
+
+    def import_dot(self, file):
+        self.network = nx.drawing.nx_pydot.read_dot(file)
+
+    def import_sif(self, file):
+        self.network = read_sif(file)
+
+    def export_graphml(self, file):
+        nx.write_graphml(self.network, file, prettyprint=True)
+
+    def export_dot(self, file):
+        nx.drawing.nx_pydot.write_dot(self.network, file)
+
+    def export_sif(self, file):
+        write_sif(self.network, file)
---
+++ packages/pyrthomas/pyrthomas-0.0.9.tar.gz/pyrthomas-0.0.9/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyrthomas']
+
+package_data = \
+{'': ['*'], 'pyrthomas': ['UNKNOWN.egg-info/*']}
+
+install_requires = \
+['networkx>=2.0']
+
+setup_kwargs = {
+    'name': 'pyrthomas',
+    'version': '0.0.9',
+    'description': "Python implementation for René Thomas's Framework",
+    'long_description': "Python implementation for Rene` Thomas's Framework\n\nFor installation execute\n\n`pip install pyrthomas`\n\nTo learn how to use this package please check examples directory.\n\n",
+    'author': 'Talha Junaid',
+    'author_email': 'talhajunaidd@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/talhajunaidd/pyrthomas',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
