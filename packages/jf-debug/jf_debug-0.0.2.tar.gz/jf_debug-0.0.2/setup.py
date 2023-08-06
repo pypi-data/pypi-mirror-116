0c7bdfb907ba01499729a3c670ca7f822a8ec28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:47 +0000

    Add package jf-debug/0.0.2/jf_debug-0.0.2.tar.gz

---
+++ packages/jf-debug/jf_debug-0.0.2.tar.gz/jf_debug-0.0.2/setup.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+import setuptools
+
+
+setuptools.setup(
+    name="jf_debug",
+    version="0.0.2",
+    author="DePanda", 
+    author_email="957454525@qq.com",
+    description="debug过程中辅助修饰方法",
+    long_description=open('README.rst').read(),
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/ImDePanDa/pyrelease",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License", #License
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=2.7", 
+)
