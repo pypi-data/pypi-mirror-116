c10e4538c4ffdeff204028165e9740e83b31225e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:12 +0000

    Add package rainbowchar/1.0/rainbowchar-1.0.tar.gz

---
+++ packages/rainbowchar/rainbowchar-1.0.tar.gz/rainbowchar-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='rainbowchar',
+    packages=['rainbowchar'],
+    version='1.0',
+    description='RainbowChar is a lib that stylizes text by formatting in ANSI escape code. It only supports true color terminals.',
+    author='Gabriel da Rosa Silveira',
+    author_email='gabrielsilveirapro@gmail.com',
+    url='https://github.com/roderiano/rainbow-char-lib',
+    download_url='https://github.com/roderiano/rainbow-char-lib/archive/refs/tags/v1.0.tar.gz',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner==4.4'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
