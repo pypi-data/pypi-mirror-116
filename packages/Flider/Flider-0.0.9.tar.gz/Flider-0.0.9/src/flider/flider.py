bb1fb09333b69cfe610ec3bb85ce2919140c2e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:18 +0000

    Add package Flider/0.0.9/Flider-0.0.9.tar.gz

---
+++ packages/Flider/Flider-0.0.9.tar.gz/Flider-0.0.9/src/flider/flider.py
@@ -0,0 +1,30 @@
+import os
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    # Try backported to PY<37 `importlib_resources`.
+    import importlib_resources as pkg_resources
+from . import read_files
+
+def Flider():
+    try:
+        os.mkdir('flask_app')
+        dirs = ['templates', 'config', 'models', 'controllers', 
+        'static', 'static/css', 'static/js', 'static/img']
+        for dir in dirs:
+            os.mkdir('flask_app/'+dir)
+    except Exception as e:
+        print(e)
+        print('Continuing...')
+    save_paths = ['flask_app/templates/', 'flask_app/config', 'flask_app/', 'flask_app/static/css/', 'flask_app/static/js/', '']
+    file_names = ['index.html', 'mysqlcontroller.py', '__init__.py', 'style.css', 'scripts.js', 'server.py']
+    read_files = ['index', 'mysqlcontroller', '__init__', 'style', 'scripts', 'server']
+    for i in range(len(save_paths)):
+        try:
+            completeName = os.path.join(save_paths[i], file_names[i])
+            write_file = open(completeName, 'w')
+            write_file.write(pkg_resources.read_text(read_files, read_files[i]))
+            write_file.close()
+        except Exception as e:
+            print(e)
+            print('Error on: ' + save_paths[i] + file_names[i])
