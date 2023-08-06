ed6b396fac103dc6282139e48502db52c0526c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:47 +0000

    Add package asyncosu/0.0.1/AsyncOsu-0.0.1.tar.gz

---
+++ packages/asyncosu/AsyncOsu-0.0.1.tar.gz/AsyncOsu-0.0.1/README.md
@@ -0,0 +1,36 @@
+<div align="center">
+
+# AsyncOsu
+
+</div>
+
+Asynchronous osu! api wrapper for python using aiohttp. 
+
+## OsuApiV2
+This package supports [osu api v2.](https://osu.ppy.sh/docs/index.html)
+
+Basic usage with context manager:
+
+```python
+import asyncio
+from AsyncOsu import OsuApiV2
+
+
+async def main():
+    async with OsuApiV2(client_id='client_id', client_secret='client_secret') as osu_api:
+        return await osu_api.get_user_from_username(f'heyronii')
+
+
+if __name__ == "__main__":
+    print(asyncio.run(main()))
+```
+
+without context_manager:
+
+```python
+async def main():
+    osu_api = OsuApiV2(client_id='client_id', client_secret='client_secret')
+    user_info = await osu_api.get_user_from_username(f'heyronii')
+    await osu_api.close()  # Close the connection before creating another instance
+    return user_info
+```
---
+++ packages/asyncosu/AsyncOsu-0.0.1.tar.gz/AsyncOsu-0.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+requirements = ['aiohttp>=3.7.4,<4.0.0']
+
+setuptools.setup(
+    name="AsyncOsu",
+    version="0.0.1",
+    author="Efehan Atıcı",
+    author_email="efehanatici@gmail.com",
+    description="Async osu! Api Wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aticie/async_osu_api",
+    project_urls={
+        "Bug Tracker": "https://github.com/aticie/async_osu_api/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=requirements,
+    python_requires=">=3.6",
+)
