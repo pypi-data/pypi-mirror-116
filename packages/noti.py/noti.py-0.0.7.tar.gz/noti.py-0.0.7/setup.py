82850a8f194646039574861b1a6e9b432c455d63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:01 +0000

    Add package noti.py/0.0.7/noti.py-0.0.7.tar.gz

---
+++ packages/noti.py/noti.py-0.0.7.tar.gz/noti.py-0.0.7/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['noti_py', 'noti_py.config']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=5.4.1,<6.0.0', 'click>=8.0.1,<9.0.0', 'requests>=2.26.0,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['notipy = noti_py.noti:main']}
+
+setup_kwargs = {
+    'name': 'noti.py',
+    'version': '0.0.7',
+    'description': 'Python-based notification script to post a message to a materix server via the Matrix client/server API',
+    'long_description': None,
+    'author': 'Alex Kelly',
+    'author_email': 'kellya@arachnitech.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
