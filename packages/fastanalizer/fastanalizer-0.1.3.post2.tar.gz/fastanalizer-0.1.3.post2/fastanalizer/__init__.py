08fe2ee0d544f2c17f14e69a108b7ee3d3b838f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:21 +0000

    Add package fastanalizer/0.1.3.post2/fastanalizer-0.1.3.post2.tar.gz

---
+++ packages/fastanalizer/fastanalizer-0.1.3.post2.tar.gz/fastanalizer-0.1.3.post2/fastanalizer/__init__.py
@@ -0,0 +1,7 @@
+from fastanalizer import pyfasta
+from fastanalizer import geral
+from fastanalizer import domainsearch
+from fastanalizer import pdomain
+from fastanalizer import align
+from fastanalizer import tree
+from fastanalizer.fastanalizer import main
