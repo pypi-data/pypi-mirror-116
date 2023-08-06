dc2a7bf206c7d08c5ada692e37fb991444027fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:11 +0000

    Add package otter-grader/2.2.6/otter_grader-2.2.6-py3-none-any.whl

---
+++ packages/otter-grader/otter_grader-2.2.6-py3-none-any.whl/otter/run/run_autograder/constants.py
@@ -0,0 +1,32 @@
+"""
+Default configurations for autograding
+"""
+
+DEFAULT_OPTIONS = {
+    "score_threshold": None,
+    "points_possible": None,
+    "show_stdout": False,
+    "show_hidden": False,
+    "show_all_public": False,
+    "seed": None,
+    "grade_from_log": False,
+    "serialized_variables": {},
+    "public_multiplier": 0,
+    "pdf": False,
+    "token": None,
+    "course_id": 'None',
+    "assignment_id": 'None',
+    "filtering": False,
+    "pagebreaks": False,
+    "debug": False,
+    "autograder_dir": '/autograder',
+    "lang": 'python',
+    "miniconda_path": '/root/miniconda3',
+    "test_visibility": 'hidden',
+    "separate_tests": True,
+    "plugins": [],
+    "logo": True,
+    "print_summary": True,
+    "print_score": True,
+    "zips": False,
+}
