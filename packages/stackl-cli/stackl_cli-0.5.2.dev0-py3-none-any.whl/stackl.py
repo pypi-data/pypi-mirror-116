ffd3aeb1fde2bf0378165e249bf6ce28aa17d127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:38 +0000

    Add package stackl-cli/0.5.2.dev0/stackl_cli-0.5.2.dev0-py3-none-any.whl

---
+++ packages/stackl-cli/stackl_cli-0.5.2.dev0-py3-none-any.whl/stackl.py
@@ -0,0 +1,32 @@
+import click
+
+from commands.apply import apply
+from commands.connect import connect
+from commands.create import create
+from commands.delete import delete
+from commands.edit import edit
+from commands.get import get
+from commands.info import info
+from commands.restore import restore
+from commands.update import update
+from commands.generate import generate
+
+CLICK_CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+@click.version_option()
+@click.group(context_settings=CLICK_CONTEXT_SETTINGS)
+def cli():
+    pass
+
+
+cli.add_command(connect)
+cli.add_command(get)
+cli.add_command(info)
+cli.add_command(apply)
+cli.add_command(edit)
+cli.add_command(create)
+cli.add_command(update)
+cli.add_command(delete)
+cli.add_command(restore)
+cli.add_command(generate)
