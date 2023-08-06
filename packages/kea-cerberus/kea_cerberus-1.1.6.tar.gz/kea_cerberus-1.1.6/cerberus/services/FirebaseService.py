c441aae6bae97ab6db80c9be71c66a19f93b6fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:07 +0000

    Add package kea-cerberus/1.1.6/kea_cerberus-1.1.6.tar.gz

---
+++ packages/kea-cerberus/kea_cerberus-1.1.6.tar.gz/kea_cerberus-1.1.6/cerberus/services/FirebaseService.py
@@ -0,0 +1,18 @@
+import firebase_admin
+from firebase_admin import auth, credentials
+from cerberus.exceptions.exceptions import InvalidUserCredentialsException, NotFoundUserException
+
+class FirebaseService():
+
+    def __init__(self,firebaseCredential):
+        cred = credentials.Certificate(firebaseCredential)
+        if (not len(firebase_admin._apps)):
+            default_app = firebase_admin.initialize_app(cred)
+
+    def getDecoded_token(self, tokenId):
+        try:
+            decoded_token = auth.verify_id_token(tokenId)
+        except Exception as error:
+             raise InvalidUserCredentialsException()
+
+        return decoded_token
