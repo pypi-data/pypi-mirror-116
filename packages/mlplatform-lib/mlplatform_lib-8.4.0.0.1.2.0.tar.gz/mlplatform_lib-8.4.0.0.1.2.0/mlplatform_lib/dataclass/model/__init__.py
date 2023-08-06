779fd13556ea78409b7a4b84950d9a97e47e803d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:43 +0000

    Add package mlplatform-lib/8.4.0.0.1.2.0/mlplatform_lib-8.4.0.0.1.2.0.tar.gz

---
+++ packages/mlplatform-lib/mlplatform_lib-8.4.0.0.1.2.0.tar.gz/mlplatform_lib-8.4.0.0.1.2.0/mlplatform_lib/dataclass/model/__init__.py
@@ -0,0 +1,6 @@
+from .model_automl_dto import ModelAutomlDto
+from .model_dto import ModelDto
+from .model_predefinedai_dto import ModelPredefinedaiDto
+from .model_info_dto import ModelInfoDto
+
+__all__ = ["ModelDto", "ModelPredefinedaiDto", "ModelInfoDto", "ModelAutomlDto"]
