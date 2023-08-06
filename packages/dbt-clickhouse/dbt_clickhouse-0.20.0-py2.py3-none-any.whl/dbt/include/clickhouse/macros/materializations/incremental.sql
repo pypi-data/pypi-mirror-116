51d2b4f1fec18e2aebf1dcc7ac83ee512d14c419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:17 +0000

    Add package dbt-clickhouse/0.20.0/dbt_clickhouse-0.20.0-py2.py3-none-any.whl

---
+++ packages/dbt-clickhouse/dbt_clickhouse-0.20.0-py2.py3-none-any.whl/dbt/include/clickhouse/macros/materializations/incremental.sql
@@ -0,0 +1,16 @@
+{% macro incremental_upsert(tmp_relation, target_relation, unique_key=none, statement_name='main') %}
+  {%- set dest_columns = adapter.get_columns_in_relation(target_relation) -%}
+  {%- set dest_cols_csv = dest_columns | map(attribute='quoted') | join(', ') -%}
+
+  {%- if unique_key is not none -%}
+  alter table {{ target_relation }} {{ on_cluster_clause(label="on cluster") }} delete
+  where ({{ unique_key }}) in (
+    select ({{ unique_key }})
+    from {{ tmp_relation }}
+  )
+  {%- endif %}
+
+  insert into {{ target_relation }} ({{ dest_cols_csv }})
+    select {{ dest_cols_csv }}
+    from {{ tmp_relation }};
+{%- endmacro %}
